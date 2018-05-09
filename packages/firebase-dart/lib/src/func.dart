4082f6d3b53e5f24a8621cbcfa5a76fce4eef4c0
Kevin Moore <kevmoo@google.com>
2018-05-08 22:50:55 -0700

    Fix nit in comment

--- packages/firebase-dart/lib/src/func.dart
+++ packages/firebase-dart/lib/src/func.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// TODO: once Dart v1 support is dropped, use new Function syntaxt instead
+// TODO: once Dart v1 support is dropped, use new Function syntax instead
 
 typedef R Func0<R>();
 typedef R Func1<A, R>(A a);
