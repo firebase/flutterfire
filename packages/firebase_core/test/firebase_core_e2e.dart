8df20bdedbeda9959e995cd2480a0eab177b52af
Maurice Parrish <bmparr@google.com>
2019-10-14 16:10:59 -0700

    Add e2e test files

---
+++ packages/firebase_core/test/firebase_core_e2e.dart
@@ -0,0 +1,14 @@
+// Copyright 2019, the Chromium project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:e2e/e2e.dart';
+
+void main() {
+  E2EWidgetsFlutterBinding.ensureInitialized();
+
+  testWidgets('Can get battery level', (WidgetTester tester) async {
+    expect(true, isFalse);
+  });
+}
