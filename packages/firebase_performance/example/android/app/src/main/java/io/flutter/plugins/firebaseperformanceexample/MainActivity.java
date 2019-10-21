6784ddf22abdfb95763f4c382b40a631e44e2087
Maurice Parrish <bmparr@google.com>
2019-10-21 11:46:09 -0500

    Embedder e2e stuff

--- packages/firebase_performance/example/android/app/src/main/java/io/flutter/plugins/firebaseperformanceexample/MainActivity.java
+++ packages/firebase_performance/example/android/app/src/main/java/io/flutter/plugins/firebaseperformanceexample/MainActivity.java
@@ -5,6 +5,7 @@ import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugins.firebaseperformance.FirebasePerformancePlugin;
 
 public class MainActivity extends FlutterActivity {
+  // TODO(bparrishMines): Remove this once v2 of GeneratedPluginRegistrant rolls to stable. https://github.com/flutter/flutter/issues/42694
   @Override
   public void configureFlutterEngine(FlutterEngine flutterEngine) {
     flutterEngine.getPlugins().add(new FirebasePerformancePlugin());
