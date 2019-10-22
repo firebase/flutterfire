93eb0325628e42bf16e68c337dbf2b60b476d81e
Maurice Parrish <bmparr@google.com>
2019-10-22 11:29:23 -0500

    Javadocs

--- packages/firebase_core/android/src/main/java/io/flutter/plugins/firebase/core/FirebaseCorePlugin.java
+++ packages/firebase_core/android/src/main/java/io/flutter/plugins/firebase/core/FirebaseCorePlugin.java
@@ -15,17 +15,33 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Flutter plugin implementation controlling the entrypoint for the Firebase SDK.
+ *
+ * <p>Instantiate this in an add to app scenario to gracefully handle activity and context changes.</p>
+ */
 public class FirebaseCorePlugin implements FlutterPlugin, MethodChannel.MethodCallHandler {
   private static final String CHANNEL_NAME = "plugins.flutter.io/firebase_core";
 
   private MethodChannel channel;
   private Context applicationContext;
 
+  /**
+   * Registers a plugin with the v1 embedding api {@code io.flutter.plugin.common}.
+   *
+   * <p>Calling this will register the plugin with the passed registrar. However plugins initialized
+   * this way won't react to changes in activity or context, unlike {@link FirebaseCorePlugin}.
+   */
   public static void registerWith(PluginRegistry.Registrar registrar) {
     final MethodChannel channel = new MethodChannel(registrar.messenger(), CHANNEL_NAME);
     channel.setMethodCallHandler(new FirebaseCorePlugin(registrar.context()));
   }
 
+  /**
+   * Default Constructor.
+   *
+   * <p>Use this constructor in an add to app scenario to gracefully handle activity and context changes.</p>
+   */
   public FirebaseCorePlugin() {}
 
   private FirebaseCorePlugin(Context applicationContext) {
