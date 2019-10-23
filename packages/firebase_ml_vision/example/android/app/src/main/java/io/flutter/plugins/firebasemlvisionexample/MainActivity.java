addf679025a418f9164d86109bc12dadb03c1600
Maurice Parrish <bmparr@google.com>
2019-10-23 13:58:58 -0700

    Use new camera with v2 embedding in MainActivity

--- packages/firebase_ml_vision/example/android/app/src/main/java/io/flutter/plugins/firebasemlvisionexample/MainActivity.java
+++ packages/firebase_ml_vision/example/android/app/src/main/java/io/flutter/plugins/firebasemlvisionexample/MainActivity.java
@@ -3,6 +3,7 @@ package io.flutter.plugins.firebasemlvisionexample;
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.embedding.engine.plugins.shim.ShimPluginRegistry;
+import io.flutter.plugins.camera.CameraPlugin;
 import io.flutter.plugins.firebasemlvision.FirebaseMlVisionPlugin;
 import io.flutter.plugins.imagepicker.ImagePickerPlugin;
 import io.flutter.plugins.pathprovider.PathProviderPlugin;
@@ -11,13 +12,12 @@ public class MainActivity extends FlutterActivity {
   @Override
   public void configureFlutterEngine(FlutterEngine flutterEngine) {
     flutterEngine.getPlugins().add(new FirebaseMlVisionPlugin());
+    flutterEngine.getPlugins().add(new CameraPlugin());
 
     final ShimPluginRegistry shimPluginRegistry = new ShimPluginRegistry(flutterEngine);
     PathProviderPlugin.registerWith(
         shimPluginRegistry.registrarFor("io.flutter.plugins.pathprovider.PathProviderPlugin"));
     ImagePickerPlugin.registerWith(
         shimPluginRegistry.registrarFor("io.flutter.plugins.imagepicker.VideoPlayerPlugin"));
-    //    CameraPlugin.registerWith(
-    //        shimPluginRegistry.registrarFor("io.flutter.plugins.camera.CameraPlugin"));
   }
 }
