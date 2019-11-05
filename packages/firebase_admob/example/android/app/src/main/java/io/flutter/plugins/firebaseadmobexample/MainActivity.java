04208e0faa151e9dd1d1d3c84914385ae4241663
Maurice Parrish <bmparr@google.com>
2019-11-05 14:52:51 -0800

    register e2e test

--- packages/firebase_admob/example/android/app/src/main/java/io/flutter/plugins/firebaseadmobexample/MainActivity.java
+++ packages/firebase_admob/example/android/app/src/main/java/io/flutter/plugins/firebaseadmobexample/MainActivity.java
@@ -4,6 +4,7 @@
 
 package io.flutter.plugins.firebaseadmobexample;
 
+import dev.flutter.plugins.e2e.E2EPlugin;
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugins.firebaseadmob.FirebaseAdMobPlugin;
@@ -12,6 +13,7 @@ public class MainActivity extends FlutterActivity {
   // TODO(bparrishMines): Remove this once v2 of GeneratedPluginRegistrant rolls to stable. https://github.com/flutter/flutter/issues/42694
   @Override
   public void configureFlutterEngine(FlutterEngine flutterEngine) {
+    flutterEngine.getPlugins().add(new E2EPlugin());
     flutterEngine.getPlugins().add(new FirebaseAdMobPlugin());
   }
 }
