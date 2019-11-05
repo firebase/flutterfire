32f16947aba4d9799230a3a93704f23edecc5d2a
Aaron Clarke <aaclarke@google.com>
2019-11-05 12:51:52 -0800

    Fixed running of tests.

--- packages/firebase_in_app_messaging/example/android/app/src/main/java/com/example/firebase_in_app_messaging_example/MainActivity.java
+++ packages/firebase_in_app_messaging/example/android/app/src/main/java/com/example/firebase_in_app_messaging_example/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.firebase_in_app_messaging_example;
 
+import dev.flutter.plugins.e2e.E2EPlugin;
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 import com.example.firebase_in_app_messaging.FirebaseInAppMessagingPlugin;
@@ -11,6 +12,7 @@ public class MainActivity extends FlutterActivity {
   // TODO(<github-username>): Remove this once v2 of GeneratedPluginRegistrant rolls to stable. https://github.com/flutter/flutter/issues/42694
   @Override
   public void configureFlutterEngine(FlutterEngine flutterEngine) {
+    flutterEngine.getPlugins().add(new E2EPlugin());
     flutterEngine.getPlugins().add(new FirebaseInAppMessagingPlugin());
     final ShimPluginRegistry shimPluginRegistry = new ShimPluginRegistry(flutterEngine);
     FirebaseAnalyticsPlugin.registerWith(
