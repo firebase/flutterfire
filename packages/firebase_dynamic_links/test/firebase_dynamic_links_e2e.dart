48452fe1b3963163f3937f9b592057789259643e
Maurice Parrish <bmparr@google.com>
2019-11-12 11:39:30 -0800

    Add an actual test

--- packages/firebase_dynamic_links/test/firebase_dynamic_links_e2e.dart
+++ packages/firebase_dynamic_links/test/firebase_dynamic_links_e2e.dart
@@ -6,7 +6,27 @@ import '../lib/firebase_dynamic_links.dart';
 void main() {
   E2EWidgetsFlutterBinding.ensureInitialized();
 
-  testWidgets('Is true true?', (WidgetTester tester) async {
-    expect(true, isTrue);
+  testWidgets('buildUrl', (WidgetTester tester) async {
+    final DynamicLinkParameters parameters = DynamicLinkParameters(
+      uriPrefix: 'https://cx4k7.app.goo.gl',
+      link: Uri.parse('https://dynamic.link.example/helloworld'),
+      androidParameters: AndroidParameters(
+        packageName: 'io.flutter.plugins.firebasedynamiclinksexample',
+        minimumVersion: 0,
+      ),
+      dynamicLinkParametersOptions: DynamicLinkParametersOptions(
+        shortDynamicLinkPathLength: ShortDynamicLinkPathLength.short,
+      ),
+      iosParameters: IosParameters(
+        bundleId: 'com.google.FirebaseCppDynamicLinksTestApp.dev',
+        minimumVersion: '0',
+      ),
+    );
+
+    final Uri uri = await parameters.buildUrl();
+    expect(
+      uri.toString(),
+      'https://cx4k7.app.goo.gl?amv=0&apn=io.flutter.plugins.firebasedynamiclinksexample&ibi=com.google.FirebaseCppDynamicLinksTestApp.dev&imv=0&link=https%3A%2F%2Fdynamic.link.example%2Fhelloworld',
+    );
   });
 }
