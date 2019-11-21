45a63acc81ca22860c19f21548c7107cbdf87d25
Xiao Yu <xiao@xster.net>
2019-11-20 19:38:08 -0800

    Update plugin API usage for stable compatible

--- packages/firebase_auth/android/src/main/java/io/flutter/plugins/firebaseauth/FirebaseAuthPlugin.java
+++ packages/firebase_auth/android/src/main/java/io/flutter/plugins/firebaseauth/FirebaseAuthPlugin.java
@@ -77,7 +77,7 @@ public class FirebaseAuthPlugin implements FlutterPlugin, MethodCallHandler, Act
 
   @Override
   public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
-    channel = new MethodChannel(binding.getBinaryMessenger(), "plugins.flutter.io/firebase_auth");
+    channel = new MethodChannel(binding.getFlutterEngine().getDartExecutor(), "plugins.flutter.io/firebase_auth");
     channel.setMethodCallHandler(this);
     FirebaseApp.initializeApp(binding.getApplicationContext());
   }
