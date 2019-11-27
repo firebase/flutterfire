e91ae463fb5ea25a6a26420295b00f9e362a2c23
Harry Terkelsen <het@google.com>
2019-11-27 10:20:10 -0800

    Add documentation

--- packages/firebase_core/firebase_core_web/lib/src/firebase_js.dart
+++ packages/firebase_core/firebase_core_web/lib/src/firebase_js.dart
@@ -7,15 +7,21 @@ library firebase_js;
 
 import 'package:js/js.dart';
 
+/// A Firebase app.
 @JS()
 class App {
+  /// The name of the app.
   external String get name;
+
+  /// The options that the app was configured with.
   external Options get options;
 }
 
+/// Options for configuring an app.
 @JS()
 @anonymous
 class Options {
+  /// Creates an options JavaScript object.
   external factory Options({
     String apiKey,
     String authDomain,
@@ -27,22 +33,44 @@ class Options {
     String measurementId,
   });
 
+  /// The API key used to authenticate requests.
   external String get apiKey;
+
+  /// Domain for authentication.
   external String get authDomain;
+
+  /// Realtime Database URL.
   external String get databaseURL;
+
+  /// The Google Cloud project ID.
   external String get projectId;
+
+  /// The Google Cloud Storage bucket name.
   external String get storageBucket;
+
+  /// The project number used to configure Messaging.
   external String get messagingSenderId;
+
+  /// The Google App ID that uniquely identifies an app instance.
   external String get appId;
+
+  /// An ID used for analytics.
   external String get measurementId;
 }
 
+/// The `firebase` namespace.
 @JS()
 class FirebaseCore {
+  /// Returns a list of initialized apps.
   external List<App> get apps;
+
+  /// Initializes the app named [name] with the given [options].
   external App initializeApp(Options options, String name);
+  
+  /// Returns the already-initialized app with the given [name].
   external App app(String name);
 }
 
+/// Return the `firebase` object.
 @JS()
 external FirebaseCore get firebase;
