c562c4135410d63b6c401712004c9eaf1bb09032
Chris Yang <ychris@google.com>
2019-12-12 12:43:39 -0800

    review fixes

--- packages/firebase_core/firebase_core_web/android/src/main/java/io/flutter/plugins/firebase_core_web/FirebaseCoreWebPlugin.java
+++ packages/firebase_core/firebase_core_web/android/src/main/java/io/flutter/plugins/firebase_core_web/FirebaseCoreWebPlugin.java
@@ -2,20 +2,14 @@ package io.flutter.plugins.firebase_core_web;
 
 import android.support.annotation.NonNull;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
-import io.flutter.plugin.common.MethodCall;
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
-import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /** FirebaseCoreWebPlugin */
-public class FirebaseCoreWebPlugin implements FlutterPlugin, MethodCallHandler {
+public class FirebaseCoreWebPlugin implements FlutterPlugin {
   @Override
   public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {}
 
   public static void registerWith(Registrar registrar) {}
-
-  @Override
-  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {}
 
   @Override
   public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {}
