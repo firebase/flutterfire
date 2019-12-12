d45f24dbc00e63b87d4ed17002efff22fe2a54bf
Maurice Parrish <bmparr@google.com>
2019-12-12 14:20:49 -0800

    Test native ad

--- packages/firebase_admob/example/android/app/src/main/java/io/flutter/plugins/firebaseadmobexample/EmbeddingV1Activity.java
+++ packages/firebase_admob/example/android/app/src/main/java/io/flutter/plugins/firebaseadmobexample/EmbeddingV1Activity.java
@@ -4,14 +4,37 @@
 
 package io.flutter.plugins.firebaseadmobexample;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.TextView;
+import com.google.android.gms.ads.formats.UnifiedNativeAd;
+import com.google.android.gms.ads.formats.UnifiedNativeAdView;
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.plugins.firebaseadmob.FirebaseAdMobPlugin.NativeAdFactory;
 
-public class EmbeddingV1Activity extends FlutterActivity {
+public class EmbeddingV1Activity extends FlutterActivity implements NativeAdFactory {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
+  }
+
+  @Override
+  public UnifiedNativeAdView createNativeAd(UnifiedNativeAd nativeAd) {
+    final UnifiedNativeAdView adView =
+        (UnifiedNativeAdView) getLayoutInflater().inflate(R.layout.my_native_ad, null);
+    final TextView headlineView = adView.findViewById(R.id.ad_headline);
+    final TextView bodyView = adView.findViewById(R.id.ad_body);
+
+    headlineView.setText(nativeAd.getHeadline());
+    bodyView.setText(nativeAd.getBody());
+
+    adView.setBackgroundColor(Color.YELLOW);
+
+    adView.setNativeAd(nativeAd);
+    adView.setBodyView(bodyView);
+    adView.setHeadlineView(headlineView);
+    return adView;
   }
 }
--- packages/firebase_admob/test/firebase_admob_e2e.dart
+++ packages/firebase_admob/test/firebase_admob_e2e.dart
@@ -12,4 +12,32 @@ void main() {
       completion(isTrue),
     );
   });
+
+  testWidgets('Native Ads', (WidgetTester tester) async {
+    bool adLoaded = false;
+    bool adImpression = false;
+
+    final NativeAd nativeAd = NativeAd(
+      adUnitId: NativeAd.testAdUnitId,
+      targetingInfo: MobileAdTargetingInfo(
+        keywords: <String>['foo', 'bar'],
+        contentUrl: 'http://foo.com/bar.html',
+        childDirected: true,
+        nonPersonalizedAds: true,
+      ),
+      listener: (MobileAdEvent event) {
+        print('NativeAd event: $event');
+        if (event == MobileAdEvent.loaded) adLoaded = true;
+        if (event == MobileAdEvent.impression) adImpression = true;
+      },
+    );
+
+    await nativeAd.load();
+    await Future<void>.delayed(Duration(seconds: 10));
+    expect(adLoaded, isTrue);
+
+    await nativeAd.show();
+    await Future<void>.delayed(Duration(seconds: 10));
+    expect(adImpression, isTrue);
+  });
 }
