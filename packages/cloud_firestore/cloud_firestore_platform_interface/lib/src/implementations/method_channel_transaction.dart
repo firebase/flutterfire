c65a7ddde6e1928da8de78b76215eb5ab4e54fe0
David Iglesias Teixeira <ditman@gmail.com>
2019-12-13 18:38:24 -0800

    [firestore_platform_interface] Add MethodChannelTransaction implementation.

--- packages/cloud_firestore/cloud_firestore_platform_interface/lib/src/implementations/method_channel_transaction.dart
+++ packages/cloud_firestore/cloud_firestore_platform_interface/lib/src/implementations/method_channel_transaction.dart
@@ -4,9 +4,10 @@
 import 'dart:async';
 
 import 'package:flutter/services.dart';
-import 'package:meta/meta.dart' show visibleForTesting;
+import 'package:meta/meta.dart' show required, visibleForTesting;
 
 import '../interfaces/transaction_platform.dart';
+import '../types.dart';
 
 class MethodChannelTransaction extends TransactionPlatform {
   MethodChannelTransaction(StandardMessageCodec codec) {
@@ -21,6 +22,10 @@ class MethodChannelTransaction extends TransactionPlatform {
   static MethodChannel get channel => MethodChannelTransaction._channel;
   static MethodChannel _channel;
 
+  final Map<int, PlatformTransactionHandler> _transactionHandlers =
+      <int, PlatformTransactionHandler>{};
+  int _transactionHandlerId = 0;
+
   Future<dynamic> _callHandler(MethodCall call) async {
     switch (call.method) {
       case 'DoTransaction':
@@ -28,8 +33,94 @@ class MethodChannelTransaction extends TransactionPlatform {
     }
   }
 
-  void _handleDoTransaction(MethodCall call) {
+  Future<dynamic> _handleDoTransaction(MethodCall call) async {
     final int transactionId = call.arguments['transactionId'];
     // Do the transaction
+    final PlatformTransactionHandler handler = _transactionHandlers[transactionId];
+    return await handler(PlatformTransaction(transactionId: transactionId));
+  }
+
+  @override
+  Future<Map<String, dynamic>> run(
+    String app, {
+    @required PlatformTransactionHandler updateFunction,
+    int transactionTimeout,
+  }) async {
+    // The [updateFunction] will be used by the [_handleDoTransaction] method later
+    final int transactionId = _transactionHandlerId++;
+    _transactionHandlers[transactionId] = updateFunction;
+
+    return channel.invokeMapMethod<String, dynamic>(
+        'Firestore#runTransaction', <String, dynamic>{
+        'app': app,
+        'transactionId': transactionId,
+        'transactionTimeout': transactionTimeout
+      });
+  }
+
+  @override
+  Future<PlatformDocumentSnapshot> get(
+    String app, {
+    @required String path,
+    @required int transactionId,
+  }) {
+    return channel
+        .invokeMapMethod<String, dynamic>('Transaction#get', <String, dynamic>{
+          'app': app,
+          'transactionId': transactionId,
+          'path': path,
+        }).then((data) {
+          return PlatformDocumentSnapshot(
+              path: data['path'],
+              data: data['data']?.cast<String, dynamic>(),
+              metadata: PlatformSnapshotMetadata(
+                  hasPendingWrites: data['metadata']['hasPendingWrites'],
+                  isFromCache: data['metadata']['isFromCache'],
+                ),
+          );
+        });
+  }
+
+  @override
+  Future<void> delete(
+    String app, {
+    @required String path,
+    @required int transactionId,
+  }) {
+    return channel.invokeMethod<void>('Transaction#delete', <String, dynamic>{
+        'app': app,
+        'transactionId': transactionId,
+        'path': path,
+      });
+  }
+
+  @override
+  Future<void> update(
+    String app, {
+    @required String path,
+    @required int transactionId,
+    Map<String, dynamic> data,
+  }) async {
+    return channel.invokeMethod<void>('Transaction#update', <String, dynamic>{
+        'app': app,
+        'transactionId': transactionId,
+        'path': path,
+        'data': data,
+      });
+  }
+
+  @override
+  Future<void> set(
+    String app, {
+    @required String path,
+    @required int transactionId,
+    Map<String, dynamic> data,
+  }) async {
+    return channel.invokeMethod<void>('Transaction#set', <String, dynamic>{
+        'app': app,
+        'transactionId': transactionId,
+        'path': path,
+        'data': data,
+      });
   }
 }
