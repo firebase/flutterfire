8860659e7ea77a5a1972e3dde3ad38c9155b711f
Amr Yousef <amr.farid140@gmail.com>
2019-12-20 15:03:06 +0000

    Separated common testing logic

--- packages/cloud_firestore/cloud_firestore_platform_interface/test/method_channel_document_reference.dart
+++ packages/cloud_firestore/cloud_firestore_platform_interface/test/method_channel_document_reference.dart
@@ -1,25 +1,20 @@
-import 'dart:async';
-
 import 'package:cloud_firestore_platform_interface/cloud_firestore_platform_interface.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
-typedef MethodCallCallback = dynamic Function(MethodCall methodCall);
+import 'test_common.dart';
 
 class MockFiledValue extends Mock implements FieldValueInterface {}
 
-const _kCollectionId = "test";
-const _kDocumentId = "document";
-
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
+
   group("$MethodChannelDocumentReference()", () {
     MethodChannelDocumentReference _documentReference;
     final mockFieldValue = MockFiledValue();
     setUp(() {
       _documentReference = MethodChannelDocumentReference(
-          FirestorePlatform.instance, [_kCollectionId, _kDocumentId]);
+          FirestorePlatform.instance, [kCollectionId, kDocumentId]);
       reset(mockFieldValue);
       when(mockFieldValue.type).thenReturn(FieldValueType.incrementDouble);
       when(mockFieldValue.value).thenReturn(2.0);
@@ -35,8 +30,11 @@ void main() {
 
     test("updateData", () async {
       bool isMethodCalled = false;
-      final Map<String,dynamic> data = {"test":"test","fieldValue": mockFieldValue};
-      _handleMethodCall((call) {
+      final Map<String, dynamic> data = {
+        "test": "test",
+        "fieldValue": mockFieldValue
+      };
+      handleMethodCall((call) {
         if (call.method == "DocumentReference#updateData") {
           isMethodCalled = true;
           expect(call.arguments["data"]["test"], equals(data["test"]));
@@ -48,17 +46,18 @@ void main() {
           reason: "DocumentReference.updateData was not called");
     });
 
-    test("get",() async {
+    test("get", () async {
       await _assertGetMethodCalled(_documentReference, null, "default");
       await _assertGetMethodCalled(_documentReference, Source.cache, "cache");
       await _assertGetMethodCalled(_documentReference, Source.server, "server");
-      await _assertGetMethodCalled(_documentReference, Source.serverAndCache, "default");
+      await _assertGetMethodCalled(
+          _documentReference, Source.serverAndCache, "default");
     });
 
     test("delete", () async {
       bool isMethodCalled = false;
-      _handleMethodCall((call) {
-        if(call.method == "DocumentReference#delete") {
+      handleMethodCall((call) {
+        if (call.method == "DocumentReference#delete") {
           isMethodCalled = true;
         }
       });
@@ -69,33 +68,31 @@ void main() {
 
     test("snapshots", () async {
       bool isMethodCalled = false;
-      _handleMethodCall((call)  {
-        if(call.method == "DocumentReference#addSnapshotListener") {
+      handleMethodCall((call) {
+        if (call.method == "DocumentReference#addSnapshotListener") {
           isMethodCalled = true;
         }
         return 0;
       });
-      _documentReference.snapshots().listen((_){});
+      _documentReference.snapshots().listen((_) {});
       expect(isMethodCalled, isTrue,
           reason: "DocumentReference.addSnapshotListener was not called");
     });
   });
 }
 
-void _assertGetMethodCalled(DocumentReference documentReference,Source source, String expectedSourceString ) async {
+void _assertGetMethodCalled(DocumentReference documentReference, Source source,
+    String expectedSourceString) async {
   bool isMethodCalled = false;
-  _handleMethodCall((call) {
+  handleMethodCall((call) {
     if (call.method == "DocumentReference#get") {
       isMethodCalled = true;
       expect(call.arguments["source"], equals(expectedSourceString));
     }
     return {
-      "path":"test/test",
+      "path": "test/test",
       "data": {},
-      "metadata": {
-        "hasPendingWrites": false,
-        "isFromCache": false
-      }
+      "metadata": {"hasPendingWrites": false, "isFromCache": false}
     };
   });
   if (source != null) {
@@ -107,13 +104,14 @@ void _assertGetMethodCalled(DocumentReference documentReference,Source source, S
       reason: "DocumentReference.get was not called");
 }
 
-void _assertSetDataMethodCalled(DocumentReference documentReference, bool expectedMergeValue, FieldValueInterface fieldValue) async {
+void _assertSetDataMethodCalled(DocumentReference documentReference,
+    bool expectedMergeValue, FieldValueInterface fieldValue) async {
   bool isMethodCalled = false;
-  final Map<String,dynamic> data = {"test":"test"};
-  if(fieldValue != null) {
+  final Map<String, dynamic> data = {"test": "test"};
+  if (fieldValue != null) {
     data.addAll({"fieldValue": fieldValue});
   }
-  _handleMethodCall((call) {
+  handleMethodCall((call) {
     if (call.method == "DocumentReference#setData") {
       isMethodCalled = true;
       expect(call.arguments["data"]["test"], equals(data["test"]));
@@ -123,15 +121,8 @@ void _assertSetDataMethodCalled(DocumentReference documentReference, bool expect
   if (expectedMergeValue == null) {
     await documentReference.setData(data);
   } else {
-    await documentReference.setData(data,merge: expectedMergeValue);
+    await documentReference.setData(data, merge: expectedMergeValue);
   }
   expect(isMethodCalled, isTrue,
       reason: "DocumentReference.setData was not called");
 }
-
-void _handleMethodCall(MethodCallCallback methodCallCallback) =>
-    MethodChannelFirestore.channel.setMockMethodCallHandler((call) async {
-      expect(call.arguments["app"], equals(FirestorePlatform.instance.appName()));
-      expect(call.arguments["path"], equals("$_kCollectionId/$_kDocumentId"));
-      return await methodCallCallback(call);
-    });
---
+++ packages/cloud_firestore/cloud_firestore_platform_interface/test/test_common.dart
@@ -0,0 +1,15 @@
+import 'package:cloud_firestore_platform_interface/cloud_firestore_platform_interface.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+typedef MethodCallCallback = dynamic Function(MethodCall methodCall);
+
+const kCollectionId = "test";
+const kDocumentId = "document";
+
+void handleMethodCall(MethodCallCallback methodCallCallback) =>
+    MethodChannelFirestore.channel.setMockMethodCallHandler((call) async {
+      expect(call.arguments["app"], equals(FirestorePlatform.instance.appName()));
+      expect(call.arguments["path"], equals("$kCollectionId/$kDocumentId"));
+      return await methodCallCallback(call);
+    });
