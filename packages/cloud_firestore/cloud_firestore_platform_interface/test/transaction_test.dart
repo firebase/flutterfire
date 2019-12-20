afd6b0e78afd60bc67e982216d32a24e1310b896
Amr Yousef <amr.farid140@gmail.com>
2019-12-20 15:02:47 +0000

    added transaction unit test

---
+++ packages/cloud_firestore/cloud_firestore_platform_interface/test/transaction_test.dart
@@ -0,0 +1,93 @@
+import 'package:cloud_firestore_platform_interface/cloud_firestore_platform_interface.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+import 'test_common.dart';
+
+class MockDocumentReference extends Mock implements DocumentReference {}
+
+class MockFiledValue extends Mock implements FieldValueInterface {}
+
+const _kTransactionId = 1022;
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+  final mockFieldValue = MockFiledValue();
+
+  group("$Transaction()", (){
+    Transaction transaction;
+    final mockDocumentReference = MockDocumentReference();
+    when(mockDocumentReference.path).thenReturn("$kCollectionId/$kDocumentId");
+    setUp((){
+      transaction = Transaction(_kTransactionId,FirestorePlatform.instance);
+      reset(mockFieldValue);
+      when(mockFieldValue.type).thenReturn(FieldValueType.incrementDouble);
+      when(mockFieldValue.value).thenReturn(2.0);
+    });
+
+    test("get", () async {
+      bool isMethodCalled = false;
+      handleMethodCall((call){
+        if(call.method == "Transaction#get") {
+          isMethodCalled = true;
+          expect(call.arguments["transactionId"], equals(_kTransactionId));
+        }
+      });
+      await transaction.get(mockDocumentReference);
+      expect(isMethodCalled, isTrue,
+          reason: "Transaction.get was not called");
+    });
+
+    test("delete", () async {
+      bool isMethodCalled = false;
+      handleMethodCall((call){
+        if(call.method == "Transaction#delete") {
+          isMethodCalled = true;
+          expect(call.arguments["transactionId"], equals(_kTransactionId));
+        }
+      });
+      await transaction.delete(mockDocumentReference);
+      expect(isMethodCalled, isTrue,
+          reason: "Transaction.delete was not called");
+    });
+
+    test("update", () async {
+      bool isMethodCalled = false;
+      final Map<String, dynamic> data = {
+        "test": "test",
+        "fieldValue": mockFieldValue
+      };
+      handleMethodCall((call) {
+        if (call.method == "Transaction#update") {
+          isMethodCalled = true;
+          expect(call.arguments["transactionId"], equals(_kTransactionId));
+          expect(call.arguments["data"]["test"], equals(data["test"]));
+        }
+      });
+      await transaction.update(mockDocumentReference,data);
+      verify(mockFieldValue.instance);
+      expect(isMethodCalled, isTrue,
+          reason: "Transaction#update was not called");
+    });
+
+    test("set", () async {
+      bool isMethodCalled = false;
+      final Map<String, dynamic> data = {
+        "test": "test",
+        "fieldValue": mockFieldValue
+      };
+      handleMethodCall((call) {
+        if (call.method == "Transaction#set") {
+          isMethodCalled = true;
+          expect(call.arguments["transactionId"], equals(_kTransactionId));
+          expect(call.arguments["data"]["test"], equals(data["test"]));
+        }
+      });
+      await transaction.set(mockDocumentReference,data);
+      verify(mockFieldValue.instance);
+      expect(isMethodCalled, isTrue,
+          reason: "Transaction#set was not called");
+    });
+
+  });
+}
