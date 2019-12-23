49edadaaa1ca5cc6819cab7ef6b6a08efb7aed15
Amr Yousef <amr.farid140@gmail.com>
2019-12-23 07:49:41 +0000

    Added visiblefortesting annotation

--- packages/cloud_firestore/cloud_firestore_platform_interface/lib/src/firestore_message_codec.dart
+++ packages/cloud_firestore/cloud_firestore_platform_interface/lib/src/firestore_message_codec.dart
@@ -8,6 +8,7 @@ part of cloud_firestore_platform_interface;
 // ignoring lint rule here as it's only visible for testing
 // ignore: public_member_api_docs
 class FirestoreMessageCodec extends StandardMessageCodec {
+  @visibleForTesting
   // ignoring lint rule here as it's only visible for testing
   // ignore: public_member_api_docs
   const FirestoreMessageCodec();
