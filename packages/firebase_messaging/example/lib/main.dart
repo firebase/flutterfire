46816426692fdb9b33e8e2bc13c6add00974fee1
Maurice Parrish <bmparr@google.com>
2020-01-08 17:44:02 -0800

    Add notification handling

--- packages/firebase_messaging/example/lib/main.dart
+++ packages/firebase_messaging/example/lib/main.dart
@@ -7,66 +7,60 @@ import 'dart:convert';
 
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:http/http.dart' as http;
 
-final Map<String, Item> _items = <String, Item>{};
-Item _itemForMessage(Map<String, dynamic> message) {
-  final dynamic data = message['data'] ?? message;
-  final String itemId = data['id'];
-  final Item item = _items.putIfAbsent(itemId, () => Item(itemId: itemId))
-    ..status = data['status'];
-  return item;
-}
+final Map<String, Notification> _notifications = <String, Notification>{};
 
-class Item {
-  Item({this.itemId});
-  final String itemId;
+class Notification {
+  Notification.fromJson(Map<String, dynamic> message)
+      : title = message['notification']['title'],
+        body = message['notification']['body'],
+        id = message['id'];
 
-  StreamController<Item> _controller = StreamController<Item>.broadcast();
-  Stream<Item> get onChanged => _controller.stream;
+  final String id;
+  final String title;
+  final String body;
 
-  String _status;
-  String get status => _status;
-  set status(String value) {
-    _status = value;
-    _controller.add(this);
-  }
+  StreamController<Notification> _controller =
+      StreamController<Notification>.broadcast();
+  Stream<Notification> get onChanged => _controller.stream;
 
   static final Map<String, Route<void>> routes = <String, Route<void>>{};
+
   Route<void> get route {
-    final String routeName = '/detail/$itemId';
+    final String routeName = '/detail/$id';
     return routes.putIfAbsent(
       routeName,
       () => MaterialPageRoute<void>(
         settings: RouteSettings(name: routeName),
-        builder: (BuildContext context) => DetailPage(itemId),
+        builder: (BuildContext context) => DetailPage(this),
       ),
     );
   }
 }
 
 class DetailPage extends StatefulWidget {
-  DetailPage(this.itemId);
-  final String itemId;
+  DetailPage(this.notification);
+
+  final Notification notification;
+
   @override
   _DetailPageState createState() => _DetailPageState();
 }
 
 class _DetailPageState extends State<DetailPage> {
-  Item _item;
-  StreamSubscription<Item> _subscription;
+  StreamSubscription<Notification> _subscription;
 
   @override
   void initState() {
     super.initState();
-    _item = _items[widget.itemId];
-    _subscription = _item.onChanged.listen((Item item) {
+    _subscription =
+        widget.notification.onChanged.listen((Notification notification) {
       if (!mounted) {
         _subscription.cancel();
       } else {
-        setState(() {
-          _item = item;
-        });
+        setState(() {});
       }
     });
   }
@@ -75,10 +69,10 @@ class _DetailPageState extends State<DetailPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Item ${_item.itemId}"),
+        title: Text('Title: ${widget.notification.title}'),
       ),
       body: Material(
-        child: Center(child: Text("Item status: ${_item.status}")),
+        child: Center(child: Text('Body: ${widget.notification.body}')),
       ),
     );
   }
@@ -93,14 +87,16 @@ class _PushMessagingExampleState extends State<PushMessagingExample> {
   String _homeScreenText = "Waiting for token...";
   bool _topicButtonsDisabled = false;
   final String serverToken = '<Server-Token>';
-
+  final FlutterLocalNotificationsPlugin _notificationsPlugin =
+      FlutterLocalNotificationsPlugin();
   final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
-  final TextEditingController _topicController =
-      TextEditingController(text: 'topic');
+  final TextEditingController _topicController = TextEditingController(
+    text: 'topic',
+  );
 
-  Widget _buildDialog(BuildContext context, Item item) {
+  Widget _buildDialog(BuildContext context, Notification notification) {
     return AlertDialog(
-      content: Text("Item ${item.itemId} has been updated"),
+      content: Text("Notification ${notification.id} has been updated"),
       actions: <Widget>[
         FlatButton(
           child: const Text('CLOSE'),
@@ -121,7 +117,14 @@ class _PushMessagingExampleState extends State<PushMessagingExample> {
   void _showItemDialog(Map<String, dynamic> message) {
     showDialog<bool>(
       context: context,
-      builder: (_) => _buildDialog(context, _itemForMessage(message)),
+      builder: (_) {
+        Notification notification = Notification.fromJson(message);
+        notification = _notifications.putIfAbsent(
+          notification.id,
+          () => notification,
+        );
+        return _buildDialog(context, notification);
+      },
     ).then((bool shouldNavigate) {
       if (shouldNavigate == true) {
         _navigateToItemDetail(message);
@@ -150,27 +153,67 @@ class _PushMessagingExampleState extends State<PushMessagingExample> {
           'priority': 'high',
           'data': <String, dynamic>{
             'click_action': 'FLUTTER_NOTIFICATION_CLICK',
-            'id': '1',
+            'id': '2',
             'status': 'done'
           },
           'to': await _firebaseMessaging.getToken(),
         },
       ),
     );
+
+    final NotificationDetails details = NotificationDetails(
+      AndroidNotificationDetails(
+        'your other channel id',
+        'your other channel name',
+        'your other channel description',
+        importance: Importance.Max,
+        priority: Priority.High,
+        ticker: 'ticker',
+      ),
+      IOSNotificationDetails(),
+    );
+    await _notificationsPlugin.show(0, 'plain title', 'plain body', details,
+        payload: 'plain payload');
   }
 
   void _navigateToItemDetail(Map<String, dynamic> message) {
-    final Item item = _itemForMessage(message);
+    final Notification notification = Notification.fromJson(message);
     // Clear away dialogs
     Navigator.popUntil(context, (Route<dynamic> route) => route is PageRoute);
-    if (!item.route.isCurrent) {
-      Navigator.push(context, item.route);
+    if (!notification.route.isCurrent) {
+      Navigator.push(context, notification.route);
     }
   }
 
   @override
   void initState() {
     super.initState();
+    _setupFirebaseMessaging();
+    _setupLocalNotifications();
+  }
+
+  void _setupLocalNotifications() {
+    final InitializationSettings settings = InitializationSettings(
+      AndroidInitializationSettings('app_icon'),
+      IOSInitializationSettings(
+        onDidReceiveLocalNotification: (
+          int id,
+          String title,
+          String body,
+          String payload,
+        ) {
+          print('onDidReceiveLocalNotification: $id $title $body $payload');
+          return Future<void>.value();
+        },
+      ),
+    );
+    _notificationsPlugin.initialize(settings, onSelectNotification: (_) {
+      print('onSelectNotification: $_');
+      return Future<void>.value();
+    });
+  }
+
+  void _setupFirebaseMessaging() {
     _firebaseMessaging.configure(
       onMessage: (Map<String, dynamic> message) async {
         print("onMessage: $message");
@@ -186,8 +229,13 @@ class _PushMessagingExampleState extends State<PushMessagingExample> {
       },
     );
     _firebaseMessaging.requestNotificationPermissions(
-        const IosNotificationSettings(
-            sound: true, badge: true, alert: true, provisional: true));
+      const IosNotificationSettings(
+        sound: true,
+        badge: true,
+        alert: true,
+        provisional: true,
+      ),
+    );
     _firebaseMessaging.onIosSettingsRegistered
         .listen((IosNotificationSettings settings) {
       print("Settings registered: $settings");
