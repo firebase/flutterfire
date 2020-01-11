9af142a104f85996322d6ce9d0bc79d3ffc81af7
David Iglesias Teixeira <ditman@gmail.com>
2020-01-10 18:09:56 -0800

    [firebase_auth_web] Add null check to _fromJsUser.
    * Added unit test for the onAuthStateChanged stream (from JS).
    
    Fixes https://github.com/FirebaseExtended/flutterfire/issues/1685

--- packages/firebase_auth/firebase_auth_web/test/firebase_auth_web_test.dart
+++ packages/firebase_auth/firebase_auth_web/test/firebase_auth_web_test.dart
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 @TestOn('chrome')
 
+import 'dart:async';
 import 'dart:js' as js;
 
 import 'package:flutter_test/flutter_test.dart';
@@ -42,6 +43,50 @@ void main() {
       AuthResult result = await auth.signInAnonymously();
       expect(result, isNotNull);
     });
+
+    group('onAuthStateChanged', () {
+      final List seenUsers = [];
+      final Completer<js.JsFunction> nextUserCallback =
+          Completer<js.JsFunction>();
+
+      final List<dynamic> streamValues = [_fakeRawUser(), null, _fakeRawUser()];
+      final List<dynamic> expectedValueMatchers = [
+        isA<FirebaseUser>(),
+        isNull,
+        isA<FirebaseUser>()
+      ];
+
+      test('non authenticated user present in stream', () async {
+        js.context['firebase']['auth'] = js.allowInterop((dynamic app) {
+          return js.JsObject.jsify(
+            <String, dynamic>{
+              'onAuthStateChanged': js.allowInterop(
+                  (js.JsFunction nextUserCb, js.JsFunction errorCb) {
+                if (!nextUserCallback.isCompleted) {
+                  nextUserCallback.complete(nextUserCb);
+                }
+                return js.allowInterop(() {});
+              }),
+            },
+          );
+        });
+
+        FirebaseAuth auth = FirebaseAuth.instance;
+
+        // Subscribe our spy function
+        auth.onAuthStateChanged
+            .listen((FirebaseUser user) => seenUsers.add(user));
+
+        // Capture the JS function that lets us push users to the Stream from JS.
+        js.JsFunction nextUser = await nextUserCallback.future;
+
+        streamValues.forEach((streamValue) => nextUser.apply([streamValue]));
+
+        for (int i = 0; i < expectedValueMatchers.length; i++) {
+          expect(seenUsers[i], expectedValueMatchers[i]);
+        }
+      });
+    });
   });
 }
 
@@ -64,5 +109,16 @@ js.JsObject _fakeUserCredential() {
       'providerData': <dynamic>[],
     },
     'additionalUserInfo': <String, dynamic>{},
+  });
+}
+
+js.JsObject _fakeRawUser() {
+  return js.JsObject.jsify(<String, dynamic>{
+    'providerId': 'email',
+    'metadata': <String, dynamic>{
+      'creationTime': 'Wed, 04 Dec 2019 18:19:11 GMT',
+      'lastSignInTime': 'Wed, 04 Dec 2019 18:19:11 GMT',
+    },
+    'providerData': <dynamic>[],
   });
 }
