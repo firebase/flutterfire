a29adaa0b3f08fd9da8c9d694b2d3b3654d14d4e
Zhen Qiu <zhenqiu@google.com>
2020-01-23 14:51:38 -0800

    Keep a local registrar to get activity for foreground actions for v1 embedder.

--- packages/firebase_analytics/firebase_analytics/android/src/main/java/io/flutter/plugins/firebaseanalytics/FirebaseAnalyticsPlugin.java
+++ packages/firebase_analytics/firebase_analytics/android/src/main/java/io/flutter/plugins/firebaseanalytics/FirebaseAnalyticsPlugin.java
@@ -25,12 +25,20 @@ import java.util.Map;
 public class FirebaseAnalyticsPlugin implements MethodCallHandler, FlutterPlugin, ActivityAware {
   private FirebaseAnalytics firebaseAnalytics;
   private MethodChannel methodChannel;
+  // Only set registrar for v1 embedder.
+  private PluginRegistry.Registrar registrar;
+  // Only set activity for v2 embedder. Always access activity from getActivity() method.
   private Activity activity;
 
   public static void registerWith(PluginRegistry.Registrar registrar) {
     FirebaseAnalyticsPlugin instance = new FirebaseAnalyticsPlugin();
-    instance.setActivity(registrar.activity());
+    instance.registrar = registrar;
     instance.onAttachedToEngine(registrar.context(), registrar.messenger());
+  }
+
+  // Only access activity with this method.
+  private Activity getActivity() {
+    return registrar != null ? registrar.activity() : activity;
   }
 
   @Override
@@ -120,7 +128,7 @@ public class FirebaseAnalyticsPlugin implements MethodCallHandler, FlutterPlugin
   }
 
   private void handleSetCurrentScreen(MethodCall call, Result result) {
-    if (activity == null) {
+    if (getActivity() == null) {
       result.error("no_activity", "handleSetCurrentScreen requires a foreground activity", null);
       return;
     }
@@ -128,7 +136,7 @@ public class FirebaseAnalyticsPlugin implements MethodCallHandler, FlutterPlugin
     final String screenName = call.argument("screenName");
     final String screenClassOverride = call.argument("screenClassOverride");
 
-    firebaseAnalytics.setCurrentScreen(activity, screenName, screenClassOverride);
+    firebaseAnalytics.setCurrentScreen(getActivity(), screenName, screenClassOverride);
     result.success(null);
   }
 
