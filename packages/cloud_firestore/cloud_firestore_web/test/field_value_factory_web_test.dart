e40a1c3d5d24546376af5f9c27be0f4d63c550fe
David Iglesias Teixeira <ditman@gmail.com>
2020-02-03 16:02:33 -0800

    Add copyright notices everywhere.

--- packages/cloud_firestore/cloud_firestore_web/test/field_value_factory_web_test.dart
+++ packages/cloud_firestore/cloud_firestore_web/test/field_value_factory_web_test.dart
@@ -1,3 +1,7 @@
+// Copyright 2017, the Chromium project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 @TestOn("chrome")
 import 'package:cloud_firestore_platform_interface/cloud_firestore_platform_interface.dart';
 import 'package:cloud_firestore_web/src/field_value_factory_web.dart';
