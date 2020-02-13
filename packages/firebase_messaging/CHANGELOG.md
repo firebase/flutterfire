c2807ebc79d09c926991937ef8169f10a25c826e
Ben Hagen <c.ben.hagen@gmail.com>
2020-02-13 09:13:35 +0100

    Add macos example and depend on macos firebase_core

--- packages/firebase_messaging/CHANGELOG.md
+++ packages/firebase_messaging/CHANGELOG.md
@@ -1,3 +1,7 @@
+## 6.0.11
+
+* Add macOS support
+
 ## 6.0.10
 
 * Update README to explain how to correctly implement Android background message handling with the new v2 embedding. 
---
+++ packages/firebase_messaging/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import firebase_messaging
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
+}
