5211979ae6ef696e0b5f8c8b0df9c1db2ac0d6f2
Sander Roest <s.roest@dalosy.com>
2020-02-17 21:35:38 +0100

    Swap both width and height non iOS (Android), otherwise the center of  validRect is not calculated correctly.

--- packages/firebase_ml_vision/example/lib/material_barcode_scanner.dart
+++ packages/firebase_ml_vision/example/lib/material_barcode_scanner.dart
@@ -210,11 +210,11 @@ class _MaterialBarcodeScannerState extends State<MaterialBarcodeScanner>
         data.size.height - padding.top - padding.bottom;
 
     // Width & height are flipped from CameraController.previewSize on iOS
-    final double imageHeight = defaultTargetPlatform == TargetPlatform.iOS
-        ? imageSize.height
-        : imageSize.width;
+    if (defaultTargetPlatform != TargetPlatform.iOS) {
+      imageSize = Size(imageSize.height, imageSize.width);
+    }
 
-    final double imageScale = imageHeight / maxLogicalHeight;
+    final double imageScale = imageSize.height / maxLogicalHeight;
     final double halfWidth = imageScale * widget.validRectangle.width / 2;
     final double halfHeight = imageScale * widget.validRectangle.height / 2;
 
