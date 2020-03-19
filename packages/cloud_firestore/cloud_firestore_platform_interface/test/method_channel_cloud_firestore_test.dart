bd9bcd49f7dffe757df48bed54f0cb67552c2eec
David Iglesias <ditman@gmail.com>
2020-03-19 16:31:43 -0700

    [cloud_firestore_web] Ensure Query pagination methods encode values. (#2166)
    This change ensures that the package:firestore Query objects are created with properly encoded values when done so from the start/end (pagination) methods.
    
    This also adds some Timestamps to the unit tests to verify the behavior of Timestamp -> DateTime in the web implementation.
    
    (There are some integration tests in cloud_firestore that cover this behavior further.)
    
    Fixes https://github.com/FirebaseExtended/flutterfire/issues/2153
    
    * [cloud_firestore_platform_interface] Fix 'equal_keys_in_map' lint (unrelated fix, but this change triggered an analysis in a sibling package)

--- packages/cloud_firestore/cloud_firestore_platform_interface/test/method_channel_cloud_firestore_test.dart
+++ packages/cloud_firestore/cloud_firestore_platform_interface/test/method_channel_cloud_firestore_test.dart
@@ -1018,7 +1018,6 @@ void main() {
                   'app': app.name,
                   'path': 'bar',
                   'isCollectionGroup': true,
-                  'source': 'default',
                   'parameters': <String, dynamic>{
                     'where': <List<dynamic>>[],
                     'orderBy': <List<dynamic>>[],
@@ -1036,7 +1035,6 @@ void main() {
                   'app': app.name,
                   'path': 'bar',
                   'isCollectionGroup': true,
-                  'source': 'default',
                   'parameters': <String, dynamic>{
                     'where': <List<dynamic>>[],
                     'orderBy': <List<dynamic>>[],
