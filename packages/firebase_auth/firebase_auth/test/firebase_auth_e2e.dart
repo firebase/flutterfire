c19143e0fb45d649779361c3b3e044962c18b3f3
Zhen Qiu <zhenqiu@google.com>
2020-04-01 16:58:25 -0700

    add e2e test case

--- packages/firebase_auth/firebase_auth/test/firebase_auth_e2e.dart
+++ packages/firebase_auth/firebase_auth/test/firebase_auth_e2e.dart
@@ -3,7 +3,14 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'package:e2e/e2e.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() {
   E2EWidgetsFlutterBinding.ensureInitialized();
+
+  testWidgets('Can initialize the plugin', (WidgetTester tester) async {
+    FirebaseAuth auth = FirebaseAuth();
+    expect(auth, isNotNull);
+  });
 }
