567dc5001e7f3d4742da8d82c199b5b6a654472f
Maurice Parrish <bmparr@google.com>
2020-04-22 18:24:07 -0700

    Make test a driver test

--- packages/firebase_admob/test/firebase_admob_e2e.dart
+++ packages/firebase_admob/test/firebase_admob_e2e.dart
@@ -13,26 +13,6 @@ void main() {
     );
   });
 
-  testWidgets('Rewarded Video Ads', (WidgetTester tester) async {
-    // Request without a targeting info
-    bool hasStartedLoading = await RewardedVideoAd.instance.load(
-      adUnitId: RewardedVideoAd.testAdUnitId,
-    );
-    expect(hasStartedLoading, isTrue);
-
-    // Request with a targeting info
-    hasStartedLoading = await RewardedVideoAd.instance.load(
-      adUnitId: RewardedVideoAd.testAdUnitId,
-      targetingInfo: MobileAdTargetingInfo(
-        keywords: <String>['foo', 'bar'],
-        contentUrl: 'http://foo.com/bar.html',
-        childDirected: true,
-        nonPersonalizedAds: true,
-      ),
-    );
-    expect(hasStartedLoading, isTrue);
-  });
-
   testWidgets('Native Ads', (WidgetTester tester) async {
     bool adLoaded = false;
 
