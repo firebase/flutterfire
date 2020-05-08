06b3d888055f7aba320344470504861074a0069e
Brainants PC <pc.brainants@gmail.com>
2020-05-09 00:24:55 +0545

    Added final polishing issues

--- packages/firebase_database/firebase_database_web/test/firebase_database_web_test.dart
+++ packages/firebase_database/firebase_database_web/test/firebase_database_web_test.dart
@@ -1,8 +1,10 @@
-import 'package:flutter/services.dart';
+// Copyright 2017, the Chromium project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  const MethodChannel channel = MethodChannel('firebase_database_web');
-
+  // TODO(awazgyawalu): Write test for firestore web
   TestWidgetsFlutterBinding.ensureInitialized();
 }
