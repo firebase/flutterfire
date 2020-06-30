a61e8ba847dc2012adf548db83b75a6c89ace4e7
Alon Horovitz <alonhorowitz@gmail.com>
2020-06-29 21:48:28 -0500

    format

--- packages/firebase_analytics/firebase_analytics_web/lib/firebase_analytics_web.dart
+++ packages/firebase_analytics/firebase_analytics_web/lib/firebase_analytics_web.dart
@@ -67,18 +67,18 @@ class FirebaseAnalyticsWeb extends FirebaseAnalyticsPlatform {
     String endDate,
   }) async {
     _analytics.logEvent("add_to_cart",{
-      "itemId" : itemId,
-      "itemName" : itemName,
-      "itemCategory" : itemCategory,
-      "quantity" : quantity,
-      "price" : price,
-      "value" : value,
-      "currency" : currency,
-      "origin" : origin,
-      "itemLocationId" : itemLocationId,
-      "destination" : destination,
-      "startDate" : startDate,
-      "endDate" : endDate
+      "itemId": itemId,
+      "itemName": itemName,
+      "itemCategory": itemCategory,
+      "quantity": quantity,
+      "price": price,
+      "value": value,
+      "currency": currency,
+      "origin": origin,
+      "itemLocationId": itemLocationId,
+      "destination": destination,
+      "startDate": startDate,
+      "endDate": endDate
     });
   }
 
@@ -101,21 +101,21 @@ class FirebaseAnalyticsWeb extends FirebaseAnalyticsPlatform {
     String travelClass,
   }) async {
     _analytics.logEvent("purchase",{
-      "currency" : currency,
-      "value" : value,
-      "transactionId" : transactionId,
-      "tax" : tax,
-      "shipping" : shipping,
-      "coupon" : coupon,
-      "location" : location,
-      "numberOfNights" : numberOfNights,
-      "numberOfRooms" : numberOfRooms,
-      "numberOfPassengers" : numberOfPassengers,
-      "origin" : origin,
-      "destination" : destination,
-      "startDate" : startDate,
-      "endDate" : endDate,
-      "travelClass" : travelClass,
+      "currency": currency,
+      "value": value,
+      "transactionId": transactionId,
+      "tax": tax,
+      "shipping": shipping,
+      "coupon": coupon,
+      "location": location,
+      "numberOfNights": numberOfNights,
+      "numberOfRooms": numberOfRooms,
+      "numberOfPassengers": numberOfPassengers,
+      "origin": origin,
+      "destination": destination,
+      "startDate": startDate,
+      "endDate": endDate,
+      "travelClass": travelClass,
     });
   }
 }
