48aa79326aa1d2bdf8537470d05971d9766b84d4
Mike Diarmid <mike.diarmid@gmail.com>
2020-07-07 15:20:39 +0100

    feat(firebase_firestore): v1 rework (#28)
    Co-authored-by: ehesp <elliot.hesp@gmail.com>
    Co-authored-by: Helena Ford <helenaellieford@gmail.com>
    Co-authored-by: Kirsty Williams <kirstywilliams@users.noreply.github.com>
    Co-authored-by: Greg Hesp <greg.hesp@gmail.com>

--- packages/cloud_firestore/cloud_firestore/example/test_driver/cloud_firestore_e2e_test.dart
+++ packages/cloud_firestore/cloud_firestore/example/test_driver/cloud_firestore_e2e_test.dart
@@ -2,14 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'dart:async';
-import 'dart:io';
-import 'package:flutter_driver/flutter_driver.dart';
+import 'package:drive/drive_driver.dart' as drive;
 
-Future<void> main() async {
-  final FlutterDriver driver = await FlutterDriver.connect();
-  final String result =
-      await driver.requestData(null, timeout: const Duration(minutes: 1));
-  await driver.close();
-  exit(result == 'pass' ? 0 : 1);
-}
+void main() => drive.main();
---
+++ packages/cloud_firestore/cloud_firestore/macos/Classes/Private/FLTFirebaseFirestoreUtils.h
@@ -0,0 +1,1 @@
+../../../ios/Classes/Private/FLTFirebaseFirestoreUtils.h
