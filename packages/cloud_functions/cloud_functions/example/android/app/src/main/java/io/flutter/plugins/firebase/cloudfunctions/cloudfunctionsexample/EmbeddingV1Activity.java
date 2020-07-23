607c6d805b4b1508b810dab1e141b1ec1791aabd
Arthur Thompson <arthurthompson@google.com>
2020-07-23 16:20:09 -0700

    fix typos

--- packages/cloud_functions/cloud_functions/example/android/app/src/main/java/io/flutter/plugins/firebase/cloudfunctions/cloudfunctionsexample/EmbeddingV1Activity.java
+++ packages/cloud_functions/cloud_functions/example/android/app/src/main/java/io/flutter/plugins/firebase/cloudfunctions/cloudfunctionsexample/EmbeddingV1Activity.java
@@ -1,22 +1,22 @@
 package io.flutter.plugins.firebasestorageexample;
 
 
-import android.os.Bundle;	import android.os.Bundle;
+import android.os.Bundle;
 import dev.flutter.plugins.e2e.E2EPlugin;
-import io.flutter.app.FlutterActivity;	import io.flutter.app.FlutterActivity;
-import io.flutter.plugins.GeneratedPluginRegistrant;	import io.flutter.plugins.firebase.core.FlutterFirebaseCorePlugin;
-import io.flutter.plugins.firebase.storage.FirebaseStoragePlugin;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.firebase.core.FlutterFirebaseCorePlugin;
+import io.flutter.plugins.firebase.cloudfunctions.CloudFunctionsPlugin;
 
 
-public class EmbeddingV1Activity extends FlutterActivity {	public class EmbeddingV1Activity extends FlutterActivity {
-  @Override	  @Override
-  protected void onCreate(Bundle savedInstanceState) {	  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);	    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);	    FlutterFirebaseCorePlugin.registerWith(
+public class EmbeddingV1Activity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    FlutterFirebaseCorePlugin.registerWith(
       registrarFor("io.flutter.plugins.firebase.core.FlutterFirebaseCorePlugin"));
     // TODO(Salakar) rename as part of re-work CloudFunctionsPlugin -> FlutterFirebaseFunctionsPlugin
     CloudFunctionsPlugin.registerWith(
       registrarFor("io.flutter.plugins.firebase.cloudfunctions.CloudFunctionsPlugin"));
     E2EPlugin.registerWith(registrarFor("dev.flutter.plugins.e2e.E2EPlugin"));
-  }	  }
-}	}
+  }
+}
