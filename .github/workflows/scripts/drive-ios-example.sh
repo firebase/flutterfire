663d2cc89b7f1bec05aed74537d2a570408eb90d
Mike Diarmid <mike.diarmid@gmail.com>
2020-07-28 23:31:20 +0100

    tests(firebase_functions): rework ci workflow

--- .github/workflows/scripts/drive-ios-example.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-xcrun simctl boot "iPhone 11"
-
-melos exec -c 1 --fail-fast --scope="$FLUTTERFIRE_PLUGIN_SCOPE_EXAMPLE" --dir-exists=test_driver -- \
-  flutter drive -d \"iPhone 11\" --no-pub --target=./test_driver/MELOS_PARENT_PACKAGE_NAME_e2e.dart
