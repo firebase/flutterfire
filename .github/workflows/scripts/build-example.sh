d68e773df3bfd74b2ef7f6195fbfccc5569d97b6
Salakar <mike.diarmid@gmail.com>
2020-07-29 11:06:51 +0100

    tests(*): add macos drive/build scripts

--- .github/workflows/scripts/build-example.sh
+++ .github/workflows/scripts/build-example.sh
@@ -18,6 +18,13 @@ then
   exit
 fi
 
+if [ "$ACTION" == "macos" ]
+then
+  melos exec -c 1 --scope="$FLUTTERFIRE_PLUGIN_SCOPE_EXAMPLE" -- \
+    flutter build macos --debug --target=./test_driver/MELOS_PARENT_PACKAGE_NAME_e2e.dart
+  exit
+fi
+
 if [ "$ACTION" == "web" ]
 then
   melos exec -c 1 --scope="$FLUTTERFIRE_PLUGIN_SCOPE_EXAMPLE" --dir-exists=web -- \
--- .github/workflows/scripts/drive-example.sh
+++ .github/workflows/scripts/drive-example.sh
@@ -14,6 +14,14 @@ then
   xcrun simctl boot "iPhone 11"
   melos exec -c 1 --fail-fast --scope="$FLUTTERFIRE_PLUGIN_SCOPE_EXAMPLE" --dir-exists=test_driver -- \
     flutter drive -d \"iPhone 11\" --no-pub --no-build --target=./test_driver/MELOS_PARENT_PACKAGE_NAME_e2e.dart
+  xcrun simctl shutdown "iPhone 11"
+  exit
+fi
+
+if [ "$ACTION" == "macos" ]
+then
+  melos exec -c 1 --fail-fast --scope="$FLUTTERFIRE_PLUGIN_SCOPE_EXAMPLE" --dir-exists=test_driver -- \
+    flutter drive -d macos --no-pub --no-build --target=./test_driver/MELOS_PARENT_PACKAGE_NAME_e2e.dart
   exit
 fi
 
