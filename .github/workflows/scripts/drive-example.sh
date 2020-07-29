80898c5e47334812632b877be58016fd84dbaeff
Salakar <mike.diarmid@gmail.com>
2020-07-29 14:10:00 +0100

    refactor: update web CI scripts

--- .github/workflows/scripts/drive-example.sh
+++ .github/workflows/scripts/drive-example.sh
@@ -27,6 +27,7 @@ fi
 
 if [ "$ACTION" == "web" ]
 then
+  melos bootstrap
   chromedriver --port=4444 &
   melos exec -c 1 --scope="$FLUTTERFIRE_PLUGIN_SCOPE_EXAMPLE" --dir-exists=web -- \
     flutter drive --release --no-pub --verbose-system-logs --browser-name=chrome --target=./test_driver/MELOS_PARENT_PACKAGE_NAME_e2e.dart
