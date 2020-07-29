aa1917868cf6308c683f9fa9b5675292089cfb24
Salakar <mike.diarmid@gmail.com>
2020-07-29 15:25:50 +0100

    -

--- .github/workflows/scripts/drive-example.sh
+++ .github/workflows/scripts/drive-example.sh
@@ -19,8 +19,9 @@ then
   xcrun simctl logverbose "$SIMULATOR" enable
   # Sleep to allow simulator to settle.
   sleep 15
+  xcrun simctl spawn booted log stream --predicate 'eventMessage contains "flutter"' &
   melos exec -c 1 --fail-fast --scope="$FLUTTERFIRE_PLUGIN_SCOPE_EXAMPLE" --dir-exists=test_driver -- \
-    flutter drive -d \"$SIMULATOR\" --no-pub --no-build --target=./test_driver/MELOS_PARENT_PACKAGE_NAME_e2e.dart
+    flutter drive -d \"$SIMULATOR\" --verbose --no-pub --no-build --target=./test_driver/MELOS_PARENT_PACKAGE_NAME_e2e.dart
   xcrun simctl shutdown "$SIMULATOR"
   exit
 fi
