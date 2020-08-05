8f8e6ba08691915ae77fb92d31e30c4052d521af
Polina Govorkova <catchingthegist@gmail.com>
2020-08-05 14:01:13 -0400

    Placed final values before constructors

--- packages/firebase_ml_vision/lib/src/document_text_recognizer.dart
+++ packages/firebase_ml_vision/lib/src/document_text_recognizer.dart
@@ -31,10 +31,12 @@ enum TextRecognizedBreakType {
 ///     await documentTextRecognizer.processImage(image);
 /// ```
 class DocumentTextRecognizer {
+  final int _handle;
+
   DocumentTextRecognizer._({
     @required int handle,
   }) : _handle = handle;
-  final int _handle;
+
   bool _hasBeenOpened = false;
   bool _isClosed = false;
 
@@ -69,50 +71,35 @@ class DocumentTextRecognizer {
 
 /// Representation for start or end of a structural component.
 class DocumentTextRecognizedBreak {
+  /// Is set to the detected break type in a text logical component.
+  final TextRecognizedBreakType detectedBreakType;
+
+  /// Is set to true if break prepends an element.
+  final bool isPrefix;
+
   DocumentTextRecognizedBreak._(dynamic data)
       : detectedBreakType =
             TextRecognizedBreakType.values[data['detectedBreakType']],
         isPrefix = data['detectedBreakPrefix'];
-  final TextRecognizedBreakType detectedBreakType;
-  final bool isPrefix;
 }
 
 /// Recognized document text in a document image.
 class VisionDocumentText {
-  VisionDocumentText._(Map<String, dynamic> data)
-      : text = data['text'],
-        blocks = List<DocumentTextBlock>.unmodifiable(data['blocks']
-            .map<DocumentTextBlock>(
-                (dynamic block) => DocumentTextBlock._(block)));
-
   /// String representation of the recognized text.
   final String text;
 
   /// All recognized text broken down into individual blocks.
   final List<DocumentTextBlock> blocks;
+
+  VisionDocumentText._(Map<String, dynamic> data)
+      : text = data['text'],
+        blocks = List<DocumentTextBlock>.unmodifiable(data['blocks']
+            .map<DocumentTextBlock>(
+                (dynamic block) => DocumentTextBlock._(block)));
 }
 
 /// Abstract class for common attributes of text elements in a document image.
 abstract class DocumentTextContainer {
-  DocumentTextContainer._(Map<dynamic, dynamic> data)
-      : boundingBox = data['left'] != null
-            ? Rect.fromLTWH(
-                data['left'],
-                data['top'],
-                data['width'],
-                data['height'],
-              )
-            : null,
-        confidence =
-            data['confidence'] == null ? null : data['confidence'].toDouble(),
-        recognizedBreak = data['recognizedBreak'] == null
-            ? null
-            : DocumentTextRecognizedBreak._(data['recognizedBreak']),
-        recognizedLanguages = List<RecognizedLanguage>.unmodifiable(
-            data['recognizedLanguages'].map<RecognizedLanguage>(
-                (dynamic language) => RecognizedLanguage._(language))),
-        text = data['text'];
-
   /// Axis-aligned bounding rectangle of the detected text.
   ///
   /// The point (0, 0) is defined as the upper-left corner of the image.
@@ -136,41 +123,60 @@ abstract class DocumentTextContainer {
   ///
   /// Returns empty string if nothing is found.
   final String text;
+
+  DocumentTextContainer._(Map<dynamic, dynamic> data)
+      : boundingBox = data['left'] != null
+            ? Rect.fromLTWH(
+                data['left'],
+                data['top'],
+                data['width'],
+                data['height'],
+              )
+            : null,
+        confidence =
+            data['confidence'] == null ? null : data['confidence'].toDouble(),
+        recognizedBreak = data['recognizedBreak'] == null
+            ? null
+            : DocumentTextRecognizedBreak._(data['recognizedBreak']),
+        recognizedLanguages = List<RecognizedLanguage>.unmodifiable(
+            data['recognizedLanguages'].map<RecognizedLanguage>(
+                (dynamic language) => RecognizedLanguage._(language))),
+        text = data['text'];
 }
 
 /// A logical element on the page.
 class DocumentTextBlock extends DocumentTextContainer {
+  /// The content of the document block, broken down into individual paragraphs.
+  final List<DocumentTextParagraph> paragraphs;
+
   DocumentTextBlock._(Map<dynamic, dynamic> block)
       : paragraphs = List<DocumentTextParagraph>.unmodifiable(
             block['paragraphs'].map<DocumentTextParagraph>(
                 (dynamic paragraph) => DocumentTextParagraph._(paragraph))),
         super._(block);
-
-  /// The content of the document block, broken down into individual paragraphs.
-  final List<DocumentTextParagraph> paragraphs;
 }
 
 /// A structural unit of text representing a number of words in certain order.
 class DocumentTextParagraph extends DocumentTextContainer {
+  /// The content of the document paragraph, broken down into individual words.
+  final List<DocumentTextWord> words;
+
   DocumentTextParagraph._(Map<dynamic, dynamic> paragraph)
       : words = List<DocumentTextWord>.unmodifiable(paragraph['words']
             .map<DocumentTextWord>((dynamic word) => DocumentTextWord._(word))),
         super._(paragraph);
-
-  /// The content of the document paragraph, broken down into individual words.
-  final List<DocumentTextWord> words;
 }
 
 /// A single word representation.
 class DocumentTextWord extends DocumentTextContainer {
+  /// The content of the document word, broken down into individual symbols.
+  final List<DocumentTextSymbol> symbols;
+
   DocumentTextWord._(Map<dynamic, dynamic> word)
       : symbols = List<DocumentTextSymbol>.unmodifiable(word['symbols']
             .map<DocumentTextSymbol>(
                 (dynamic symbol) => DocumentTextSymbol._(symbol))),
         super._(word);
-
-  /// The content of the document word, broken down into individual symbols.
-  final List<DocumentTextSymbol> symbols;
 }
 
 /// A single symbol representation.
