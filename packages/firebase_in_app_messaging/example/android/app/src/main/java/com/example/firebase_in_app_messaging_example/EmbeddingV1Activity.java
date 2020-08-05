591b99a684aeec4889579ae3198f9357852dcff0
Arthur Thompson <arthurthompson@google.com>
2020-08-04 21:29:47 -0700

    fix formatting

--- packages/firebase_in_app_messaging/example/android/app/src/main/java/com/example/firebase_in_app_messaging_example/EmbeddingV1Activity.java
+++ packages/firebase_in_app_messaging/example/android/app/src/main/java/com/example/firebase_in_app_messaging_example/EmbeddingV1Activity.java
@@ -2,17 +2,16 @@ package com.example.firebase_in_app_messaging_example;
 
 import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
-import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class EmbeddingV1Activity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     FlutterFirebaseCorePlugin.registerWith(
-      registrarFor("io.flutter.plugins.firebase.core.FlutterFirebaseCorePlugin"));
+        registrarFor("io.flutter.plugins.firebase.core.FlutterFirebaseCorePlugin"));
     // TODO(Salakar) rename as part of re-work FirebaseInAppMessagingPlugin -> FlutterFirebaseInAppMessagingPlugin
     FirebasePerformancePlugin.registerWith(
-      registrarFor("io.flutter.plugins.firebase.inappmessaging.FirebasePerformancePlugin"));
+        registrarFor("io.flutter.plugins.firebase.inappmessaging.FirebasePerformancePlugin"));
     E2EPlugin.registerWith(registrarFor("dev.flutter.plugins.e2e.E2EPlugin"));
   }
 }
