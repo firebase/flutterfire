78511714c3dc7f16c8aa8ed10ec8e1dc4779349f
Arthur Thompson <arthurthompson@google.com>
2020-08-06 08:48:45 -0700

    chore(firebase_in_app_messaging): migrate to new `firebase_core` plugin (#3102)
    Co-authored-by: Mike Diarmid <mike.diarmid@gmail.com>

--- packages/firebase_in_app_messaging/android/src/main/java/io/flutter/plugins/firebase/inappmessaging/FirebaseInAppMessagingPlugin.java
+++ packages/firebase_in_app_messaging/android/src/main/java/io/flutter/plugins/firebase/inappmessaging/FirebaseInAppMessagingPlugin.java
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package com.example.firebase_in_app_messaging;
+package io.flutter.plugins.firebase.inappmessaging;
 
 import com.google.firebase.inappmessaging.FirebaseInAppMessaging;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
