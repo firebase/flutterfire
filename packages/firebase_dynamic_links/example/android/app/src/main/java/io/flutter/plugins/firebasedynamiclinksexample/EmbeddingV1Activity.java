9a733b82142f82a7e86e296c930c0ed08f18f42c
Arthur Thompson <arthurthompson@google.com>
2020-08-06 07:33:18 -0700

    fix formatting

--- packages/firebase_dynamic_links/example/android/app/src/main/java/io/flutter/plugins/firebasedynamiclinksexample/EmbeddingV1Activity.java
+++ packages/firebase_dynamic_links/example/android/app/src/main/java/io/flutter/plugins/firebasedynamiclinksexample/EmbeddingV1Activity.java
@@ -4,17 +4,16 @@ import android.os.Bundle;
 import dev.flutter.plugins.e2e.E2EPlugin;
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.firebase.core.FlutterFirebaseCorePlugin;
-import io.flutter.plugins.firebase.inappmessaging.FirebaseInAppMessagingPlugin;
 
 public class EmbeddingV1Activity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     FlutterFirebaseCorePlugin.registerWith(
-      registrarFor("io.flutter.plugins.firebase.core.FlutterFirebaseCorePlugin"));
+        registrarFor("io.flutter.plugins.firebase.core.FlutterFirebaseCorePlugin"));
     // TODO(Salakar) rename as part of re-work FirebaseDynamicLinksPlugin -> FlutterFirebaseDynamicLinksPlugin
     FirebaseDynamicLinksPlugin.registerWith(
-      registrarFor("io.flutter.plugins.firebasedynamiclinks.FirebaseDynamicLinksPlugin"));
+        registrarFor("io.flutter.plugins.firebasedynamiclinks.FirebaseDynamicLinksPlugin"));
     E2EPlugin.registerWith(registrarFor("dev.flutter.plugins.e2e.E2EPlugin"));
   }
 }
