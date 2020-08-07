717c9ac08782da29b6635d2dc16b8809425ceb00
Polina Govorkova <catchingthegist@gmail.com>
2020-08-07 13:56:46 -0400

    [firebase_ml_custom] Final adjustments before the initial release (#3121)
    * Documentation, package names and dependency boundary changes
    
    * Added try-catch in the main
    
    * Added slash before the model name and changed some vars to finals
    
    * Changed simple comments to dartdoc

--- packages/firebase_ml_custom/example/lib/main.dart
+++ packages/firebase_ml_custom/example/lib/main.dart
@@ -19,7 +19,7 @@ void main() {
   );
 }
 
-/// Widget with a future function that initiates actions from FirebaseML
+/// Widget with a future function that initiates actions from FirebaseML.
 class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
@@ -29,61 +29,99 @@ class _MyAppState extends State<MyApp> {
   final picker = ImagePicker();
   File _image;
   List<Map<dynamic, dynamic>> _labels;
+  //When the model is ready, _loaded changes to trigger the screen state change.
   Future<String> _loaded = loadModel();
 
+  /// Triggers selection of an image and the consequent inference.
   Future<void> getImageLabels() async {
-    final pickedFile = await picker.getImage(source: ImageSource.gallery);
-    final image = File(pickedFile.path);
-    if (image == null) {
-      return;
+    try {
+      final pickedFile = await picker.getImage(source: ImageSource.gallery);
+      final image = File(pickedFile.path);
+      if (image == null) {
+        return;
+      }
+      var labels = List<Map>.from(await Tflite.runModelOnImage(
+        path: image.path,
+        imageStd: 127.5,
+      ));
+      setState(() {
+        _labels = labels;
+        _image = image;
+      });
     }
-    var labels = List<Map>.from(await Tflite.runModelOnImage(
-      path: image.path,
-      imageStd: 127.5,
-    ));
-    setState(() {
-      _labels = labels;
-      _image = image;
-    });
+    catch (exception) {
+      print("Failed on getting your image and it's labels: $exception");
+      print('Continuing with the program...');
+      rethrow;
+    }
   }
 
+  /// Gets the model ready for inference on images.
   static Future<String> loadModel() async {
-    var modelFile = await loadModelFromFirebase();
+    final modelFile = await loadModelFromFirebase();
     return await loadTFLiteModel(modelFile);
   }
 
+  /// Downloads custom model from the Firebase console and return its file.
+  /// located on the mobile device.
   static Future<File> loadModelFromFirebase() async {
-    var model = FirebaseCustomRemoteModel('mobilenet_v1_1_0_224');
+    try {
+      // Create model with a name that is specified in the Firebase console
+      final model = FirebaseCustomRemoteModel('mobilenet_v1_1_0_224');
 
-    var conditions = FirebaseModelDownloadConditions(androidRequireWifi: true);
-    var modelManager = FirebaseModelManager.instance;
+      // Specify conditions when the model can be downloaded.
+      // If there is no wifi access when the app is started,
+      // this app will continue loading until the conditions are satisfied.
+      final conditions = FirebaseModelDownloadConditions(
+          androidRequireWifi: true, iosAllowCellularAccess: false);
 
-    await modelManager.download(model, conditions);
-    assert(await modelManager.isModelDownloaded(model) == true);
+      // Create model manager associated with default Firebase App instance.
+      final modelManager = FirebaseModelManager.instance;
 
-    var modelFile = await modelManager.getLatestModelFile(model);
-    assert(modelFile != null);
-    return modelFile;
+      // Begin downloading and wait until the model is downloaded successfully.
+      await modelManager.download(model, conditions);
+      assert(await modelManager.isModelDownloaded(model) == true);
+
+      // Get latest model file to use it for inference by the interpreter.
+      var modelFile = await modelManager.getLatestModelFile(model);
+      assert(modelFile != null);
+      return modelFile;
+    }
+    catch (exception) {
+      print('Failed on loading your model from Firebase: $exception');
+      print('The program will not be resumed');
+      rethrow;
+    }
   }
 
+  /// Loads the model into some TF Lite interpreter.
+  /// In this case interpreter provided by tflite plugin.
   static Future<String> loadTFLiteModel(File modelFile) async {
-    var appDirectory = await getApplicationDocumentsDirectory();
-    var labelsData =
-        await rootBundle.load("assets/labels_mobilenet_v1_224.txt");
-    var labelsFile =
-        await File(appDirectory.path + "_labels_mobilenet_v1_224.txt")
-            .writeAsBytes(labelsData.buffer.asUint8List(
-                labelsData.offsetInBytes, labelsData.lengthInBytes));
+    try{
+      final appDirectory = await getApplicationDocumentsDirectory();
+      final labelsData =
+          await rootBundle.load("assets/labels_mobilenet_v1_224.txt");
+      final labelsFile =
+          await File(appDirectory.path + "/_labels_mobilenet_v1_224.txt")
+              .writeAsBytes(labelsData.buffer.asUint8List(
+                  labelsData.offsetInBytes, labelsData.lengthInBytes));
 
-    assert(await Tflite.loadModel(
-          model: modelFile.path,
-          labels: labelsFile.path,
-          isAsset: false,
-        ) ==
-        "success");
-    return "Model is loaded";
+      assert(await Tflite.loadModel(
+            model: modelFile.path,
+            labels: labelsFile.path,
+            isAsset: false,
+          ) ==
+          "success");
+      return "Model is loaded";
+    }
+    catch (exception) {
+      print('Failed on loading your model to the TFLite interpreter: $exception');
+      print('The program will not be resumed');
+      rethrow;
+    }
   }
 
+  /// Shows image selection screen only when the model is ready to be used.
   Widget readyScreen() {
     return Scaffold(
       appBar: AppBar(
@@ -110,22 +148,36 @@ class _MyAppState extends State<MyApp> {
     );
   }
 
+  /// In case of error shows unrecoverable error screen.
   Widget errorScreen() {
     return Scaffold(
       body: Center(
-        child: Text("Error loading model. Sorry about that :("),
+        child: Text("Error loading model. Please check the logs."),
       ),
     );
   }
 
+  /// In case of long loading shows loading screen until model is ready or
+  /// error is received.
   Widget loadingScreen() {
     return Scaffold(
       body: Center(
-        child: CircularProgressIndicator(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20.0),
+              child: CircularProgressIndicator(),
+            ),
+            Text("Please make sure that you are using wifi."),
+          ],
+        ),
       ),
     );
   }
 
+  /// Shows different screens based on the state of the custom model.
   Widget build(BuildContext context) {
     return DefaultTextStyle(
       style: Theme.of(context).textTheme.headline2,
