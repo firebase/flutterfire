90cf4ed892e020290a1186b19b99e166fbbf5d4c
Salakar <mike.diarmid@gmail.com>
2020-08-26 15:34:57 +0100

    tests: disable macOS workflows
    Issue with flutter dev branch at the moment causing CI failures for macOS.

--- .github/workflows/scripts/build-example.sh
+++ .github/workflows/scripts/build-example.sh
@@ -20,6 +20,9 @@ fi
 
 if [ "$ACTION" == "macos" ]
 then
+  # TODO Flutter dev branch is currently broken so we're unable to test MacOS.
+  echo "TODO: Skipping macOS testing due to Flutter dev branch issue."
+  exit
   melos exec -c 1 --scope="$FLUTTERFIRE_PLUGIN_SCOPE_EXAMPLE" -- \
     flutter build macos --debug --target=./test_driver/MELOS_PARENT_PACKAGE_NAME_e2e.dart
   exit
