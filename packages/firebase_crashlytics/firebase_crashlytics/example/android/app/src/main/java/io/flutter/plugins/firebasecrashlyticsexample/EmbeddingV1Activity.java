26e7ecce7bc753b9c1196f18b93a53d0210abab0
Salakar <mike.diarmid@gmail.com>
2020-09-02 13:58:33 +0100

    refactor(example): update android example to flutterEmbedding v2

--- packages/firebase_crashlytics/firebase_crashlytics/example/android/app/src/main/java/io/flutter/plugins/firebasecrashlyticsexample/EmbeddingV1Activity.java
+++ packages/firebase_crashlytics/firebase_crashlytics/example/android/app/src/main/java/io/flutter/plugins/firebasecrashlyticsexample/EmbeddingV1Activity.java
@@ -2,12 +2,18 @@ package io.flutter.plugins.firebasecrashlyticsexample;
 
 import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
-import io.flutter.plugins.GeneratedPluginRegistrant;
+import dev.flutter.plugins.e2e.E2EPlugin;
+import io.flutter.plugins.firebase.core.FlutterFirebaseCorePlugin;
+import io.flutter.plugins.firebase.crashlytics.FlutterFirebaseCrashlyticsPlugin;
 
 public class EmbeddingV1Activity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
+    FlutterFirebaseCorePlugin.registerWith(
+      registrarFor("io.flutter.plugins.firebase.core.FlutterFirebaseCorePlugin"));
+    FlutterFirebaseCrashlyticsPlugin.registerWith(
+      registrarFor("io.flutter.plugins.firebase.crashlytics.FlutterFirebaseCrashlyticsPlugin"));
+    E2EPlugin.registerWith(registrarFor("dev.flutter.plugins.e2e.E2EPlugin"));
   }
 }
