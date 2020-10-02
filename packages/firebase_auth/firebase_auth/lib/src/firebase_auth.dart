8fc2b2b8bf149d495e795ee1a920cd8f06622d99
russellwheatley <russellwheatley85@gmail.com>
2020-10-02 16:41:17 +0100

    fix(firebase_auth): fix auth listeners

--- packages/firebase_auth/firebase_auth/lib/src/firebase_auth.dart
+++ packages/firebase_auth/firebase_auth/lib/src/firebase_auth.dart
@@ -19,9 +19,10 @@ class FirebaseAuth extends FirebasePluginPlatform {
   /// If called and no [_delegatePackingProperty] exists, it will first be
   /// created and assigned before returning the delegate.
   FirebaseAuthPlatform get _delegate {
-    _delegatePackingProperty = FirebaseAuthPlatform.instanceFor(
-        app: app, pluginConstants: pluginConstants);
-
+    if (_delegatePackingProperty == null) {
+      _delegatePackingProperty = FirebaseAuthPlatform.instanceFor(
+          app: app, pluginConstants: pluginConstants);
+    }
     return _delegatePackingProperty;
   }
 
