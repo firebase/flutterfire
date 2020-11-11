872449fe186fc1eb32b862579f38312fffe74aca
ehesp <elliot.hesp@gmail.com>
2020-11-11 14:41:48 +0000

    feat: add FirebaseException.stackTrace support

--- packages/firebase_core/firebase_core_platform_interface/lib/src/firebase_exception.dart
+++ packages/firebase_core/firebase_core_platform_interface/lib/src/firebase_exception.dart
@@ -10,7 +10,7 @@ part of firebase_core_platform_interface;
 /// ```dart
 /// try {
 ///   await Firebase.initializeApp();
-/// } catch (e) {
+/// } on FirebaseException catch (e) {
 ///   print(e.toString());
 /// }
 /// ```
@@ -26,7 +26,10 @@ class FirebaseException implements Exception {
   /// }
   /// ```
   FirebaseException(
-      {@required this.plugin, @required this.message, this.code = 'unknown'});
+      {@required this.plugin,
+      @required this.message,
+      this.code = 'unknown',
+      this.stackTrace});
 
   /// The plugin the exception is for.
   ///
@@ -44,20 +47,30 @@ class FirebaseException implements Exception {
   /// not exist.
   final String code;
 
+  /// The stack trace which provides information to the user about the call
+  /// sequence that triggered an exception
+  final StackTrace stackTrace;
+
   @override
   bool operator ==(dynamic other) {
     if (identical(this, other)) return true;
     if (other is! FirebaseException) return false;
-    return other.toString() == this.toString();
+    return other.hashCode == hashCode;
   }
 
   @override
   int get hashCode {
-    return this.toString().hashCode;
+    return hash3(plugin, code, message);
   }
 
   @override
   String toString() {
-    return "[$plugin/$code] $message";
+    String output = "[$plugin/$code] $message";
+
+    if (stackTrace != null) {
+      output += "\n\n${stackTrace.toString()}";
+    }
+
+    return output;
   }
 }
