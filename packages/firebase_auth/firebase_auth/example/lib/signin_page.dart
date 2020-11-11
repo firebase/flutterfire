777b865d7809c3ea18e7e7332db97518d24ab4c2
Salakar <mike.diarmid@gmail.com>
2020-11-11 14:42:47 +0000

    refactor: remove unused import

--- packages/firebase_auth/firebase_auth/example/lib/signin_page.dart
+++ packages/firebase_auth/firebase_auth/example/lib/signin_page.dart
@@ -7,7 +7,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
-import 'dart:collection';
 
 final FirebaseAuth _auth = FirebaseAuth.instance;
 
