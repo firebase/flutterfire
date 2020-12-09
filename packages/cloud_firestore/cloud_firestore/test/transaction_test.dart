a0201040d23e1839e8d325bf39d08e19a8c34d15
ehesp <elliot.hesp@gmail.com>
2020-12-09 12:31:33 +0000

    fix analyzer warnings

--- packages/cloud_firestore/cloud_firestore/test/transaction_test.dart
+++ packages/cloud_firestore/cloud_firestore/test/transaction_test.dart
@@ -1,4 +1,4 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_test/flutter_test.dart';
 
