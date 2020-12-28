31459cfcdf35c5f7d030a22f53e8e57092b0ffbc
Arthur Thompson <arthurthompson@google.com>
2020-12-28 15:35:08 -0800

    refactor: Add platform exception conversion to Firebase Exception

---
+++ packages/firebase_remote_config/firebase_remote_config_platform_interface/lib/src/method_channel/utils/exception.dart
@@ -0,0 +1,35 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/services.dart';
+
+/// Catches a [PlatformException] and converts it into a [FirebaseException] if
+/// it was intentionally caught on the native platform.
+Exception convertPlatformException(Object exception) {
+  if (exception is! Exception || exception is! PlatformException) {
+    throw exception;
+  }
+
+  return platformExceptionToFirebaseException(exception as PlatformException);
+}
+
+/// Converts a [PlatformException] into a [FirebaseException].
+///
+/// A [PlatformException] can only be converted to a [FirebaseException] if the
+/// `details` of the exception exist. Firebase returns specific codes and messages
+/// which can be converted into user friendly exceptions.
+FirebaseException platformExceptionToFirebaseException(
+    PlatformException platformException) {
+  Map<String, String> details = platformException.details != null
+      ? Map<String, String>.from(platformException.details)
+      : null;
+
+  String code = 'unknown';
+  String message = platformException.message;
+
+  if (details != null) {
+    code = details['code'] ?? code;
+    message = details['message'] ?? message;
+  }
+
+  return FirebaseException(
+      plugin: 'firebase_remote_config', code: code, message: message);
+}
