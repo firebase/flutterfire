13fe1f5a73082104cbde9c2b9d12bf4ead32d457
Salakar <mike.diarmid@gmail.com>
2021-01-04 19:09:52 +0000

    -

--- packages/cloud_firestore/cloud_firestore_platform_interface/lib/src/method_channel/utils/firestore_message_codec.dart
+++ packages/cloud_firestore/cloud_firestore_platform_interface/lib/src/method_channel/utils/firestore_message_codec.dart
@@ -2,15 +2,19 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+// TODO required for nullsafety - remove ignore after migration
+// ignore: unused_import
+import 'dart:typed_data' show Uint8List;
+
 import 'package:cloud_firestore_platform_interface/cloud_firestore_platform_interface.dart';
+import 'package:cloud_firestore_platform_interface/src/internal/field_path_type.dart';
+import 'package:cloud_firestore_platform_interface/src/method_channel/method_channel_field_value.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
-import 'package:cloud_firestore_platform_interface/src/internal/field_path_type.dart';
-import 'package:cloud_firestore_platform_interface/src/method_channel/method_channel_field_value.dart';
 
-import '../method_channel_query.dart';
 import '../method_channel_firestore.dart';
+import '../method_channel_query.dart';
 
 /// The codec utilized to encode data back and forth between
 /// the Dart application and the native platform.
