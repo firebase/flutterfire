02965763818d0bfaf036781807d5f18655cc2c7b
Russell Wheatley <russellwheatley85@gmail.com>
2021-01-07 18:04:30 +0000

    refactor(firestore_auth)!: remove deprecated APIs (#4590)

--- packages/firebase_auth/firebase_auth_platform_interface/lib/src/action_code_settings.dart
+++ packages/firebase_auth/firebase_auth_platform_interface/lib/src/action_code_settings.dart
@@ -10,26 +10,14 @@ class ActionCodeSettings {
   // ignore: public_member_api_docs
   @protected
   ActionCodeSettings({
-    @Deprecated("Deprecated in favor of using named args instead ([androidPackageName], [androidMinimumVersion], [androidInstallApp])")
-        // ignore: deprecated_member_use_from_same_package
-        this.android,
     this.androidPackageName,
     this.androidMinimumVersion,
     this.androidInstallApp,
     this.dynamicLinkDomain,
     this.handleCodeInApp,
-    @Deprecated("Deprecated in favor of using named args instead ([iOSBundleId])")
-        // ignore: deprecated_member_use_from_same_package
-        this.iOS,
     this.iOSBundleId,
-    /*required*/ @required
-        this.url,
+    /*required*/ @required this.url,
   }) : assert(url != null);
-
-  @Deprecated(
-      "Deprecated in favor of using named args instead ([androidPackageName], [androidMinimumVersion], [androidInstallApp])")
-  // ignore: public_member_api_docs
-  Map<String, dynamic> /*?*/ android;
 
   /// The Android package name of the application to open when the URL is pressed.
   final String androidPackageName;
@@ -56,54 +44,19 @@ class ActionCodeSettings {
   /// app if installed.
   final bool handleCodeInApp;
 
-  @Deprecated("Deprecated in favor of using named args instead ([iOSBundleId])")
-  // ignore: public_member_api_docs
-  Map<String, dynamic> iOS;
-
   /// Sets the link continue/state URL
   final String url;
 
   /// Returns the current instance as a [Map].
   Map<String, dynamic> asMap() {
-    // ignore: deprecated_member_use_from_same_package
-    android ??= {};
-    // ignore: deprecated_member_use_from_same_package
-    iOS ??= {};
-
-    Map<String, dynamic> androidMap;
-    Map<String, dynamic> iOSMap;
-
-    // ignore: deprecated_member_use_from_same_package
-    if (androidPackageName != null || android['packageName'] != null) {
-      androidMap = {};
-      androidMap['packageName'] =
-          // ignore: deprecated_member_use_from_same_package
-          androidPackageName ?? android['packageName'].toString();
-      androidMap['minimumVersion'] =
-          // ignore: deprecated_member_use_from_same_package
-          androidMinimumVersion ?? android['minimumVersion']?.toString();
-      androidMap['installApp'] = androidInstallApp;
-
-      // ignore: deprecated_member_use_from_same_package
-      if (androidMap['installApp'] == null && android['installApp'] is bool) {
-        // ignore: deprecated_member_use_from_same_package
-        androidMap['installApp'] = android['installApp'];
-      }
-    }
-
-    // ignore: deprecated_member_use_from_same_package
-    if (iOSBundleId != null || iOS['bundleId'] != null) {
-      iOSMap = {};
-      // ignore: deprecated_member_use_from_same_package
-      iOSMap['bundleId'] = iOSBundleId ?? iOS['bundleId'].toString();
-    }
-
     return <String, dynamic>{
       'url': url,
       'dynamicLinkDomain': dynamicLinkDomain,
       'handleCodeInApp': handleCodeInApp,
-      'android': androidMap,
-      'iOS': iOSMap,
+      'androidPackageName': androidPackageName,
+      'androidMinimumVersion': androidMinimumVersion,
+      'androidInstallApp': androidInstallApp,
+      'iOSBundleId': iOSBundleId,
     };
   }
 
