560cc3bf914518852f04a7f55e4bf6107ad83a91
Salakar <mike.diarmid@gmail.com>
2021-01-16 13:16:20 +0000

    ci: suppress analytics reporting globally

--- .github/workflows/scripts/install-tools.sh
+++ .github/workflows/scripts/install-tools.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+flutter config --no-analytics
 flutter pub global activate melos
 echo "$HOME/.pub-cache/bin" >> $GITHUB_PATH
 echo "$GITHUB_WORKSPACE/_flutter/.pub-cache/bin" >> $GITHUB_PATH
