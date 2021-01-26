d9a82247b50537015f34fdbd61fd45b1ad9182af
Russell Wheatley <russellwheatley85@gmail.com>
2021-01-26 16:03:58 +0000

    chore: no melos version install (#4817)

--- .github/workflows/scripts/install-tools.sh
+++ .github/workflows/scripts/install-tools.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 flutter config --no-analytics
-flutter pub global activate melos 0.4.0-dev.2
+flutter pub global activate melos
 echo "$HOME/.pub-cache/bin" >> $GITHUB_PATH
 echo "$GITHUB_WORKSPACE/_flutter/.pub-cache/bin" >> $GITHUB_PATH
 echo "$GITHUB_WORKSPACE/_flutter/bin/cache/dart-sdk/bin" >> $GITHUB_PATH
