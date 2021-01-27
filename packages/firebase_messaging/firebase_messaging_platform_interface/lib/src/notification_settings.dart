20ddeeab6c7e611a41ef4b0ae6d83c6fb47260d0
ehesp <elliot.hesp@gmail.com>
2021-01-27 12:25:21 +0000

    apply platform_interface_messaging nndb migration

--- packages/firebase_messaging/firebase_messaging_platform_interface/lib/src/notification_settings.dart
+++ packages/firebase_messaging/firebase_messaging_platform_interface/lib/src/notification_settings.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// @dart=2.9
+
 
 import 'package:firebase_messaging_platform_interface/firebase_messaging_platform_interface.dart';
 
@@ -10,61 +10,61 @@ import 'package:firebase_messaging_platform_interface/firebase_messaging_platfor
 class NotificationSettings {
   // ignore: public_member_api_docs
   const NotificationSettings(
-      {this.alert,
-      this.announcement,
-      this.authorizationStatus,
-      this.badge,
-      this.carPlay,
-      this.lockScreen,
-      this.notificationCenter,
-      this.showPreviews,
-      this.sound});
+      {required this.alert,
+      required this.announcement,
+      required this.authorizationStatus,
+      required this.badge,
+      required this.carPlay,
+      required this.lockScreen,
+      required this.notificationCenter,
+      required this.showPreviews,
+      required this.sound});
 
   /// Whether or not messages containing a notification will alert the user.
   ///
   /// Apple devices only.
-  final AppleNotificationSetting/*!*/ alert;
+  final AppleNotificationSetting alert;
 
   /// Whether or not messages containing a notification be announced to the user
   /// via 3rd party services such as Siri.
   ///
   /// Apple devices only.
-  final AppleNotificationSetting/*!*/ announcement;
+  final AppleNotificationSetting announcement;
 
   /// The overall notification authorization status for the user.
-  final AuthorizationStatus/*!*/ authorizationStatus;
+  final AuthorizationStatus authorizationStatus;
 
   /// Whether or not messages containing a notification can update the application badge.
   ///
   /// Apple devices only.
-  final AppleNotificationSetting/*!*/ badge;
+  final AppleNotificationSetting badge;
 
   /// Whether or not messages containing a notification will be displayed in a
   /// CarPlay environment.
   ///
   /// Apple devices only.
-  final AppleNotificationSetting/*!*/ carPlay;
+  final AppleNotificationSetting carPlay;
 
   /// Whether or not messages containing a notification will be displayed on the
   /// device lock screen.
   ///
   /// Apple devices only.
-  final AppleNotificationSetting/*!*/ lockScreen;
+  final AppleNotificationSetting lockScreen;
 
   /// Whether or not messages containing a notification will be displayed in the
   /// device notification center.
   ///
   /// Apple devices only.
-  final AppleNotificationSetting/*!*/ notificationCenter;
+  final AppleNotificationSetting notificationCenter;
 
   /// Whether or not messages containing a notification can displayed a previewed
   /// version to users.
   ///
   /// Apple devices only.
-  final AppleShowPreviewSetting/*!*/ showPreviews;
+  final AppleShowPreviewSetting showPreviews;
 
   /// Whether or not messages containing a notification will trigger a sound.
   ///
   /// Apple devices only.
-  final AppleNotificationSetting/*!*/ sound;
+  final AppleNotificationSetting sound;
 }
