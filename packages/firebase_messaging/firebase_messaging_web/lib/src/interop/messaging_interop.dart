5782db673d368e27f020a700e361d4dc84522c5b
ehesp <elliot.hesp@gmail.com>
2021-01-27 12:37:45 +0000

    apply firebase_messaging_web nndb migration

--- packages/firebase_messaging/firebase_messaging_web/lib/src/interop/messaging_interop.dart
+++ packages/firebase_messaging/firebase_messaging_web/lib/src/interop/messaging_interop.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// @dart=2.9
+
 
 // ignore_for_file: public_member_api_docs
 
@@ -28,24 +28,24 @@ abstract class MessagingJsImpl {
 @JS()
 @anonymous
 abstract class NotificationPayloadJsImpl {
-  external String get title;
-  external String get body;
-  external String get image;
+  external String? get title;
+  external String? get body;
+  external String? get image;
 }
 
 @JS()
 @anonymous
 abstract class MessagePayloadJsImpl {
-  external String get collapseKey;
-  external FcmOptionsJsImpl get fcmOptions;
-  external NotificationPayloadJsImpl get notification;
+  external String? get collapseKey;
+  external FcmOptionsJsImpl? get fcmOptions;
+  external NotificationPayloadJsImpl? get notification;
   external dynamic /*Map<String, String>*/ get data;
-  external String get from;
+  external String? get from;
 }
 
 @JS()
 @anonymous
 abstract class FcmOptionsJsImpl {
-  external String get analyticsLabel;
-  external String get link;
+  external String? get analyticsLabel;
+  external String? get link;
 }
