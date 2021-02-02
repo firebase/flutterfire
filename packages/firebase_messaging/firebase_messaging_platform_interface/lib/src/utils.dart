1473a1e31dccbad22c1d925bbb08ea4fa62e8495
ehesp <elliot.hesp@gmail.com>
2021-02-02 09:31:07 +0000

    run formatter

--- packages/firebase_messaging/firebase_messaging_platform_interface/lib/src/utils.dart
+++ packages/firebase_messaging/firebase_messaging_platform_interface/lib/src/utils.dart
@@ -2,12 +2,11 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-
-
 import 'package:firebase_messaging_platform_interface/firebase_messaging_platform_interface.dart';
 
 /// Converts an [int] into it's [AndroidNotificationPriority] representation.
-AndroidNotificationPriority convertToAndroidNotificationPriority(int? priority) {
+AndroidNotificationPriority convertToAndroidNotificationPriority(
+    int? priority) {
   switch (priority) {
     case -2:
       return AndroidNotificationPriority.minimumPriority;
