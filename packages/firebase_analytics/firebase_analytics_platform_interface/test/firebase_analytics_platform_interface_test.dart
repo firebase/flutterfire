76ec16d47eac97ae1f2e0eddf96d23f88ab6360f
Dennis Kugelmann <dennis.kugelmann@simpleclub.com>
2021-03-14 13:15:51 +0100

    Fix platform interface tests

--- packages/firebase_analytics/firebase_analytics_platform_interface/test/firebase_analytics_platform_interface_test.dart
+++ packages/firebase_analytics/firebase_analytics_platform_interface/test/firebase_analytics_platform_interface_test.dart
@@ -2,12 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-
-
 import 'package:firebase_analytics_platform_interface/firebase_analytics_platform_interface.dart';
 import 'package:firebase_analytics_platform_interface/method_channel_firebase_analytics.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/mockito.dart';
 
 void main() {
   group('$FirebaseAnalyticsPlatform', () {
@@ -19,7 +16,7 @@ void main() {
     test('Cannot be implemented with `implements`', () {
       expect(() {
         FirebaseAnalyticsPlatform.instance =
-            ImplementsFirebaseAnalyticsPlatform();
+            ImplementsFirebaseAnalyticsPlatform(false);
       }, throwsAssertionError);
     });
 
@@ -29,14 +26,27 @@ void main() {
 
     test('Can be mocked with `implements`', () {
       final ImplementsFirebaseAnalyticsPlatform mock =
-          ImplementsFirebaseAnalyticsPlatform();
-      when(mock.isMock).thenReturn(true);
+          ImplementsFirebaseAnalyticsPlatform(true);
       FirebaseAnalyticsPlatform.instance = mock;
     });
   });
 }
 
+class Mock {
+  @override
+  dynamic noSuchMethod(Invocation invocation) {
+    return super.noSuchMethod(invocation);
+  }
+}
+
 class ImplementsFirebaseAnalyticsPlatform extends Mock
-    implements FirebaseAnalyticsPlatform {}
+    implements FirebaseAnalyticsPlatform {
+  ImplementsFirebaseAnalyticsPlatform(this._isMock);
+
+  bool _isMock;
+
+  @override
+  bool get isMock => _isMock;
+}
 
 class ExtendsFirebaseAnalyticsPlatform extends FirebaseAnalyticsPlatform {}
