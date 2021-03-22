399a5f757cbabd3f006417f9874bb620ef8f4f61
Remi Rousselet <darky12s@gmail.com>
2021-03-22 11:38:47 +0000

    fix NNBD error

--- packages/cloud_firestore/cloud_firestore/example/test_driver/document_change_e2e.dart
+++ packages/cloud_firestore/cloud_firestore/example/test_driver/document_change_e2e.dart
@@ -93,7 +93,7 @@ void runDocumentChangeTests() {
           expect(change.newIndex, equals(0));
           expect(change.oldIndex, equals(-1));
           expect(change.type, equals(DocumentChangeType.added));
-          expect(change.doc.data()['name'], equals('doc1'));
+          expect(change.doc.data()!['name'], equals('doc1'));
         } else if (call == 2) {
           expect(snapshot.docs.length, equals(0));
           expect(snapshot.docChanges.length, equals(1));
@@ -102,7 +102,7 @@ void runDocumentChangeTests() {
           expect(change.newIndex, equals(-1));
           expect(change.oldIndex, equals(0));
           expect(change.type, equals(DocumentChangeType.removed));
-          expect(change.doc.data()['name'], equals('doc1'));
+          expect(change.doc.data()!['name'], equals('doc1'));
         } else {
           fail('Should not have been called');
         }
@@ -139,7 +139,7 @@ void runDocumentChangeTests() {
             expect(change.oldIndex, equals(-1));
             expect(change.newIndex, equals(index));
             expect(change.type, equals(DocumentChangeType.added));
-            expect(change.doc.data()['value'], equals(index + 1));
+            expect(change.doc.data()!['value'], equals(index + 1));
           });
         } else if (call == 2) {
           expect(snapshot.docs.length, equals(3));
