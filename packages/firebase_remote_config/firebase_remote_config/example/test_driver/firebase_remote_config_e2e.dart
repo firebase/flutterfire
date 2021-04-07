35a3229a69eeab3244896d2a372b8cbf635e8d7f
Remi Rousselet <darky12s@gmail.com>
2021-04-07 15:23:21 +0100

    fix import

--- packages/firebase_remote_config/firebase_remote_config/example/test_driver/firebase_remote_config_e2e.dart
+++ packages/firebase_remote_config/firebase_remote_config/example/test_driver/firebase_remote_config_e2e.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:integration_test/integration_test_driver.dart';
+import 'package:integration_test/integration_test.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_remote_config/firebase_remote_config.dart';
 
