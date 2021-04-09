e12f88ab0b6bf9e9f786b8f7e4846bb915da90ad
russellwheatley <russellwheatley85@gmail.com>
2021-04-09 13:11:05 +0100

    test(functions): add integration test

--- .github/workflows/scripts/functions/src/index.ts
+++ .github/workflows/scripts/functions/src/index.ts
@@ -1,105 +1,104 @@
 import * as assert from "assert";
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
 
 // For example app.
 // noinspection JSUnusedGlobalSymbols
 export const listFruit = functions.https.onCall(() => {
-  return [
-    "Apple",
-    "Banana",
-    "Cherry",
-    "Date",
-    "Fig",
-    "Grapes"
-  ];
+  return ["Apple", "Banana", "Cherry", "Date", "Fig", "Grapes"];
 });
-
 
 // For e2e testing a custom region.
 // noinspection JSUnusedGlobalSymbols
 export const testFunctionCustomRegion = functions
-  .region('europe-west1')
-  .https.onCall(() => 'europe-west1');
+  .region("europe-west1")
+  .https.onCall(() => "europe-west1");
 
 // For e2e testing timeouts.
-export const testFunctionTimeout = functions
-  .https.onCall(data => {
-    console.log(JSON.stringify({data}));
-    return new Promise((resolve, reject) => {
-      if (data && data.testTimeout) {
-        setTimeout(() => resolve({timeLimit: 'exceeded'}), parseInt(data.testTimeout, 10));
-      } else {
-        reject(new functions.https.HttpsError(
-          'invalid-argument',
-          'testTimeout must be provided.'
-        ));
-      }
-    });
+export const testFunctionTimeout = functions.https.onCall((data) => {
+  console.log(JSON.stringify({ data }));
+  return new Promise((resolve, reject) => {
+    if (data && data.testTimeout) {
+      setTimeout(
+        () => resolve({ timeLimit: "exceeded" }),
+        parseInt(data.testTimeout, 10)
+      );
+    } else {
+      reject(
+        new functions.https.HttpsError(
+          "invalid-argument",
+          "testTimeout must be provided."
+        )
+      );
+    }
   });
+});
 
 // For e2e testing errors & return values.
 // noinspection JSUnusedGlobalSymbols
-export const testFunctionDefaultRegion = functions.https.onCall(data => {
-  console.log(JSON.stringify({data}));
-  if (typeof data === 'undefined') {
-    return 'undefined';
+export const testFunctionDefaultRegion = functions.https.onCall((data) => {
+  console.log(JSON.stringify({ data }));
+  if (typeof data === "undefined") {
+    return "undefined";
   }
 
-  if (typeof data === 'string') {
-    return 'string';
+  if (typeof data === "string") {
+    return "string";
   }
 
-  if (typeof data === 'number') {
-    return 'number';
+  if (typeof data === "number") {
+    return "number";
   }
 
-  if (typeof data === 'boolean') {
-    return 'boolean';
+  if (typeof data === "boolean") {
+    return "boolean";
   }
 
   if (data === null) {
-    return 'null';
+    return "null";
   }
 
   if (Array.isArray(data)) {
-    return 'array';
+    return "array";
   }
 
   const sampleData: {
-    [key: string]: any
+    [key: string]: any;
   } = {
     number: 1234,
-    string: 'acde',
+    string: "acde",
     boolean: true,
-    'null': null,
+    null: null,
     map: {
       number: 1234,
-      string: 'acde',
+      string: "acde",
       boolean: true,
       null: null,
     },
-    list: [1234, 'acde', true, null],
+    list: [1234, "acde", true, null],
     deepMap: {
       number: 123,
-      string: 'foo',
+      string: "foo",
       booleanTrue: true,
       booleanFalse: false,
       null: null,
-      'list': ['1', 2, true, false],
-      'map': {
+      list: ["1", 2, true, false],
+      map: {
         number: 123,
-        string: 'foo',
+        string: "foo",
         booleanTrue: true,
         booleanFalse: false,
         null: null,
       },
     },
     deepList: [
-      '1', 2, true, false,
-      ['1', 2, true, false],
+      "1",
+      2,
+      true,
+      false,
+      ["1", 2, true, false],
       {
         number: 123,
-        string: 'foo',
+        string: "foo",
         booleanTrue: true,
         booleanFalse: false,
         null: null,
@@ -107,15 +106,19 @@ export const testFunctionDefaultRegion = functions.https.onCall(data => {
     ],
   };
 
-  const {type, asError, inputData}: {
-    type: string,
-    asError?: boolean,
-    inputData?: any,
+  const {
+    type,
+    asError,
+    inputData,
+  }: {
+    type: string;
+    asError?: boolean;
+    inputData?: any;
   } = data;
   if (!Object.hasOwnProperty.call(sampleData, type)) {
     throw new functions.https.HttpsError(
-      'invalid-argument',
-      'Invalid test requested.'
+      "invalid-argument",
+      "Invalid test requested."
     );
   }
 
@@ -126,8 +129,8 @@ export const testFunctionDefaultRegion = functions.https.onCall(data => {
   } catch (e) {
     console.error(e);
     throw new functions.https.HttpsError(
-      'invalid-argument',
-      'Input and Output types did not match.',
+      "invalid-argument",
+      "Input and Output types did not match.",
       e.message
     );
   }
@@ -135,8 +138,8 @@ export const testFunctionDefaultRegion = functions.https.onCall(data => {
   // all good
   if (asError) {
     throw new functions.https.HttpsError(
-      'cancelled',
-      'Response data was requested to be sent as part of an Error payload, so here we are!',
+      "cancelled",
+      "Response data was requested to be sent as part of an Error payload, so here we are!",
       outputData
     );
   }
@@ -144,3 +147,6 @@ export const testFunctionDefaultRegion = functions.https.onCall(data => {
   return outputData;
 });
 
+export const testMapConvertType = functions.https.onCall((data) => ({
+  foo: "bar",
+}));
