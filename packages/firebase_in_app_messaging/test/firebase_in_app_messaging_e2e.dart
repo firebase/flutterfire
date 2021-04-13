ce2f728236d40e382066877d89af23f2d79bc40c
Alan Trope <atrope@users.noreply.github.com>
2021-04-13 18:34:39 +0300

    feat: migrate in_app_messaging to sound null safety (#5676)
    Co-authored-by: Remi Rousselet <darky12s@gmail.com>

--- packages/firebase_in_app_messaging/test/firebase_in_app_messaging_e2e.dart
+++ packages/firebase_in_app_messaging/test/firebase_in_app_messaging_e2e.dart
@@ -1,12 +1,10 @@
-// @dart=2.9
-
 import 'package:flutter_test/flutter_test.dart';
 import 'package:e2e/e2e.dart';
 import 'package:firebase_in_app_messaging/firebase_in_app_messaging.dart';
 
 void main() {
   E2EWidgetsFlutterBinding.ensureInitialized();
-  FirebaseInAppMessaging fiam;
+  late FirebaseInAppMessaging fiam;
 
   setUp(() {
     fiam = FirebaseInAppMessaging();
