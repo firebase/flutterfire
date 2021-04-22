d204d946ae92523d587bc01e60f15773111e83b5
Chingjun Lau <chingjun@google.com>
2021-04-22 12:48:57 -0700

    fix(firebase_auth,ios): Import all necessary headers from the header file.

--- packages/firebase_auth/firebase_auth/ios/Classes/Public/FLTFirebaseAuthPlugin.h
+++ packages/firebase_auth/firebase_auth/ios/Classes/Public/FLTFirebaseAuthPlugin.h
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#import <Firebase/Firebase.h>
 #if TARGET_OS_OSX
 #import <FlutterMacOS/FlutterMacOS.h>
 #else
