e16c2ff87dbc6a766a7633da12693226725f1a12
Salakar <mike.diarmid@gmail.com>
2021-04-23 13:00:53 +0100

    refactor(remote_config): switch e2e tests to use `drive` package + fix analyzer issues
    To match all other packages.

--- packages/firebase_remote_config/firebase_remote_config/example/test_driver/firebase_remote_config_e2e.dart
+++ packages/firebase_remote_config/firebase_remote_config/example/test_driver/firebase_remote_config_e2e.dart
@@ -1,20 +1,18 @@
+// @dart = 2.9
+import 'package:drive/drive.dart' as drive;
 import 'package:flutter_test/flutter_test.dart';
-// ignore: import_of_legacy_library_into_null_safe
-import 'package:integration_test/integration_test.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_remote_config/firebase_remote_config.dart';
 
-void main() {
-  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-
-  group('$RemoteConfig', () {
-    late RemoteConfig remoteConfig;
+void testsMain() {
+  group('RemoteConfig', () {
+    RemoteConfig remoteConfig;
 
     setUp(() async {
       await Firebase.initializeApp();
-      remoteConfig = await RemoteConfig.instance;
+      remoteConfig = RemoteConfig.instance;
       await remoteConfig.setConfigSettings(RemoteConfigSettings(
-        fetchTimeout: Duration(seconds: 8),
+        fetchTimeout: const Duration(seconds: 8),
         minimumFetchInterval: Duration.zero,
       ));
       await remoteConfig.setDefaults(<String, dynamic>{
@@ -24,14 +22,12 @@ void main() {
       await remoteConfig.ensureInitialized();
     });
 
-    testWidgets('fetch', (WidgetTester tester) async {
+    test('fetch', () async {
       final mark = DateTime.now();
       expect(remoteConfig.lastFetchTime.isBefore(mark), true);
       await remoteConfig.fetchAndActivate();
       expect(remoteConfig.lastFetchStatus, RemoteConfigFetchStatus.success);
       expect(remoteConfig.lastFetchTime.isAfter(mark), true);
-
-      // TODO should verify that our config settings actually took
       expect(remoteConfig.getString('welcome'), 'Earth, welcome! Hello!');
       expect(remoteConfig.getValue('welcome').source, ValueSource.valueRemote);
 
@@ -46,20 +42,23 @@ void main() {
       );
     });
 
-    testWidgets('settings', (WidgetTester tester) async {
-      expect(remoteConfig.settings.fetchTimeout, Duration(seconds: 8));
+    test('settings', () async {
+      expect(remoteConfig.settings.fetchTimeout, const Duration(seconds: 8));
       expect(remoteConfig.settings.minimumFetchInterval, Duration.zero);
       await remoteConfig.setConfigSettings(RemoteConfigSettings(
         fetchTimeout: Duration.zero,
-        minimumFetchInterval: Duration(seconds: 88),
+        minimumFetchInterval: const Duration(seconds: 88),
       ));
-      expect(remoteConfig.settings.fetchTimeout, Duration(seconds: 60));
-      expect(remoteConfig.settings.minimumFetchInterval, Duration(seconds: 88));
+      expect(remoteConfig.settings.fetchTimeout, const Duration(seconds: 60));
+      expect(remoteConfig.settings.minimumFetchInterval,
+          const Duration(seconds: 88));
       await remoteConfig.setConfigSettings(RemoteConfigSettings(
-          fetchTimeout: Duration(seconds: 10, milliseconds: 500),
+          fetchTimeout: const Duration(seconds: 10, milliseconds: 500),
           minimumFetchInterval: Duration.zero));
-      expect(remoteConfig.settings.fetchTimeout, Duration(seconds: 10));
+      expect(remoteConfig.settings.fetchTimeout, const Duration(seconds: 10));
       expect(remoteConfig.settings.minimumFetchInterval, Duration.zero);
     });
   });
 }
+
+void main() => drive.main(testsMain);
