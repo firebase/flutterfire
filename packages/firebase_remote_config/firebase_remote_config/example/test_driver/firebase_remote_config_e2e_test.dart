2b5ec6d0256c3c4c2c47d87b029e388dfa5dee43
Salakar <mike.diarmid@gmail.com>
2021-04-23 13:00:53 +0100

    refactor(remote_config): switch e2e tests to use `drive` package + fix analyzer issues
    To match all other packages.

--- packages/firebase_remote_config/firebase_remote_config/example/test_driver/firebase_remote_config_e2e_test.dart
+++ packages/firebase_remote_config/firebase_remote_config/example/test_driver/firebase_remote_config_e2e_test.dart
@@ -1,8 +1,8 @@
+// @dart = 2.9
 // Copyright 2020, the Chromium project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// ignore: import_of_legacy_library_into_null_safe
-import 'package:integration_test/integration_test_driver.dart';
+import 'package:drive/drive_driver.dart' as drive;
 
-Future<void> main() => integrationDriver();
+void main() => drive.main();
