72fa488ca53573319e459f2fae40d639728b35cd
russellwheatley <russellwheatley85@gmail.com>
2021-05-10 16:59:35 +0100

    test(firestore): loadBundle e2e test

--- packages/cloud_firestore/cloud_firestore/example/test_driver/load_bundle_e2e.dart
+++ packages/cloud_firestore/cloud_firestore/example/test_driver/load_bundle_e2e.dart
@@ -6,27 +6,26 @@ import 'dart:typed_data';
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore_platform_interface/cloud_firestore_platform_interface.dart';
 import 'package:http/http.dart' as http;
 
 void runLoadBundleTests() {
   group('$DocumentReference', () {
     late FirebaseFirestore firestore;
-
+    late Uri url;
+    late Uint8List buffer;
     setUpAll(() async {
       firestore = FirebaseFirestore.instance;
+      // endpoint serves a bundle with 3 documents each containing
+      // a 'number' property that increments in value 1-3.
+      url = Uri.https('api.rnfirebase.io', '/firestore/bundle');
+      final response = await http.get(url);
+      String string = response.body;
+      buffer = new Uint8List.fromList(string.codeUnits);
     });
 
     group('FirebaseFirestore.loadBundle()', () {
       test('loadBundle()', () async {
-        // endpoint serves a bundle with 3 documents each containing
-        // a 'number' property that increments in value 1-3.
-        //TODO(russellwheatley): move to RNFB testing API
-        final url = Uri.https('', '/');
-        final response = await http.get(url);
-
-        String string = response.body;
-        Uint8List buffer = new Uint8List.fromList(string.codeUnits);
-
         LoadBundleTask task = firestore.loadBundle(buffer);
 
         await task.stream.last;
@@ -39,6 +38,32 @@ void runLoadBundleTests() {
         expect(snapshot.docs[0]['number'], 1);
         expect(snapshot.docs[1]['number'], 2);
         expect(snapshot.docs[2]['number'], 3);
+      });
+
+      test('loadBundle(): LoadBundleTaskProgress stream snapshots', () async {
+        LoadBundleTask task = firestore.loadBundle(buffer);
+
+        List<LoadBundleTaskSnapshot> list = [];
+
+        task.stream.listen((event) {
+          list.add(event);
+        });
+
+        LoadBundleTaskSnapshot lastSnapshot = await task.stream.last;
+
+        if (list.length > 1) {
+          expect(list.first.taskState, LoadBundleTaskState.running);
+          expect(lastSnapshot.taskState, LoadBundleTaskState.success);
+        } else {
+          expect(lastSnapshot.taskState, LoadBundleTaskState.success);
+        }
+
+        expect(lastSnapshot.bytesLoaded, isNonNegative);
+        expect(lastSnapshot.documentsLoaded, isNonNegative);
+        expect(lastSnapshot.totalBytes, isNonNegative);
+        expect(lastSnapshot.totalDocuments, isNonNegative);
+
+        expect(lastSnapshot, isInstanceOf<LoadBundleTaskSnapshot>());
       });
     });
   });
