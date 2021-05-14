8d0aa60dd46b958a2501312649733c68dd8bdc74
Salakar <mike.diarmid@gmail.com>
2021-05-14 20:17:37 +0100

    -

--- packages/firebase_app_check/firebase_app_check/android/src/main/java/io/flutter/plugins/firebase/appcheck/FlutterFirebaseAppCheckPlugin.java
+++ packages/firebase_app_check/firebase_app_check/android/src/main/java/io/flutter/plugins/firebase/appcheck/FlutterFirebaseAppCheckPlugin.java
@@ -1,37 +1,95 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 package io.flutter.plugins.firebase.appcheck;
 
 import androidx.annotation.NonNull;
-import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import androidx.annotation.Nullable;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
+import com.google.firebase.FirebaseApp;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.PluginRegistry.Registrar;
+import io.flutter.plugins.firebase.core.FlutterFirebasePlugin;
+import java.io.IOException;
+import java.io.InterruptedIOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
-/** FirebaseAppCheckPlugin */
-public class FlutterFirebaseAppCheckPlugin implements FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private MethodChannel channel;
+public class FlutterFirebaseAppCheckPlugin implements FlutterFirebasePlugin, MethodCallHandler {
 
-  @Override
-  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "firebase_app_check");
-    channel.setMethodCallHandler(this);
+  public static void registerWith(Registrar registrar) {
+    final MethodChannel channel =
+      new MethodChannel(registrar.messenger(), "plugins.flutter.io/firebase_app_check");
+    channel.setMethodCallHandler(
+      new FlutterFirebaseAppCheckPlugin());
+  }
+
+
+  private Task<Void> activate(Map<String, Object> arguments) {
+    return Tasks.call(
+      cachedThreadPool,
+      () -> {
+        // TODO
+        return null;
+      });
   }
 
   @Override
-  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android " + android.os.Build.VERSION.RELEASE);
-    } else {
+  public void onMethodCall(MethodCall call, @NonNull final Result result) {
+    if (!call.method.equals("FirebaseAppCheck#activate")) {
       result.notImplemented();
+      return;
     }
+
+    activate(call.arguments())
+      .addOnCompleteListener(
+        task -> {
+          if (task.isSuccessful()) {
+            result.success(task.getResult());
+          } else {
+            Exception exception = task.getException();
+            result.error(
+              "firebase_app_check",
+              exception != null ? exception.getMessage() : null,
+              getExceptionDetails(exception));
+          }
+        });
+  }
+
+  private Map<String, Object> getExceptionDetails(@Nullable Exception exception) {
+    Map<String, Object> details = new HashMap<>();
+
+    if (exception == null) {
+      return details;
+    }
+
+    String code = "UNKNOWN";
+    String message = exception.getMessage();
+
+    // TODO
+
+    details.put("code", code.replace("_", "-").toLowerCase());
+    details.put("message", message);
+
+
+
+    return details;
   }
 
   @Override
-  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
-    channel.setMethodCallHandler(null);
+  public Task<Map<String, Object>> getPluginConstantsForFirebaseApp(FirebaseApp firebaseApp) {
+    return Tasks.call(() -> null);
+  }
+
+  @Override
+  public Task<Void> didReinitializeFirebaseCore() {
+    return Tasks.call(() -> null);
   }
 }
