e95be6c9a6e5919a2c99842a5b7617f760d8facc
russellwheatley <russellwheatley85@gmail.com>
2021-05-17 13:20:48 +0100

    test(firestore): namedQueryGet() tests

--- packages/cloud_firestore/cloud_firestore/example/test_driver/load_bundle_e2e.dart
+++ packages/cloud_firestore/cloud_firestore/example/test_driver/load_bundle_e2e.dart
@@ -8,20 +8,20 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cloud_firestore_platform_interface/cloud_firestore_platform_interface.dart';
 import 'package:http/http.dart' as http;
+// import 'package:test/test.dart';
 
 void runLoadBundleTests() {
   group('$DocumentReference', () {
     late FirebaseFirestore firestore;
     final url = Uri.https('api.rnfirebase.io', '/firestore/bundle');
-    late Uint8List buffer;
     const String collection = 'firestore-bundle-tests';
 
-    Future<void> loadBundleSetup() async {
+    Future<Uint8List> loadBundleSetup() async {
       // endpoint serves a bundle with 3 documents each containing
       // a 'number' property that increments in value 1-3.
       final response = await http.get(url);
       String string = response.body;
-      buffer = Uint8List.fromList(string.codeUnits);
+      return Uint8List.fromList(string.codeUnits);
     }
 
     setUp(() {
@@ -30,7 +30,7 @@ void runLoadBundleTests() {
 
     group('FirebaseFirestore.loadBundle()', () {
       test('loadBundle()', () async {
-        await loadBundleSetup();
+        Uint8List buffer = await loadBundleSetup();
         LoadBundleTask task = firestore.loadBundle(buffer);
 
         // ensure the bundle has been completely cached
@@ -38,16 +38,14 @@ void runLoadBundleTests() {
 
         QuerySnapshot<Map<String, Object?>> snapshot = await firestore
             .collection(collection)
-            .orderBy('number')
             .get(const GetOptions(source: Source.cache));
 
-        expect(snapshot.docs[0]['number'], 1);
-        expect(snapshot.docs[1]['number'], 2);
-        expect(snapshot.docs[2]['number'], 3);
+        expect(snapshot.docs.map((document) => document['number']),
+            everyElement(anyOf(1, 2, 3)));
       });
 
       test('loadBundle(): LoadBundleTaskProgress stream snapshots', () async {
-        await loadBundleSetup();
+        Uint8List buffer = await loadBundleSetup();
         LoadBundleTask task = firestore.loadBundle(buffer);
 
         final list = await task.stream().toList();
@@ -64,6 +62,45 @@ void runLoadBundleTests() {
 
         LoadBundleTaskSnapshot lastSnapshot = list.last;
         expect(lastSnapshot.taskState, LoadBundleTaskState.success);
+      });
+    });
+
+    group('FirebaeFirestore.namedQueryGet()', () {
+      test('namedQueryGet() successful', () async {
+        Uint8List buffer = await loadBundleSetup();
+        LoadBundleTask task = firestore.loadBundle(buffer);
+
+        // ensure the bundle has been completely cached
+        await task.stream().last;
+
+        //namedQuery 'named-bundle-test' which returns a QuerySnaphot of the same 3 documents
+        // with 'number' property
+        QuerySnapshot<Map<String, Object?>> snapshot =
+            await firestore.namedQueryGet(
+                'named-bundle-test', GetOptions(source: Source.cache));
+
+        expect(snapshot.docs.map((document) => document['number']),
+            everyElement(anyOf(1, 2, 3)));
+      });
+
+      test('namedQueryGet() error', () async {
+        Uint8List buffer = await loadBundleSetup();
+        LoadBundleTask task = firestore.loadBundle(buffer);
+
+        // ensure the bundle has been completely cached
+        await task.stream().last;
+
+        try {
+          await firestore.namedQueryGet(
+              'wrong-name', GetOptions(source: Source.cache));
+        } catch (error) {
+          expect(error, isA<FirebaseException>());
+          expect((error as FirebaseException).message,
+              contains('Named query has not been found'));
+          return;
+        }
+
+        throw ('did not catch error');
       });
     });
   });
