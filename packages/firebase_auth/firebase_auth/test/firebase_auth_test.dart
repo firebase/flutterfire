18d9dddc419a7296d53f2ae49347c73f5b0f81db
Elliot Hesp <elliot.hesp@gmail.com>
2021-05-17 14:37:31 +0100

    Update packages/firebase_auth/firebase_auth/test/firebase_auth_test.dart
    Co-authored-by: Remi Rousselet <darky12s@gmail.com>

--- packages/firebase_auth/firebase_auth/test/firebase_auth_test.dart
+++ packages/firebase_auth/firebase_auth/test/firebase_auth_test.dart
@@ -216,7 +216,9 @@ void main() {
             FakeFirebaseAuthPlatform(tenantId: 'foo');
         auth = FirebaseAuth.instanceFor(app: app);
         expect(auth!.tenantId, 'foo');
+
         auth!.tenantId = 'bar';
+
         expect(auth!.tenantId, 'bar');
         expect(FirebaseAuthPlatform.instance.tenantId, 'bar');
       });
