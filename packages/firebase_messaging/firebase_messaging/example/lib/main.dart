d7ce0b06648a3a7b78deef4803763bdf45a48a3f
Eyoel Defare <eyoeldefare@gmail.com>
2021-07-05 16:40:17 -0400

    firebase app initialized twice
    The firebase app is initialized twice both in the `main()` method and also inside `_firebaseMessagingBackgroundHandler` method which is also called in the `main` method. I have tested it in my environment and it works without this redundancy.

--- packages/firebase_messaging/firebase_messaging/example/lib/main.dart
+++ packages/firebase_messaging/firebase_messaging/example/lib/main.dart
@@ -26,7 +26,6 @@ import 'token_monitor.dart';
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   // If you're going to use other Firebase services in the background, such as Firestore,
   // make sure you call `initializeApp` before using other Firebase services.
-  await Firebase.initializeApp();
   print('Handling a background message ${message.messageId}');
 }
 
