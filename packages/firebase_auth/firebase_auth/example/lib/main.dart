1c04e2a5eaaa4c18827b84b6e3c49ecd7ad43d77
russellwheatley <russellwheatley85@gmail.com>
2021-07-07 10:18:59 +0100

    chore(auth): update example initializeApp()

--- packages/firebase_auth/firebase_auth/example/lib/main.dart
+++ packages/firebase_auth/firebase_auth/example/lib/main.dart
@@ -16,7 +16,13 @@ import './signin_page.dart';
 // e.g via `melos run firebase:emulator`.
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+    apiKey: 'AIzaSyAHAsf51D0A407EklG1bs-5wA7EbyfNFg0',
+    appId: '1:448618578101:ios:2bc5c1fe2ec336f8ac3efc',
+    messagingSenderId: '448618578101',
+    projectId: 'react-native-firebase-testing',
+  ));
   await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
   runApp(AuthExampleApp());
 }
