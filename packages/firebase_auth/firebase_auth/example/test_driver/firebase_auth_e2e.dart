031d5e25b20d089cb57181299335b7c1a1808474
russellwheatley <russellwheatley85@gmail.com>
2021-07-13 15:15:18 +0100

    test(auth, ios): fix bundleId

--- packages/firebase_auth/firebase_auth/example/test_driver/firebase_auth_e2e.dart
+++ packages/firebase_auth/firebase_auth/example/test_driver/firebase_auth_e2e.dart
@@ -22,10 +22,11 @@ void testsMain() {
     await Firebase.initializeApp(
         options: const FirebaseOptions(
             apiKey: 'AIzaSyAHAsf51D0A407EklG1bs-5wA7EbyfNFg0',
-            appId: '1:448618578101:ios:2bc5c1fe2ec336f8ac3efc',
+            appId: '1:448618578101:ios:4cd06f56e36384acac3efc',
             messagingSenderId: '448618578101',
             projectId: 'react-native-firebase-testing',
-            authDomain: 'react-native-firebase-testing.firebaseapp.com'));
+            authDomain: 'react-native-firebase-testing.firebaseapp.com',
+        ));
 
     if (useEmulator) {
       await FirebaseAuth.instance
