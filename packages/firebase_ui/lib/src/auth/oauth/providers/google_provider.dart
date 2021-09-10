6c06076ade2f04717c5f8aa3796e59840c185e4c
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2021-09-10 18:47:15 +0300

    feat(firebase_ui): implement macos google sign in (webview flow)

--- packages/firebase_ui/lib/src/auth/oauth/providers/google_provider.dart
+++ packages/firebase_ui/lib/src/auth/oauth/providers/google_provider.dart
@@ -8,14 +8,45 @@ import 'package:url_launcher/url_launcher.dart' show launch;
 import 'package:deep_links/deep_links.dart';
 
 import '../oauth_providers.dart';
+import 'package:flutter/services.dart';
+
+class GoogleSignInDesktop {
+  static const _methodChannel = MethodChannel('google_sign_in_desktop');
+
+  static GoogleSignInDesktop? _instance;
+  static GoogleSignInDesktop get instance =>
+      _instance ?? GoogleSignInDesktop._();
+
+  GoogleSignInDesktop._();
+
+  Future<OAuthCredential> signIn(String clientId, String redirectUri) async {
+    final callbackUrl = await _methodChannel.invokeMethod<String>('signIn', {
+      'clientId': clientId,
+      'redirectUri': redirectUri,
+    });
+
+    final uri = Uri.parse(callbackUrl!.replaceFirst('#', '?'));
+
+    final credential = GoogleAuthProvider.credential(
+      accessToken: uri.queryParameters['access_token'],
+    );
+
+    return credential;
+  }
+}
 
 class GoogleProviderImpl extends Google {
+  final String clientId;
+  final String redirectUri;
+
   final _provider = GoogleSignIn();
+
+  GoogleProviderImpl({required this.clientId, required this.redirectUri});
 
   @override
   Future<OAuthCredential> signIn() async {
     if (Platform.isMacOS) {
-      return _signInMacOS();
+      return _signInMacOSWebViewFlow();
     }
 
     final user = await _provider.signIn();
@@ -29,7 +60,11 @@ class GoogleProviderImpl extends Google {
     return credential;
   }
 
-  Future<OAuthCredential> _signInMacOS() async {
+  Future<OAuthCredential> _signInMacOSWebViewFlow() async {
+    return GoogleSignInDesktop.instance.signIn(clientId, redirectUri);
+  }
+
+  Future<OAuthCredential> _signInMacOSBrowserFlow() async {
     await launch('http://localhost:5000/google_sign_in.html');
     final dl = DeepLinks();
     final link = await dl.onLinkReceived.first;
@@ -47,7 +82,18 @@ class GoogleProviderImpl extends Google {
 }
 
 class GoogleProviderConfiguration extends OAuthProviderConfiguration {
-  final _provider = GoogleProviderImpl();
+  final String clientId;
+  final String redirectUri;
+
+  late final _provider = GoogleProviderImpl(
+    clientId: clientId,
+    redirectUri: redirectUri,
+  );
+
+  GoogleProviderConfiguration({
+    required this.clientId,
+    required this.redirectUri,
+  });
 
   @override
   String get providerId => GOOGLE_PROVIDER_ID;
