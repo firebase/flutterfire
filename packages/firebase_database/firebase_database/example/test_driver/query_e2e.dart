49bfec9f3993bb4aaf83bc61926fc1a3f2a3fd5d
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2021-09-14 16:09:29 +0300

    drop unused import

--- packages/firebase_database/firebase_database/example/test_driver/query_e2e.dart
+++ packages/firebase_database/firebase_database/example/test_driver/query_e2e.dart
@@ -3,7 +3,6 @@
 import 'dart:async';
 
 import 'package:firebase_database/firebase_database.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'firebase_database_e2e.dart';
