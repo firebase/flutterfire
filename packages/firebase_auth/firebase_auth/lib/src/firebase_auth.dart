50d4b458956669f7e0d1e02a8a104f8e0786597f
russellwheatley <russellwheatley85@gmail.com>
2021-09-23 11:13:30 +0200

    peg google sign in test

--- packages/firebase_auth/firebase_auth/lib/src/firebase_auth.dart
+++ packages/firebase_auth/firebase_auth/lib/src/firebase_auth.dart
@@ -4,6 +4,7 @@
 // found in the LICENSE file.
 
 part of firebase_auth;
+
 //testing auth package
 /// The entry point of the Firebase Authentication SDK.
 class FirebaseAuth extends FirebasePluginPlatform {
