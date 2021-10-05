76f9912c8a1168fd4b496fbdc42f68142c47e7f5
Julian Steenbakker <juliansteenbakker@outlook.com>
2021-10-05 16:14:34 +0200

    imp: reinstate null check

--- packages/firebase_messaging/firebase_messaging/android/src/main/java/io/flutter/plugins/firebase/messaging/FlutterFirebaseMessagingPlugin.java
+++ packages/firebase_messaging/firebase_messaging/android/src/main/java/io/flutter/plugins/firebase/messaging/FlutterFirebaseMessagingPlugin.java
@@ -70,7 +70,9 @@ public class FlutterFirebaseMessagingPlugin extends BroadcastReceiver
 
   @Override
   public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
-    LocalBroadcastManager.getInstance(binding.getApplicationContext()).unregisterReceiver(this);
+    if (binding.getApplicationContext() != null) {
+      LocalBroadcastManager.getInstance(binding.getApplicationContext()).unregisterReceiver(this);
+    }
   }
 
   @Override
