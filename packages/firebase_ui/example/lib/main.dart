f37cad4be60e9f7a291e089b52f319f5ad662f98
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2021-10-06 19:27:42 +0300

    chore(firebase_ui): drop firebaseui builder from material app

--- packages/firebase_ui/example/lib/main.dart
+++ packages/firebase_ui/example/lib/main.dart
@@ -60,7 +60,6 @@ class FirebaseAuthUIExample extends StatelessWidget {
             border: OutlineInputBorder(),
           ),
         ),
-        builder: FirebaseUIInit.builder,
         home: const AuthResolver(),
       ),
     );
