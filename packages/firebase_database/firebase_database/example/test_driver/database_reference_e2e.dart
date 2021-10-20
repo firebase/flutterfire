65f94681a9a4946f9006b2a3125598a2a98640c0
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2021-10-20 15:13:17 +0300

    chore(database): fix analyzer

--- packages/firebase_database/firebase_database/example/test_driver/database_reference_e2e.dart
+++ packages/firebase_database/firebase_database/example/test_driver/database_reference_e2e.dart
@@ -57,7 +57,7 @@ void runDatabaseReferenceTests() {
     test('get primitive list values', () async {
       List<String> data = ['first', 'second'];
       final FirebaseDatabase database = FirebaseDatabase.instance;
-      final DatabaseReference ref = database.reference().child('list-values');
+      final DatabaseReference ref = database.ref('list-values');
 
       await ref.set({'list': data});
 
