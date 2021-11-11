fa9635b9211c4a102339bf570d11c24da2fe3abf
russellwheatley <russellwheatley85@gmail.com>
2021-11-11 14:04:24 +0000

    tests(dynamic_links): update e2e

--- packages/firebase_dynamic_links/firebase_dynamic_links/example/test_driver/firebase_dynamic_links_e2e.dart
+++ packages/firebase_dynamic_links/firebase_dynamic_links/example/test_driver/firebase_dynamic_links_e2e.dart
@@ -1,16 +1,21 @@
 // ignore_for_file: require_trailing_commas
-// @dart = 2.9
-// Copyright 2020, the Chromium project authors.  Please see the AUTHORS file
+// Copyright 2021, the Chromium project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
 import 'package:drive/drive.dart' as drive;
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void testsMain() {
+  setUpAll(() async {
+    await Firebase.initializeApp();
+  });
+
   group('DynamicLinks', () {
     test('buildUrl', () async {
+      FirebaseDynamicLinks dynamicLinks = FirebaseDynamicLinks.instance;
       const String androidPackageName =
           'io.flutter.plugins.firebasedynamiclinksexample';
       const String iosBundleId =
@@ -18,23 +23,23 @@ void testsMain() {
       const String urlHost = 'cx4k7.app.goo.gl';
       const String link = 'https://dynamic.link.example/helloworld';
 
-      final DynamicLinkBuilder parameters = DynamicLinkBuilder(
+      final DynamicLinkParameters parameters = DynamicLinkParameters(
         uriPrefix: 'https://$urlHost',
         link: Uri.parse(link),
-        androidParameters: AndroidParameters(
+        androidParameters: const AndroidParameters(
           packageName: androidPackageName,
           minimumVersion: 1,
         ),
-        dynamicLinkParametersOptions: DynamicLinkParametersOptions(
+        dynamicLinkParametersOptions: const DynamicLinkParametersOptions(
           shortDynamicLinkPathLength: ShortDynamicLinkPathLength.short,
         ),
-        iosParameters: IosParameters(
+        iosParameters: const IosParameters(
           bundleId: iosBundleId,
           minimumVersion: '2',
         ),
       );
 
-      final Uri uri = await parameters.buildUrl();
+      final Uri uri = await dynamicLinks.buildUrl(parameters);
 
       // androidParameters.minimumVersion
       expect(
