c909f0b4ba2974534ef82e45708a244b8d25e314
Mais Alheraki <hiimechi@gmail.com>
2021-11-16 21:09:09 +0300

    chore: remove duplicate license header
    Co-authored-by: Mike Diarmid <mike.diarmid@gmail.com>

--- packages/firebase_in_app_messaging/firebase_in_app_messaging_platform_interface/lib/firebase_in_app_messaging_platform_interface.dart
+++ packages/firebase_in_app_messaging/firebase_in_app_messaging_platform_interface/lib/firebase_in_app_messaging_platform_interface.dart
@@ -2,11 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// ignore_for_file: require_trailing_commas
-// Copyright 2020 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 library firebase_in_app_messaging_platform_interface;
 
 export 'src/platform_interface/platform_interface_firebase_in_app_messaging.dart';
