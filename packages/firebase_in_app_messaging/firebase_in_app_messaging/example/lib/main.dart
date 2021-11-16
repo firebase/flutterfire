6b004e350903ab22266be9be7449ff5fc0d58c13
pr_Mais <hiimechi@gmail.com>
2021-11-16 19:42:43 +0300

    fix: example app

--- packages/firebase_in_app_messaging/firebase_in_app_messaging/example/lib/main.dart
+++ packages/firebase_in_app_messaging/firebase_in_app_messaging/example/lib/main.dart
@@ -6,10 +6,15 @@
 import 'dart:async';
 
 import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_in_app_messaging/firebase_in_app_messaging.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   static FirebaseAnalytics analytics = FirebaseAnalytics.instance;
