1135ed70e935e06c951a8c89708f83a890eb1f0a
russellwheatley <russellwheatley85@gmail.com>
2021-11-16 10:48:06 +0000

    chore(dynamic_links): update example init

--- packages/firebase_dynamic_links/firebase_dynamic_links/example/lib/main.dart
+++ packages/firebase_dynamic_links/firebase_dynamic_links/example/lib/main.dart
@@ -15,16 +15,15 @@ import 'package:url_launcher/url_launcher.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: const FirebaseOptions(
-      apiKey: 'AIzaSyAHAsf51D0A407EklG1bs-5wA7EbyfNFg0',
-      appId: '1:448618578101:ios:4cd06f56e36384acac3efc',
-      messagingSenderId: '448618578101',
-      projectId: 'react-native-firebase-testing',
-      authDomain: 'react-native-firebase-testing.firebaseapp.com',
-      iosClientId:
-      '448618578101-m53gtqfnqipj12pts10590l37npccd2r.apps.googleusercontent.com',
-    )
-  );
+      options: const FirebaseOptions(
+    apiKey: 'AIzaSyCuu4tbv9CwwTudNOweMNstzZHIDBhgJxA',
+    appId: '1:448618578101:ios:4cd06f56e36384acac3efc',
+    messagingSenderId: '448618578101',
+    projectId: 'react-native-firebase-testing',
+    authDomain: 'react-native-firebase-testing.firebaseapp.com',
+    iosClientId:
+        '448618578101-m53gtqfnqipj12pts10590l37npccd2r.apps.googleusercontent.com',
+  ));
   runApp(MaterialApp(
     title: 'Dynamic Links Example',
     routes: <String, WidgetBuilder>{
