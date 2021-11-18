cbcb1d105dc2fe63025462caf4a2b66bbadc5108
russellwheatley <russellwheatley85@gmail.com>
2021-11-18 09:14:44 +0000

    test(crashlytics): init from config files

--- packages/firebase_crashlytics/firebase_crashlytics/example/test_driver/firebase_crashlytics_e2e.dart
+++ packages/firebase_crashlytics/firebase_crashlytics/example/test_driver/firebase_crashlytics_e2e.dart
@@ -16,14 +16,7 @@ void testsMain() {
     /*late*/ FirebaseCrashlytics crashlytics;
 
     setUpAll(() async {
-      await Firebase.initializeApp(
-          options: const FirebaseOptions(
-        apiKey: 'AIzaSyAHAsf51D0A407EklG1bs-5wA7EbyfNFg0',
-        appId: '1:448618578101:ios:2bc5c1fe2ec336f8ac3efc',
-        messagingSenderId: '448618578101',
-        authDomain: 'react-native-firebase-testing.firebaseapp.com',
-        projectId: 'react-native-firebase-testing',
-      ));
+      await Firebase.initializeApp();
       crashlytics = FirebaseCrashlytics.instance;
     });
 
