a43fbd334fe8fd300213a83a770d116544ad9419
russellwheatley <russellwheatley85@gmail.com>
2021-11-25 14:33:48 +0000

    chore(performance, ios): rm dead code

--- packages/firebase_performance/firebase_performance/ios/Classes/FLTHttpMetric.m
+++ packages/firebase_performance/firebase_performance/ios/Classes/FLTHttpMetric.m
@@ -23,20 +23,6 @@
     [self start:result];
   } else if ([@"HttpMetric#stop" isEqualToString:call.method]) {
     [self stop:call result:result];
-  } else if ([@"HttpMetric#httpResponseCode" isEqualToString:call.method]) {
-    [self setHttpResponseCode:call result:result];
-  } else if ([@"HttpMetric#requestPayloadSize" isEqualToString:call.method]) {
-    [self requestPayloadSize:call result:result];
-  } else if ([@"HttpMetric#responseContentType" isEqualToString:call.method]) {
-    [self responseContentType:call result:result];
-  } else if ([@"HttpMetric#responsePayloadSize" isEqualToString:call.method]) {
-    [self responsePayloadSize:call result:result];
-  } else if ([@"HttpMetric#putAttribute" isEqualToString:call.method]) {
-    [self putAttribute:call result:result];
-  } else if ([@"HttpMetric#removeAttribute" isEqualToString:call.method]) {
-    [self removeAttribute:call result:result];
-  } else if ([@"HttpMetric#getAttributes" isEqualToString:call.method]) {
-    [self getAttributes:result];
   } else {
     result(FlutterMethodNotImplemented);
   }
@@ -53,13 +39,13 @@
   NSNumber *requestPayloadSize = call.arguments[@"requestPayloadSize"];
   NSString *responseContentType = call.arguments[@"responseContentType"];
   NSNumber *responsePayloadSize = call.arguments[@"responsePayloadSize"];
-  
+
   [attributes enumerateKeysAndObjectsUsingBlock:^(NSString *attributeName, NSString *value, BOOL *stop) {
     [_metric setValue:value forAttribute:attributeName];
   }];
-  
+
   if (httpResponseCode != nil) {
-    _metric.responseCode = [httpResponseCode intValue];
+    _metric.responseCode = [httpResponseCode integerValue];
   }
   if (responseContentType != nil) {
     _metric.responseContentType = responseContentType;
@@ -79,56 +65,4 @@
   result(nil);
 }
 
-- (void)setHttpResponseCode:(FlutterMethodCall *)call result:(FlutterResult)result {
-  NSNumber *responseCode = call.arguments[@"httpResponseCode"];
-
-  if (![responseCode isEqual:[NSNull null]]) _metric.responseCode = [responseCode integerValue];
-  result(nil);
-}
-
-- (void)requestPayloadSize:(FlutterMethodCall *)call result:(FlutterResult)result {
-  NSNumber *requestPayloadSize = call.arguments[@"requestPayloadSize"];
-
-  if (![requestPayloadSize isEqual:[NSNull null]]) {
-    _metric.requestPayloadSize = [requestPayloadSize longValue];
-  }
-  result(nil);
-}
-
-- (void)responseContentType:(FlutterMethodCall *)call result:(FlutterResult)result {
-  NSString *responseContentType = call.arguments[@"responseContentType"];
-
-  if (![responseContentType isEqual:[NSNull null]]) {
-    _metric.responseContentType = responseContentType;
-  }
-  result(nil);
-}
-
-- (void)responsePayloadSize:(FlutterMethodCall *)call result:(FlutterResult)result {
-  NSNumber *responsePayloadSize = call.arguments[@"responsePayloadSize"];
-
-  if (![responsePayloadSize isEqual:[NSNull null]]) {
-    _metric.responsePayloadSize = [responsePayloadSize longValue];
-  }
-  result(nil);
-}
-
-- (void)putAttribute:(FlutterMethodCall *)call result:(FlutterResult)result {
-  NSString *name = call.arguments[@"name"];
-  NSString *value = call.arguments[@"value"];
-
-  [_metric setValue:value forAttribute:name];
-  result(nil);
-}
-
-- (void)removeAttribute:(FlutterMethodCall *)call result:(FlutterResult)result {
-  NSString *name = call.arguments[@"name"];
-
-  [_metric removeAttribute:name];
-  result(nil);
-}
-
-- (void)getAttributes:(FlutterResult)result {
-  result([_metric attributes]);
-}
 @end
