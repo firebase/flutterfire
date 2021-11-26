cc794bf9fb6edb2c62042b638b8968e2b51460aa
Remi Rousselet <darky12s@gmail.com>
2021-11-26 17:26:57 +0100

    Update the ListView example to show a list of towns

--- packages/flutterfire_ui/example/lib/stories/widgets/firestore_list_view.dart
+++ packages/flutterfire_ui/example/lib/stories/widgets/firestore_list_view.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -7,20 +5,49 @@ import 'package:flutterfire_ui/firestore.dart';
 import 'package:flutterfire_ui_example/stories/stories_lib/story.dart';
 
 class Country {
-  Country();
-  Country.fromJson(Map<String, Object?> json) : this();
+  Country({required this.towns});
+  Country.fromJson(Map<String, Object?> json)
+      : this(
+          towns: (json['towns']! as List).map((e) => Town.fromJson(e)).toList(),
+        );
+
+  final List<Town> towns;
 
   Map<String, Object?> toJson() {
-    return {};
+    return {
+      'towns': towns.map((e) => e.toJson()).toList(),
+    };
   }
 }
 
-final countriesCollection =
-    FirebaseFirestore.instance.collection('firebasePerfTest');
-// .withConverter<Country>(
-//   fromFirestore: (snapshot, _) => Country.fromJson(snapshot.data()!),
-//   toFirestore: (country, _) => country.toJson(),
-// );
+class Town {
+  Town({required this.name, required this.mayor, required this.population});
+  Town.fromJson(Map<String, Object?> json)
+      : this(
+          name: json['name']! as String,
+          mayor: json['mayor']! as String,
+          population: json['population']! as int,
+        );
+
+  final String name;
+  final String mayor;
+  final int population;
+
+  Map<String, Object?> toJson() {
+    return {
+      'name': name,
+      'mayor': mayor,
+      'population': population,
+    };
+  }
+}
+
+final countriesCollection = FirebaseFirestore.instance
+    .collection('firebasePerfTest')
+    .withConverter<Country>(
+      fromFirestore: (snapshot, _) => Country.fromJson(snapshot.data()!),
+      toFirestore: (country, _) => country.toJson(),
+    );
 
 class FirestoreListViewStory extends StoryWidget {
   const FirestoreListViewStory({Key? key})
@@ -28,12 +55,16 @@ class FirestoreListViewStory extends StoryWidget {
 
   @override
   Widget build(StoryElement context) {
-    return FirestoreListView<Map>(
-      primary: true,
-      itemExtent: 200,
+    return FirestoreListView<Country>(
       query: countriesCollection,
+      primary: true,
       itemBuilder: (context, snapshot) {
-        return Text(snapshot.data().toString());
+        return Column(
+          children: [
+            for (final town in snapshot.data().towns)
+              Text('${town.name} (${town.population})'),
+          ],
+        );
       },
     );
   }
