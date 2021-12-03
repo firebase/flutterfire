abc653ef50f04dc04be41bfa33ab8e144a9667ab
Salakar <mike.diarmid@gmail.com>
2021-12-03 10:42:46 +0000

    -

--- packages/firebase_database/firebase_database/example/test_driver/firebase_database_e2e.dart
+++ packages/firebase_database/firebase_database/example/test_driver/firebase_database_e2e.dart
@@ -25,7 +25,16 @@ final emulatorHost =
 
 void testsMain() {
   setUpAll(() async {
-    await Firebase.initializeApp();
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: 'AIzaSyAHAsf51D0A407EklG1bs-5wA7EbyfNFg0',
+        appId: '1:448618578101:ios:2bc5c1fe2ec336f8ac3efc',
+        messagingSenderId: '448618578101',
+        projectId: 'react-native-firebase-testing',
+        databaseURL: 'https://react-native-firebase-testing.firebaseio.com',
+        storageBucket: 'react-native-firebase-testing.appspot.com',
+      ),
+    );
     database = FirebaseDatabase.instance;
     database.useDatabaseEmulator(emulatorHost, emulatorPort);
   });
