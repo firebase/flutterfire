997b69d04297a2b2bb5187dac5e8f5892c361ad1
pr_Mais <hiimechi@gmail.com>
2021-12-03 16:55:41 +0300

    chore: macOS linked files

--- packages/firebase_installations/firebase_installations/macos/Classes/FirebaseInstallationsPlugin.h
+++ packages/firebase_installations/firebase_installations/macos/Classes/FirebaseInstallationsPlugin.h
@@ -1,12 +1,1 @@
-#import <TargetConditionals.h>
-
-#if TARGET_OS_OSX
-#import <FlutterMacOS/FlutterMacOS.h>
-#else
-#import <Flutter/Flutter.h>
-#endif
-
-#import <firebase_core/FLTFirebasePlugin.h>
-
-@interface FirebaseInstallationsPlugin : FLTFirebasePlugin <FlutterPlugin, FLTFirebasePlugin>
-@end
+../../ios/Classes/FirebaseInstallationsPlugin.h
