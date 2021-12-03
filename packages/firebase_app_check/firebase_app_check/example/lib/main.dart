a67f89dc5e1e19d97c00a5e4448587811468a8d6
russellwheatley <russellwheatley85@gmail.com>
2021-12-03 16:32:35 +0000

    chore(app_check): update example app

--- packages/firebase_app_check/firebase_app_check/example/lib/main.dart
+++ packages/firebase_app_check/firebase_app_check/example/lib/main.dart
@@ -1,6 +1,6 @@
-// ignore_for_file: require_trailing_commas
 import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
@@ -13,7 +13,7 @@ Future<void> main() async {
         projectId: 'react-native-firebase-testing',
         storageBucket: 'react-native-firebase-testing.appspot.com',
         messagingSenderId: '448618578101',
-        appId: '1:448618578101:web:772d484dc9eb15e9ac3efc',
+        appId: '1:448618578101:ios:eaf25c1747605f69ac3efc',
         measurementId: 'G-0N1G9FLDZE'),
   );
 
@@ -26,19 +26,109 @@ Future<void> main() async {
 }
 
 class MyApp extends StatelessWidget {
+  final String title = 'Firebase App Check';
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Firebase App Check',
-      home: FirebaseAppCheckExample(),
+      home: FirebaseAppCheckExample(title: title),
     );
   }
 }
 
-class FirebaseAppCheckExample extends StatelessWidget {
+class FirebaseAppCheckExample extends StatefulWidget {
+  FirebaseAppCheckExample({
+    Key key,
+    this.title,
+  }) : super(key: key);
+
+  final String title;
+
+  @override
+  _FirebaseAppCheck createState() => _FirebaseAppCheck();
+}
+
+class _FirebaseAppCheck extends State<FirebaseAppCheckExample> {
+  final appCheck = FirebaseAppCheck.instance;
+  String _message = '';
+  String _eventToken = 'not yet';
+
+  @override
+  void initState() {
+    appCheck.tokenChanges().listen((event) {
+      setEventToken(event.token);
+    });
+    super.initState();
+  }
+
+  void setMessage(String message) {
+    setState(() {
+      _message = message;
+    });
+  }
+
+  void setEventToken(String token) {
+    setState(() {
+      _eventToken = token ?? 'not yet';
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Text('App Check Example');
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body:Center(
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () async {
+                if (kIsWeb) {
+                  print(
+                      'Pass in your "webRecaptchaSiteKey" key found on you Firebase Console to activate if using on the web platform.');
+                }
+                await appCheck.activate();
+                setMessage('activated!!');
+              },
+              child: const Text('activate()'),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                final result = await appCheck.getToken();
+                final token = result.token;
+                setMessage('your token: $token');
+              },
+              child: const Text('getToken()'),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                await appCheck.setTokenAutoRefreshEnabled(true);
+                setMessage('successfully set auto token refresh!!');
+              },
+              child: const Text('setTokenAutoRefreshEnabled()'),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              _message, //#007bff
+              style: const TextStyle(
+                color: Color.fromRGBO(47, 79, 79, 1),
+                fontSize: 16,
+              ),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              'Token received from tokenChanges() API: $_eventToken', //#007bff
+              style: const TextStyle(
+                color: Color.fromRGBO(128, 0, 128, 1),
+                fontSize: 16,
+              ),
+            ),
+          ],
+        ),
+      )
+    );
   }
 }
