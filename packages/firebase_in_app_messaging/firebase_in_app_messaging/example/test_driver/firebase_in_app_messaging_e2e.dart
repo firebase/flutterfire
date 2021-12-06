634a4831fca17b09f5cfb049c5f26d688825a502
Salakar <mike.diarmid@gmail.com>
2021-12-06 07:10:44 +0000

    cleanup

--- packages/firebase_in_app_messaging/firebase_in_app_messaging/example/test_driver/firebase_in_app_messaging_e2e.dart
+++ packages/firebase_in_app_messaging/firebase_in_app_messaging/example/test_driver/firebase_in_app_messaging_e2e.dart
@@ -1,4 +1,6 @@
-// @dart = 2.9
+// Copyright 2021, the Chromium project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_in_app_messaging/firebase_in_app_messaging.dart';
