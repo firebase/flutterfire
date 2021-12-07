6ff677726164ceeb294ea4bbe0b7a959dbb385bc
Remi Rousselet <darky12s@gmail.com>
2021-12-07 11:28:33 +0100

    feat(ui): add FirebaseTable & FirebaseListView (#7479)

--- packages/flutterfire_ui/lib/firestore.dart
+++ packages/flutterfire_ui/lib/firestore.dart
@@ -1,4 +1,11 @@
 export 'src/query_builder.dart'
-    show FirestoreQueryBuilder, QueryBuilderSnapshot, FirestoreListView;
+    show
+        FirestoreQueryBuilder,
+        QueryBuilderSnapshot,
+        FirestoreListView,
+        FirestoreErrorBuilder,
+        FirestoreItemBuilder,
+        FirestoreLoadingBuilder,
+        FirestoreQueryBuilderSnapshotBuilder;
 
 export 'src/firestore_table_builder.dart' show FirestoreDataTable;
