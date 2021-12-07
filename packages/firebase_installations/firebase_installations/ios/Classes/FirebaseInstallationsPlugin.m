ad035ecbf6eea23b0ef8578d327d7cbd28adc8f6
russellwheatley <russellwheatley85@gmail.com>
2021-12-07 09:27:50 +0000

    chore(installations): rename to firebase_app_installations

--- packages/firebase_installations/firebase_installations/ios/Classes/FirebaseInstallationsPlugin.m
+++ packages/firebase_installations/firebase_installations/ios/Classes/FirebaseInstallationsPlugin.m
@@ -3,13 +3,13 @@
 // found in the LICENSE file.
 
 #import "FirebaseInstallationsPlugin.h"
-#if __has_include(<flutterfire_installations/flutterfire_installations-Swift.h>)
-#import <flutterfire_installations/flutterfire_installations-Swift.h>
+#if __has_include(<firebase_app_installations/firebase_app_installations-Swift.h>)
+#import <firebase_app_installations/firebase_app_installations-Swift.h>
 #else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "flutterfire_installations-Swift.h"
+#import "firebase_app_installations-Swift.h"
 #endif
 
 @implementation FirebaseInstallationsPlugin
