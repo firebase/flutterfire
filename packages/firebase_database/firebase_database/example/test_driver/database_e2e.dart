c33c3c931d7e4c654dd0b2cd23d800b43192d95d
Salakar <mike.diarmid@gmail.com>
2021-12-08 21:06:45 +0000

    fix: database path should default to `/` if no path specified rather than an empty string (fixes #7515)

--- packages/firebase_database/firebase_database/example/test_driver/database_e2e.dart
+++ packages/firebase_database/firebase_database/example/test_driver/database_e2e.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'firebase_database_e2e.dart';
 
@@ -20,6 +21,43 @@ void runDatabaseTests() {
       expect(snapshot.key, 'flutterfire');
       expect(snapshot.value, 0);
     });
+
+    test(
+      'root reference path returns as "/"',
+      () async {
+        final rootRef = database.ref();
+        expect(rootRef.path, '/');
+        expect(rootRef.key, isNull);
+        expect(rootRef.parent, isNull);
+
+        // Actually test trying to use the root ref
+        // for https://github.com/FirebaseExtended/flutterfire/issues/7515
+        // to make sure it doesn't crash
+        await expectLater(
+          rootRef.update({
+            'foo': 'bar',
+            'flutter': 'bar',
+            'dart': 'bar',
+          }),
+          throwsA(
+            isA<FirebaseException>()
+                .having(
+                  (error) => error.code,
+                  'code',
+                  'permission-denied',
+                )
+                .having(
+                  (error) => error.message,
+                  'message',
+                  predicate(
+                    (String message) =>
+                        message.contains("doesn't have permission"),
+                  ),
+                ),
+          ),
+        );
+      },
+    );
 
     test(
       'returns a reference to the root of the database if no path specified',
