220c57633eb7b941c2a32d7b682545e41ad85856
Mike Diarmid <mike.diarmid@gmail.com>
2021-12-08 14:45:58 +0000

    chore: remove unused import

--- packages/flutterfire_ui/example/lib/main.dart
+++ packages/flutterfire_ui/example/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterfire_ui/auth.dart';
 import 'package:flutterfire_ui/i10n.dart';
