235771f379af6247713ca3c4b4e3dfbd63642254
russellwheatley <russellwheatley85@gmail.com>
2021-12-09 16:33:30 +0000

    chore(analytics): reinstate observer.dart

--- packages/firebase_analytics/firebase_analytics/lib/firebase_analytics.dart
+++ packages/firebase_analytics/firebase_analytics/lib/firebase_analytics.dart
@@ -4,8 +4,11 @@
 
 library firebase_analytics;
 
+import 'package:meta/meta.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_core_platform_interface/firebase_core_platform_interface.dart'
     show FirebasePluginPlatform;
@@ -13,6 +16,6 @@ import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:firebase_analytics_platform_interface/firebase_analytics_platform_interface.dart';
 export 'package:firebase_analytics_platform_interface/firebase_analytics_platform_interface.dart'
     show AnalyticsEventItem, AnalyticsCallOptions;
-import 'package:flutter/widgets.dart';
 
 part 'src/firebase_analytics.dart';
+part 'src/observer.dart';
