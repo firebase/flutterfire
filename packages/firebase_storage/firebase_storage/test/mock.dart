5183df31a2b5119054766ad3f77e07ee1b1dda3c
russellwheatley <russellwheatley85@gmail.com>
2021-12-09 18:41:27 +0000

    chore: rm additional inexplicable errors

--- packages/firebase_storage/firebase_storage/test/mock.dart
+++ packages/firebase_storage/firebase_storage/test/mock.dart
@@ -9,7 +9,6 @@ import 'dart:typed_data';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_core_platform_interface/firebase_core_platform_interface.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:firebase_storage_platform_interface/firebase_storage_platform_interface.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
