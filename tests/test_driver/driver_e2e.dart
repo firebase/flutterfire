b6497c1e7af523be34c864960285b4f0e6aa7bb0
Salakar <mike.diarmid@gmail.com>
2021-12-10 16:27:21 +0000

    chore: create tests app

---
+++ tests/test_driver/driver_e2e.dart
@@ -0,0 +1,12 @@
+// Copyright 2021, the Chromium project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:drive/drive.dart' as drive;
+import 'package:flutter_test/flutter_test.dart';
+
+void testsMain() {
+  // TODO
+}
+
+void main() => drive.main(testsMain);
