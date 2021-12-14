c55889931b6356c01e3710e7b158380f516397ae
Salakar <mike.diarmid@gmail.com>
2021-12-14 22:39:07 +0000

    ci: replace functions workflow with new e2e_tests workflow

--- tests/test_driver/driver_e2e.dart
+++ tests/test_driver/driver_e2e.dart
@@ -3,6 +3,7 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'package:drive/drive.dart' as drive;
+import 'cloud_functions/cloud_functions_e2e.dart' as cloud_functions;
 import 'firebase_auth/firebase_auth_e2e.dart' as firebase_auth;
 import 'firebase_core/firebase_core_e2e.dart' as firebase_core;
 import 'firebase_analytics/firebase_analytics_e2e.dart' as firebase_analytics;
@@ -11,6 +12,7 @@ void setupTests() {
   // Core first.
   firebase_core.setupTests();
   // All other tests.
+  cloud_functions.setupTests();
   firebase_auth.setupTests();
   firebase_analytics.setupTests();
 }
