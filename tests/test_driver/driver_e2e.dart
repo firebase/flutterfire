750d395d0d4963f59a3210c9cffd95cc06eada04
Salakar <mike.diarmid@gmail.com>
2021-12-14 12:08:01 +0000

    ci: replace analytics workflow with new e2e_tests workflow

--- tests/test_driver/driver_e2e.dart
+++ tests/test_driver/driver_e2e.dart
@@ -4,9 +4,11 @@
 
 import 'package:drive/drive.dart' as drive;
 import 'firebase_core/firebase_core_e2e.dart' as firebase_core;
+import 'firebase_analytics/firebase_analytics_e2e.dart' as firebase_analytics;
 
 void setupTests() {
   firebase_core.setupTests();
+  firebase_analytics.setupTests();
 }
 
 void main() => drive.main(setupTests);
