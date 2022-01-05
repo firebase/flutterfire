744a32a337fccd1e78210158270569ab9c59d5bd
KikiManjaro <kylian.meulin@gmail.com>
2022-01-05 19:37:23 +0100

    feat(flutterfire_ui): added French language

--- packages/flutterfire_ui/lib/src/i10n/default_localizations.dart
+++ packages/flutterfire_ui/lib/src/i10n/default_localizations.dart
@@ -1,5 +1,6 @@
 import 'lang/en.dart';
 import '../i10n/lang/ar.dart';
+import 'lang/fr.dart';
 
 abstract class FlutterFireUILocalizationLabels {
   const FlutterFireUILocalizationLabels();
@@ -96,6 +97,7 @@ abstract class FlutterFireUILocalizationLabels {
 const localizations = <String, FlutterFireUILocalizationLabels>{
   'en': EnLocalizations(),
   'ar': ArLocalizations(),
+  'fr': FrLocalizations(),
 };
 
 class DefaultLocalizations extends EnLocalizations {
