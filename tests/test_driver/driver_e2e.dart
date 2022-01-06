b8c596989d34f1f9938591976ed61e94928d8d6f
Salakar <mike.diarmid@gmail.com>
2022-01-06 19:04:40 +0000

    tests: move Installations e2e tests to `tests` app

--- tests/test_driver/driver_e2e.dart
+++ tests/test_driver/driver_e2e.dart
@@ -7,6 +7,8 @@ import 'cloud_functions/cloud_functions_e2e.dart' as cloud_functions;
 import 'firebase_auth/firebase_auth_e2e.dart' as firebase_auth;
 import 'firebase_core/firebase_core_e2e.dart' as firebase_core;
 import 'firebase_analytics/firebase_analytics_e2e.dart' as firebase_analytics;
+import 'firebase_app_installations/firebase_app_installations_e2e.dart'
+    as firebase_app_installations;
 import 'firebase_in_app_messaging/firebase_in_app_messaging_e2e.dart'
     as firebase_in_app_messaging;
 
@@ -18,6 +20,7 @@ void setupTests() {
   cloud_functions.setupTests();
   firebase_analytics.setupTests();
   firebase_in_app_messaging.setupTests();
+  firebase_app_installations.setupTests();
 }
 
 void main() => drive.main(setupTests);
