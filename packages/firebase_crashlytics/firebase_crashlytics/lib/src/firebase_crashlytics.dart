9a2a4d83d51a893588d246c159925573fba419a8
Elena Doty <elenadoty@google.com>
2022-01-07 19:34:47 -0500

    remove testing print

--- packages/firebase_crashlytics/firebase_crashlytics/lib/src/firebase_crashlytics.dart
+++ packages/firebase_crashlytics/firebase_crashlytics/lib/src/firebase_crashlytics.dart
@@ -133,16 +133,13 @@ class FirebaseCrashlytics extends FirebasePluginPlatform {
   Future<void> recordFlutterError(FlutterErrorDetails flutterErrorDetails) {
     FlutterError.presentError(flutterErrorDetails);
 
-    print(flutterErrorDetails.exceptionAsString());
-
     return recordError(
         flutterErrorDetails.exceptionAsString(), flutterErrorDetails.stack,
         reason: flutterErrorDetails.context,
         information: flutterErrorDetails.informationCollector == null
             ? []
             : flutterErrorDetails.informationCollector!(),
-        printDetails: false,
-        fatal: true);
+        printDetails: false);
   }
 
   /// Logs a message that's included in the next fatal or non-fatal report.
