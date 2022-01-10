6bd88c37a7c01b1c2e6a5e2bab7a8ebc8994fc6d
Bruno Dell <bruno.minuk@gmail.com>
2022-01-10 14:40:51 -0300

    add Portuguese localization support

---
+++ packages/flutterfire_ui/lib/src/i10n/lang/pt.dart
@@ -0,0 +1,266 @@
+import '../default_localizations.dart';
+
+class PtLocalizations extends FlutterFireUILocalizationLabels {
+  @override
+  final String emailInputLabel;
+  @override
+  final String passwordInputLabel;
+  @override
+  final String signInActionText;
+  @override
+  final String registerActionText;
+  @override
+  final String linkEmailButtonText;
+  @override
+  final String signInButtonText;
+  @override
+  final String registerButtonText;
+  @override
+  final String signInWithPhoneButtonText;
+  @override
+  final String signInWithGoogleButtonText;
+  @override
+  final String signInWithAppleButtonText;
+  @override
+  final String signInWithFacebookButtonText;
+  @override
+  final String signInWithTwitterButtonText;
+  @override
+  final String phoneVerificationViewTitleText;
+  @override
+  final String verifyPhoneNumberButtonText;
+  @override
+  final String verifyCodeButtonText;
+  @override
+  final String verifyingPhoneNumberViewTitle;
+  @override
+  final String unknownError;
+  @override
+  final String smsAutoresolutionFailedError;
+  @override
+  final String smsCodeSentText;
+  @override
+  final String sendingSMSCodeText;
+  @override
+  final String verifyingSMSCodeText;
+  @override
+  final String enterSMSCodeText;
+  @override
+  final String emailIsRequiredErrorText;
+  @override
+  final String isNotAValidEmailErrorText;
+  @override
+  final String userNotFoundErrorText;
+  @override
+  final String emailTakenErrorText;
+  @override
+  final String accessDisabledErrorText;
+  @override
+  final String wrongOrNoPasswordErrorText;
+  @override
+  final String signInText;
+  @override
+  final String registerText;
+  @override
+  final String registerHintText;
+  @override
+  final String signInHintText;
+  @override
+  final String signOutButtonText;
+  @override
+  final String phoneInputLabel;
+  @override
+  final String phoneNumberIsRequiredErrorText;
+  @override
+  final String phoneNumberInvalidErrorText;
+  @override
+  final String profile;
+  @override
+  final String name;
+  @override
+  final String deleteAccount;
+  @override
+  final String passwordIsRequiredErrorText;
+  @override
+  final String confirmPasswordIsRequiredErrorText;
+  @override
+  final String confirmPasswordDoesNotMatchErrorText;
+  @override
+  final String confirmPasswordInputLabel;
+  @override
+  final String forgotPasswordButtonLabel;
+  @override
+  final String forgotPasswordViewTitle;
+  @override
+  final String resetPasswordButtonLabel;
+  @override
+  final String verifyItsYouText;
+  @override
+  final String differentMethodsSignInTitleText;
+  @override
+  final String findProviderForEmailTitleText;
+  @override
+  final String continueText;
+  @override
+  final String countryCode;
+  @override
+  final String codeRequiredErrorText;
+  @override
+  final String invalidCountryCode;
+  @override
+  final String chooseACountry;
+  @override
+  final String enableMoreSignInMethods;
+  @override
+  final String signInMethods;
+  @override
+  final String provideEmail;
+  @override
+  final String goBackButtonLabel;
+  @override
+  final String passwordResetEmailSentText;
+  @override
+  final String forgotPasswordHintText;
+  @override
+  final String emailLinkSignInButtonLabel;
+  @override
+  final String signInWithEmailLinkViewTitleText;
+  @override
+  final String signInWithEmailLinkSentText;
+  @override
+  final String sendLinkButtonLabel;
+  @override
+  final String arrayLabel;
+  @override
+  final String booleanLabel;
+  @override
+  final String mapLabel;
+  @override
+  final String nullLabel;
+  @override
+  final String numberLabel;
+  @override
+  final String stringLabel;
+  @override
+  final String typeLabel;
+  @override
+  final String valueLabel;
+  @override
+  final String cancelLabel;
+  @override
+  final String updateLabel;
+  @override
+  final String northInitialLabel;
+  @override
+  final String southInitialLabel;
+  @override
+  final String westInitialLabel;
+  @override
+  final String eastInitialLabel;
+  @override
+  final String timestampLabel;
+  @override
+  final String latitudeLabel;
+  @override
+  final String longitudeLabel;
+  @override
+  final String geopointLabel;
+  @override
+  final String referenceLabel;
+
+  const PtLocalizations({
+    this.emailInputLabel = 'E-mail',
+    this.passwordInputLabel = 'Senha',
+    this.signInActionText = 'Iniciar sessão',
+    this.registerActionText = 'Registrar-se',
+    this.signInButtonText = 'Iniciar sessão',
+    this.registerButtonText = 'Registrar-se',
+    this.linkEmailButtonText = 'Próximo',
+    this.signInWithPhoneButtonText = 'Entrar com telefone',
+    this.signInWithGoogleButtonText = 'Entrar com Google',
+    this.signInWithAppleButtonText = 'Entrar com Apple',
+    this.signInWithTwitterButtonText = 'Entrar com Twitter',
+    this.signInWithFacebookButtonText = 'Entrar com Facebook',
+    this.phoneVerificationViewTitleText = 'Insira seu número de telefone',
+    this.verifyPhoneNumberButtonText = 'Próximo',
+    this.verifyCodeButtonText = 'Verificar',
+    this.verifyingPhoneNumberViewTitle = 'Isira o código SMS',
+    this.unknownError = 'Ocorreu um erro desconhecido',
+    this.smsAutoresolutionFailedError =
+        'Não foi possível identificar o código SMS automaticamente. Por favor, insira seu código manualmente',
+    this.smsCodeSentText = 'Código SMS enviado',
+    this.sendingSMSCodeText = 'Enviando código SMS ...',
+    this.verifyingSMSCodeText = 'Verificando o código SMS ...',
+    this.enterSMSCodeText = 'Insira o código SMS',
+    this.emailIsRequiredErrorText = 'O e-mail é obrigatório',
+    this.isNotAValidEmailErrorText = 'Insira um e-mail válido',
+    this.userNotFoundErrorText = 'Não existe uma conta com este usuário',
+    this.emailTakenErrorText =
+        'Já existe uma conte com este e-mail',
+    this.accessDisabledErrorText =
+        'O acesso a esta conta está temporariamente desativado',
+    this.wrongOrNoPasswordErrorText =
+        'A senha é inválida ou o usuário não possui uma senha',
+    this.signInText = 'Iniciar sessão',
+    this.registerText = 'Registrar-se',
+    this.registerHintText = 'Não tem uma conta?',
+    this.signInHintText = 'Já possui uma conta?',
+    this.signOutButtonText = 'Finalizar sessão',
+    this.phoneInputLabel = 'Número de telefone',
+    this.phoneNumberInvalidErrorText = 'O número de telefone é inválido',
+    this.phoneNumberIsRequiredErrorText =
+        'O número de telefone é obrigatório',
+    this.profile = 'Perfil',
+    this.name = 'Nome',
+    this.deleteAccount = 'Deletar conta',
+    this.passwordIsRequiredErrorText = 'A senha é obrigatória',
+    this.confirmPasswordIsRequiredErrorText = 'Confirme sua senha',
+    this.confirmPasswordDoesNotMatchErrorText = 'As senhas não são iguais',
+    this.confirmPasswordInputLabel = 'Confirme sua senha',
+    this.forgotPasswordButtonLabel = 'Você esqueceu sua senha?',
+    this.forgotPasswordViewTitle = 'Esqueci minha senha',
+    this.resetPasswordButtonLabel = 'Redefinir senha',
+    this.verifyItsYouText = 'Verifique que é você',
+    this.differentMethodsSignInTitleText =
+        'Utilize um dos seguintes métodos para iniciar a sessão',
+    this.findProviderForEmailTitleText =
+        'Insira seu e-mail para continuar',
+    this.continueText = 'Continuar',
+    this.countryCode = 'Código de país',
+    this.codeRequiredErrorText = 'O código do país é obrigatório',
+    this.invalidCountryCode = 'O código do país é obrigatório é inválido',
+    this.chooseACountry = 'Selecione um país',
+    this.enableMoreSignInMethods = 'Habilitar mais métodos de início de sessão',
+    this.signInMethods = 'Métodos de início de sessão',
+    this.provideEmail = 'Digite seu e-mail e senha',
+    this.goBackButtonLabel = 'Voltar',
+    this.passwordResetEmailSentText =
+        'Enviamos um link para seu e-mail para reestabelecer sua senha. Por favor verifique seu e-mail',
+    this.forgotPasswordHintText =
+        'Digite um e-mail e lhe enviaremos um link para redefinir sua senha',
+    this.emailLinkSignInButtonLabel = 'Iniciar sessão com link mágico',
+    this.signInWithEmailLinkViewTitleText = 'Iniciar sessão com link mágico',
+    this.signInWithEmailLinkSentText =
+        'Enviamos um e-mail com um link mágico. Verifique seu e-mail e siga o link para continuar',
+    this.sendLinkButtonLabel = 'Enviar link mágico',
+    this.arrayLabel = 'array',
+    this.booleanLabel = 'boolean',
+    this.mapLabel = 'map',
+    this.nullLabel = 'null',
+    this.numberLabel = 'number',
+    this.stringLabel = 'string',
+    this.typeLabel = 'tipo',
+    this.valueLabel = 'valor',
+    this.cancelLabel = 'cancelar',
+    this.updateLabel = 'atualizar',
+    this.northInitialLabel = 'N',
+    this.southInitialLabel = 'S',
+    this.westInitialLabel = 'O',
+    this.eastInitialLabel = 'E',
+    this.timestampLabel = 'marca de tempo',
+    this.longitudeLabel = 'longitude',
+    this.latitudeLabel = 'latitude',
+    this.geopointLabel = 'geoponto',
+    this.referenceLabel = 'referência',
+  });
+}
