e0b298663cd3e4defc74fffcbc5948291f87e3c5
russellwheatley <russellwheatley85@gmail.com>
2022-01-12 14:15:48 +0000

    chore(analytics): fix analyse issues

--- packages/firebase_analytics/firebase_analytics/example/lib/main.dart
+++ packages/firebase_analytics/firebase_analytics/example/lib/main.dart
@@ -5,7 +5,6 @@
 import 'dart:async';
 
 import 'package:firebase_analytics/firebase_analytics.dart';
-import 'package:firebase_analytics/observer.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
