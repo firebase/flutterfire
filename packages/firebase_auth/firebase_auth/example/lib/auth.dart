dadb364217302f9e8f4063333deda11e8232f69a
pr_Mais <hiimechi@gmail.com>
2022-01-12 20:56:30 +0300

    chore: remove Apple

--- packages/firebase_auth/firebase_auth/example/lib/auth.dart
+++ packages/firebase_auth/firebase_auth/example/lib/auth.dart
@@ -1,8 +1,5 @@
-import 'dart:convert';
 import 'dart:io';
-import 'dart:math';
 
-import 'package:crypto/crypto.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
@@ -10,7 +7,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
 import 'package:github_sign_in/github_sign_in.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 
 typedef OAuthSignIn = void Function();
 
@@ -94,7 +90,6 @@ class _AuthGateState extends State<AuthGate> {
     } else {
       authButtons = {
         if (!Platform.isMacOS) Buttons.Google: _signInWithGoogle,
-        if (Platform.isIOS || Platform.isMacOS) Buttons.Apple: _signInWithApple,
         if (!Platform.isMacOS) Buttons.GitHub: _signInWithGitHub,
       };
     }
@@ -573,62 +568,5 @@ class _AuthGateState extends State<AuthGate> {
     } finally {
       setIsLoading();
     }
-  }
-
-  /// Apple sign in is added to iOS and macOS, but it's possible to setup
-  /// on Android and Web by following the instructions on the plugins README.
-  Future<void> _signInWithApple() async {
-    setIsLoading();
-
-    try {
-      // To prevent replay attacks with the credential returned from Apple, we
-      // include a nonce in the credential request. When signing in with
-      // Firebase, the nonce in the id token returned by Apple, is expected to
-      // match the sha256 hash of `rawNonce`.
-      final rawNonce = generateNonce();
-      final nonce = sha256ofString(rawNonce);
-
-      // Request credential for the currently signed in Apple account.
-      final appleCredential = await SignInWithApple.getAppleIDCredential(
-        scopes: [
-          AppleIDAuthorizationScopes.email,
-          AppleIDAuthorizationScopes.fullName,
-        ],
-        nonce: nonce,
-      );
-
-      // Create an `OAuthCredential` from the credential returned by Apple.
-      final oauthCredential = OAuthProvider('apple.com').credential(
-        idToken: appleCredential.identityToken,
-        rawNonce: rawNonce,
-      );
-
-      // Sign in the user with Firebase. If the nonce we generated earlier does
-      // not match the nonce in `appleCredential.identityToken`, sign in will fail.
-      await _auth.signInWithCredential(oauthCredential);
-    } on FirebaseAuthException catch (e) {
-      setState(() {
-        error = '${e.message}';
-      });
-    } finally {
-      setIsLoading();
-    }
-  }
-
-  /// Generates a cryptographically secure random nonce, to be included in a
-  /// credential request.
-  String generateNonce([int length = 32]) {
-    const charset =
-        '0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._';
-    final random = Random.secure();
-    return List.generate(length, (_) => charset[random.nextInt(charset.length)])
-        .join();
-  }
-
-  /// Returns the sha256 hash of [input] in hex notation.
-  String sha256ofString(String input) {
-    final bytes = utf8.encode(input);
-    final digest = sha256.convert(bytes);
-    return digest.toString();
   }
 }
