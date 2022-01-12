6af0be1e84fdd5abbc492e2411aa96bd438e05ba
pr_Mais <hiimechi@gmail.com>
2022-01-12 21:25:03 +0300

    chore: GitHub config file

---
+++ packages/firebase_auth/firebase_auth/example/lib/github.dart
@@ -0,0 +1,11 @@
+/// To get your [CLIENT_ID] and [CLIENT_SECRET]:
+/// 1. Visit https://github.com/settings/developers.
+/// 2. Create a new OAuth application.
+/// 3. Set **Home Page URL** to `https://react-native-firebase-testing.firebaseapp.com`.
+/// 4. Set **Authorization callback URL** to `https://react-native-firebase-testing.firebaseapp.com/__/auth/handler`.
+Map<String, String> GitHubConfig = {
+  'CLIENT_ID': 'PLACE YOUR CLIENT ID HERE',
+  'CLIENT_SECRET': 'PLACE YOUR CLIENT SECRET HERE',
+  'REDIRECT_URL':
+      'https://react-native-firebase-testing.firebaseapp.com/__/auth/handler',
+};
