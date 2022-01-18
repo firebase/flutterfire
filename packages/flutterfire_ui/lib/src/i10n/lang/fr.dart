4d1de68d3b1540f00174d533483e1e092e9d70a6
Alessandro Rossi <alexodus71@gmail.com>
2022-01-18 21:13:32 +0100

    fix

--- packages/flutterfire_ui/lib/src/i10n/lang/fr.dart
+++ packages/flutterfire_ui/lib/src/i10n/lang/fr.dart
@@ -227,7 +227,7 @@ class FrLocalizations extends FlutterFireUILocalizationLabels {
     this.findProviderForEmailTitleText =
         'Entrez votre adresse email pour continuer',
     this.continueText = 'Continuer',
-    this.countryCode = 'Prefisso internazionale',
+    this.countryCode = 'Code',
     this.codeRequiredErrorText = 'Un code de pays est requis',
     this.invalidCountryCode = 'Code pays invalide',
     this.chooseACountry = 'Choisissez un pays',
