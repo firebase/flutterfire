b19d7b00017ee0879b22c8a8cdd7b20b407c3e44
russellwheatley <russellwheatley85@gmail.com>
2022-02-22 17:18:15 +0000

    fix(firestore): revert TransactionStreamHandler

--- packages/cloud_firestore/cloud_firestore/android/src/main/java/io/flutter/plugins/firebase/firestore/streamhandler/TransactionStreamHandler.java
+++ packages/cloud_firestore/cloud_firestore/android/src/main/java/io/flutter/plugins/firebase/firestore/streamhandler/TransactionStreamHandler.java
@@ -1,7 +1,6 @@
 package io.flutter.plugins.firebase.firestore.streamhandler;
 
-import android.os.Handler;
-import android.os.Looper;
+import android.app.Activity;
 import androidx.annotation.Nullable;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FieldPath;
@@ -20,6 +19,7 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class TransactionStreamHandler implements OnTransactionResultListener, StreamHandler {
 
@@ -28,15 +28,18 @@ public class TransactionStreamHandler implements OnTransactionResultListener, St
     void onStarted(Transaction transaction);
   }
 
+  final AtomicReference<Activity> activityRef;
   final OnTransactionStartedListener onTransactionStartedListener;
 
-  public TransactionStreamHandler(OnTransactionStartedListener onTransactionStartedListener) {
+  public TransactionStreamHandler(
+    AtomicReference<Activity> activityRef,
+    OnTransactionStartedListener onTransactionStartedListener) {
+    this.activityRef = activityRef;
     this.onTransactionStartedListener = onTransactionStartedListener;
   }
 
   final Semaphore semaphore = new Semaphore(0);
   final Map<String, Object> response = new HashMap<>();
-  final Handler mainLooper = new Handler(Looper.getMainLooper());
 
   @Override
   public void onListen(Object arguments, EventSink events) {
@@ -44,7 +47,7 @@ public class TransactionStreamHandler implements OnTransactionResultListener, St
     Map<String, Object> argumentsMap = (Map<String, Object>) arguments;
 
     FirebaseFirestore firestore =
-        (FirebaseFirestore) Objects.requireNonNull(argumentsMap.get("firestore"));
+      (FirebaseFirestore) Objects.requireNonNull(argumentsMap.get("firestore"));
 
     Object value = argumentsMap.get("timeout");
     Long timeout;
@@ -58,100 +61,97 @@ public class TransactionStreamHandler implements OnTransactionResultListener, St
     }
 
     firestore
-        .runTransaction(
-            transaction -> {
-              onTransactionStartedListener.onStarted(transaction);
+      .runTransaction(
+        transaction -> {
+          onTransactionStartedListener.onStarted(transaction);
 
-              Map<String, Object> attemptMap = new HashMap<>();
-              attemptMap.put("appName", firestore.getApp().getName());
+          Map<String, Object> attemptMap = new HashMap<>();
+          attemptMap.put("appName", firestore.getApp().getName());
 
-              mainLooper.post(() -> events.success(attemptMap));
+          activityRef.get().runOnUiThread(() -> events.success(attemptMap));
 
-              try {
-                if (!semaphore.tryAcquire(timeout, TimeUnit.MILLISECONDS)) {
-                  return FlutterFirebaseFirestoreTransactionResult.failed(
-                      new FirebaseFirestoreException("timed out", Code.DEADLINE_EXCEEDED));
-                }
-              } catch (InterruptedException e) {
-                return FlutterFirebaseFirestoreTransactionResult.failed(
-                    new FirebaseFirestoreException("interrupted", Code.DEADLINE_EXCEEDED));
-              }
+          try {
+            if (!semaphore.tryAcquire(timeout, TimeUnit.MILLISECONDS)) {
+              return FlutterFirebaseFirestoreTransactionResult.failed(
+                new FirebaseFirestoreException("timed out", Code.DEADLINE_EXCEEDED));
+            }
+          } catch (InterruptedException e) {
+            return FlutterFirebaseFirestoreTransactionResult.failed(
+              new FirebaseFirestoreException("interrupted", Code.DEADLINE_EXCEEDED));
+          }
 
-              if (response.isEmpty()) {
-                return FlutterFirebaseFirestoreTransactionResult.complete();
-              }
-              final String resultType = (String) response.get("type");
+          if (response.isEmpty()) {
+            return FlutterFirebaseFirestoreTransactionResult.complete();
+          }
+          final String resultType = (String) response.get("type");
 
-              if ("ERROR".equalsIgnoreCase(resultType)) {
-                return FlutterFirebaseFirestoreTransactionResult.complete();
-              }
+          if ("ERROR".equalsIgnoreCase(resultType)) {
+            return FlutterFirebaseFirestoreTransactionResult.complete();
+          }
 
-              @SuppressWarnings("unchecked")
-              List<Map<String, Object>> commands =
-                  (List<Map<String, Object>>) response.get("commands");
+          @SuppressWarnings("unchecked")
+          List<Map<String, Object>> commands =
+            (List<Map<String, Object>>) response.get("commands");
 
-              for (Map<String, Object> command : commands) {
-                String type = (String) Objects.requireNonNull(command.get("type"));
-                String path = (String) Objects.requireNonNull(command.get("path"));
-                DocumentReference documentReference = firestore.document(path);
+          for (Map<String, Object> command : commands) {
+            String type = (String) Objects.requireNonNull(command.get("type"));
+            String path = (String) Objects.requireNonNull(command.get("path"));
+            DocumentReference documentReference = firestore.document(path);
 
+            @SuppressWarnings("unchecked")
+            Map<String, Object> data = (Map<String, Object>) command.get("data");
+
+            switch (type) {
+              case "DELETE":
+                transaction.delete(documentReference);
+                break;
+              case "UPDATE":
+                transaction.update(documentReference, Objects.requireNonNull(data));
+                break;
+              case "SET":
+              {
                 @SuppressWarnings("unchecked")
-                Map<String, Object> data = (Map<String, Object>) command.get("data");
+                Map<String, Object> options =
+                  (Map<String, Object>) Objects.requireNonNull(command.get("options"));
+                SetOptions setOptions = null;
 
-                switch (type) {
-                  case "DELETE":
-                    transaction.delete(documentReference);
-                    break;
-                  case "UPDATE":
-                    transaction.update(documentReference, Objects.requireNonNull(data));
-                    break;
-                  case "SET":
-                    {
-                      @SuppressWarnings("unchecked")
-                      Map<String, Object> options =
-                          (Map<String, Object>) Objects.requireNonNull(command.get("options"));
-                      SetOptions setOptions = null;
-
-                      if (options.get("merge") != null && (boolean) options.get("merge")) {
-                        setOptions = SetOptions.merge();
-                      } else if (options.get("mergeFields") != null) {
-                        @SuppressWarnings("unchecked")
-                        List<FieldPath> fieldPathList =
-                            (List<FieldPath>) Objects.requireNonNull(options.get("mergeFields"));
-                        setOptions = SetOptions.mergeFieldPaths(fieldPathList);
-                      }
-
-                      if (setOptions == null) {
-                        transaction.set(documentReference, Objects.requireNonNull(data));
-                      } else {
-                        transaction.set(
-                            documentReference, Objects.requireNonNull(data), setOptions);
-                      }
-
-                      break;
-                    }
+                if (options.get("merge") != null && (boolean) options.get("merge")) {
+                  setOptions = SetOptions.merge();
+                } else if (options.get("mergeFields") != null) {
+                  @SuppressWarnings("unchecked")
+                  List<FieldPath> fieldPathList =
+                    (List<FieldPath>) Objects.requireNonNull(options.get("mergeFields"));
+                  setOptions = SetOptions.mergeFieldPaths(fieldPathList);
                 }
-              }
-              return FlutterFirebaseFirestoreTransactionResult.complete();
-            })
-        .addOnCompleteListener(
-            task -> {
-              final HashMap<String, Object> map = new HashMap<>();
-              if (task.getException() != null || task.getResult().exception != null) {
-                final @Nullable Exception exception =
-                    task.getException() != null ? task.getException() : task.getResult().exception;
-                map.put("appName", firestore.getApp().getName());
-                map.put("error", ExceptionConverter.createDetails(exception));
-              } else if (task.getResult() != null) {
-                map.put("complete", true);
-              }
 
-              mainLooper.post(
-                  () -> {
-                    events.success(map);
-                    events.endOfStream();
-                  });
-            });
+                if (setOptions == null) {
+                  transaction.set(documentReference, Objects.requireNonNull(data));
+                } else {
+                  transaction.set(
+                    documentReference, Objects.requireNonNull(data), setOptions);
+                }
+
+                break;
+              }
+            }
+          }
+          return FlutterFirebaseFirestoreTransactionResult.complete();
+        })
+      .addOnCompleteListener(
+        task -> {
+          final HashMap<String, Object> map = new HashMap<>();
+          if (task.getException() != null || task.getResult().exception != null) {
+            final @Nullable Exception exception =
+              task.getException() != null ? task.getException() : task.getResult().exception;
+            map.put("appName", firestore.getApp().getName());
+            map.put("error", ExceptionConverter.createDetails(exception));
+          } else if (task.getResult() != null) {
+            map.put("complete", true);
+          }
+
+          activityRef.get().runOnUiThread(() -> events.success(map));
+          activityRef.get().runOnUiThread(events::endOfStream);
+        });
   }
 
   @Override
