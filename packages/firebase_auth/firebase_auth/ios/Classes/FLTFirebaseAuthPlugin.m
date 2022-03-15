7053804a810c7c88dab7a38eb85e0ebdd5aa6953
Russell Wheatley <russellwheatley85@gmail.com>
2022-03-15 13:45:14 +0000

    Update packages/firebase_auth/firebase_auth/ios/Classes/FLTFirebaseAuthPlugin.m
    Co-authored-by: Mike Diarmid <mike.diarmid@gmail.com>

--- packages/firebase_auth/firebase_auth/ios/Classes/FLTFirebaseAuthPlugin.m
+++ packages/firebase_auth/firebase_auth/ios/Classes/FLTFirebaseAuthPlugin.m
@@ -1295,9 +1295,12 @@ NSString *const kErrMsgInvalidCredential =
 }
 
 - (void)ensureAPNSTokenSetting {
-  if ([FIRAuth auth].APNSToken == nil && _apnsToken != nil) {
-    [[FIRAuth auth] setAPNSToken:_apnsToken type:FIRAuthAPNSTokenTypeUnknown];
-    _apnsToken = nil;
+  FIRApp *defaultApp = [FIRApp defaultApp];
+  if (defaultApp) {
+    if ([FIRAuth auth].APNSToken == nil && _apnsToken != nil) {
+      [[FIRAuth auth] setAPNSToken:_apnsToken type:FIRAuthAPNSTokenTypeUnknown];
+      _apnsToken = nil;
+    }
   }
 }
 
