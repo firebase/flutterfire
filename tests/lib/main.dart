9e86aa32afcd360dec276ed80d1413aa63511453
Salakar <mike.diarmid@gmail.com>
2022-03-31 13:08:07 +0100

    chore: commit generated files/changes

--- tests/lib/main.dart
+++ tests/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import 'firebase_default_options.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
