3e884f2f7cb498c6dff23ff6ac2bd9a25a73034d
Matthew Shipton <ssabdb@users.noreply.github.com>
2022-03-31 16:36:50 +0100

    feat(ui): Allow setting `resizeToAvoidBottomInset` from LoginScreen and set as default `false` for backwards compatibility. (#8365)
    Co-authored-by: Mike Diarmid <mike.diarmid@gmail.com>
    Co-authored-by: russellwheatley <russellwheatley85@gmail.com>

--- packages/flutterfire_ui/lib/src/auth/screens/sign_in_screen.dart
+++ packages/flutterfire_ui/lib/src/auth/screens/sign_in_screen.dart
@@ -19,6 +19,7 @@ class SignInScreen extends MultiProviderScreen {
   final OAuthButtonVariant? oauthButtonVariant;
   final TextDirection? desktopLayoutDirection;
   final String? email;
+  final bool? resizeToAvoidBottomInset;
   final bool? showAuthActionSwitch;
   final AuthViewContentBuilder? subtitleBuilder;
   final AuthViewContentBuilder? footerBuilder;
@@ -35,6 +36,7 @@ class SignInScreen extends MultiProviderScreen {
     this.sideBuilder,
     this.oauthButtonVariant = OAuthButtonVariant.icon_and_text,
     this.desktopLayoutDirection,
+    this.resizeToAvoidBottomInset = false,
     this.showAuthActionSwitch,
     this.email,
     this.subtitleBuilder,
@@ -86,6 +88,7 @@ class SignInScreen extends MultiProviderScreen {
         desktopLayoutDirection: desktopLayoutDirection,
         oauthButtonVariant: oauthButtonVariant,
         email: email,
+        resizeToAvoidBottomInset: resizeToAvoidBottomInset,
         showAuthActionSwitch: showAuthActionSwitch,
         subtitleBuilder: subtitleBuilder,
         footerBuilder: footerBuilder,
