b6563d467b8b01c2392db3e17410d8e71f604bfd
Remi Rousselet <darky12s@gmail.com>
2022-04-19 08:59:29 +0200

    ci: enable Firestore ODM e2e tests (#8468)

--- packages/cloud_firestore_odm/cloud_firestore_odm/example/test_driver/query_reference_test.dart
+++ packages/cloud_firestore_odm/cloud_firestore_odm/example/test_driver/query_reference_test.dart
@@ -9,31 +9,17 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('QueryReference', () {
-    late FirebaseFirestore defaultFirestore;
     late FirebaseFirestore customFirestore;
 
     setUpAll(() async {
-      defaultFirestore = FirebaseFirestore.instanceFor(
-        app: await Firebase.initializeApp(
-          options: const FirebaseOptions(
-            apiKey: 'AIzaSyAHAsf51D0A407EklG1bs-5wA7EbyfNFg0',
-            appId: '1:448618578101:ios:3a3c8ae9cb0b6408ac3efc',
-            messagingSenderId: '448618578101',
-            projectId: 'react-native-firebase-testing',
-            authDomain: 'react-native-firebase-testing.firebaseapp.com',
-            iosClientId:
-                '448618578101-m53gtqfnqipj12pts10590l37npccd2r.apps.googleusercontent.com',
-          ),
-        ),
-      );
       customFirestore = FirebaseFirestore.instanceFor(
         app: await Firebase.initializeApp(
           name: 'custom-query-app',
           options: FirebaseOptions(
-            apiKey: defaultFirestore.app.options.apiKey,
-            appId: defaultFirestore.app.options.appId,
-            messagingSenderId: defaultFirestore.app.options.messagingSenderId,
-            projectId: defaultFirestore.app.options.projectId,
+            apiKey: Firebase.app().options.apiKey,
+            appId: Firebase.app().options.appId,
+            messagingSenderId: Firebase.app().options.messagingSenderId,
+            projectId: Firebase.app().options.projectId,
           ),
         ),
       );
@@ -43,7 +29,7 @@ void main() {
       test('overrides ==', () {
         expect(
           MovieCollectionReference().limit(1),
-          MovieCollectionReference(defaultFirestore).limit(1),
+          MovieCollectionReference(FirebaseFirestore.instance).limit(1),
         );
         expect(
           MovieCollectionReference().limit(1),
@@ -65,7 +51,7 @@ void main() {
       test('overrides ==', () {
         expect(
           MovieCollectionReference().doc('123').comments.limit(1),
-          MovieCollectionReference(defaultFirestore)
+          MovieCollectionReference(FirebaseFirestore.instance)
               .doc('123')
               .comments
               .limit(1),
