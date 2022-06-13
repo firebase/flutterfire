150fd56cb91bc30b9cc38cc339fcb4ee56ee035e
Mike Diarmid <mike.diarmid@gmail.com>
2022-06-13 15:35:18 +0100

    Restore driver_e2e.dart

--- tests/test_driver/driver_e2e.dart
+++ tests/test_driver/driver_e2e.dart
@@ -28,19 +28,19 @@ void setupTests() {
   // Core first.
   firebase_core.setupTests();
   // All other tests.
-  // firebase_auth.setupTests();
-  // cloud_functions.setupTests();
-  // firebase_storage.setupTests();
+  firebase_auth.setupTests();
+  cloud_functions.setupTests();
+  firebase_storage.setupTests();
   firebase_database.setupTests();
-  // firebase_app_check.setupTests();
-  // firebase_messaging.setupTests();
-  // firebase_analytics.setupTests();
-  // firebase_crashlytics.setupTests();
-  // firebase_dynamic_links.setupTests();
-  // firebase_remote_config.setupTests();
-  // firebase_in_app_messaging.setupTests();
-  // firebase_app_installations.setupTests();
-  // firebase_ml_model_downloader.setupTests();
+  firebase_app_check.setupTests();
+  firebase_messaging.setupTests();
+  firebase_analytics.setupTests();
+  firebase_crashlytics.setupTests();
+  firebase_dynamic_links.setupTests();
+  firebase_remote_config.setupTests();
+  firebase_in_app_messaging.setupTests();
+  firebase_app_installations.setupTests();
+  firebase_ml_model_downloader.setupTests();
 }
 
 void main() => drive.main(setupTests);
