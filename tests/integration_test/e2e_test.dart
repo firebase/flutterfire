ef247e01f6eae0c63ef4cfa186ce2b07bc830013
nilsreichardt <nils@reichardt.io>
2022-06-14 11:18:06 +0200

    Use `IntegrationTestWidgetsFlutterBinding.ensureInitialized();`

--- tests/integration_test/e2e_test.dart
+++ tests/integration_test/e2e_test.dart
@@ -1,6 +1,9 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
 
 void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
   group('FlutterFire', () {
     test('dummy test', () {
       expect(true, true);
