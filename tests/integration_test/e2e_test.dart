1ac5278785364aae85599b4d30f63e273852b2ec
nilsreichardt <nils@reichardt.io>
2022-06-22 01:54:13 +0200

    test(firebase_analytics): migrate to `integration_test` pacakge

--- tests/integration_test/e2e_test.dart
+++ tests/integration_test/e2e_test.dart
@@ -1,12 +1,14 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
+import 'firebase_analytics/firebase_analytics_e2e_test.dart'
+    as firebase_analytics;
+
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
+  // ignore: unnecessary_lambdas
   group('FlutterFire', () {
-    test('dummy test', () {
-      expect(true, true);
-    });
+    firebase_analytics.main();
   });
 }
