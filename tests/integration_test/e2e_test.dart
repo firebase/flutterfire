e453303fe27fe83a8305ba59a050173c020bda2a
Nils Reichardt <nils@reichardt.io>
2022-06-23 09:02:25 +0200

    test(firebase_core): migrate to `integration_test` package (#8928)
    * Migrate `firebase_core`
    
    * Add license in `e2e_test.dart`
    
    * Add `IntegrationTestWidgetsFlutterBinding.ensureInitialized();` to `firebase_core_e2e_test.dart`
    
    * Use `flutter_test` instead of `drive` package
    
    * Fix `firebase_options.dart` file

--- tests/integration_test/e2e_test.dart
+++ tests/integration_test/e2e_test.dart
@@ -1,12 +1,17 @@
+// Copyright 2019, the Chromium project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
+
+import 'firebase_core/firebase_core_e2e_test.dart' as firebase_core;
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
+  // ignore: unnecessary_lambdas
   group('FlutterFire', () {
-    test('dummy test', () {
-      expect(true, true);
-    });
+    firebase_core.main();
   });
 }
