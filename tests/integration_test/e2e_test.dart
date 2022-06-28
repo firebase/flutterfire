ceabc55a9579e4a5c89f88301b393b8ba901659b
Nils Reichardt <nils@reichardt.io>
2022-06-28 14:03:30 +0200

    test(firebase_app_installations): migrate to `integration_test` package (#8947)
    Co-authored-by: Russell Wheatley <russellwheatley85@gmail.com>

--- tests/integration_test/e2e_test.dart
+++ tests/integration_test/e2e_test.dart
@@ -5,6 +5,8 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
+import 'firebase_app_installations/firebase_app_installations_e2e_test.dart'
+    as firebase_app_installations;
 import 'firebase_analytics/firebase_analytics_e2e_test.dart'
     as firebase_analytics;
 import 'firebase_core/firebase_core_e2e_test.dart' as firebase_core;
@@ -13,6 +15,7 @@ void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   group('FlutterFire', () {
+    firebase_app_installations.main();
     firebase_analytics.main();
     firebase_core.main();
   });
