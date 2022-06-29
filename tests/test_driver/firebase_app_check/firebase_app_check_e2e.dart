aeabebb37e3a02b32e8695199ceaa29fd9f1d13e
russellwheatley <russellwheatley85@gmail.com>
2022-06-29 12:08:18 +0100

    check if app check still doesn't work

--- tests/test_driver/firebase_app_check/firebase_app_check_e2e.dart
+++ tests/test_driver/firebase_app_check/firebase_app_check_e2e.dart
@@ -3,11 +3,11 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'package:firebase_core/firebase_core.dart';
-// import 'package:firebase_app_check/firebase_app_check.dart';
+import 'package:firebase_app_check/firebase_app_check.dart';
 
 import 'package:drive/drive.dart';
 import '../firebase_default_options.dart';
-// import 'package:flutter/foundation.dart';
+import 'package:flutter/foundation.dart';
 
 // TODO RTDB emulator breaks just by including App Check
 void setupTests() {
@@ -20,48 +20,48 @@ void setupTests() {
         );
       });
 
-      // test('activate', () async {
-      //   await expectLater(
-      //     FirebaseAppCheck.instance.activate(
-      //       webRecaptchaSiteKey: '6Lemcn0dAAAAABLkf6aiiHvpGD6x-zF3nOSDU2M8',
-      //     ),
-      //     completes,
-      //   );
-      // });
+      test('activate', () async {
+        await expectLater(
+          FirebaseAppCheck.instance.activate(
+            webRecaptchaSiteKey: '6Lemcn0dAAAAABLkf6aiiHvpGD6x-zF3nOSDU2M8',
+          ),
+          completes,
+        );
+      });
 
-      // test(
-      //   'getToken',
-      //   () async {
-      //     final token = await FirebaseAppCheck.instance.getToken(true);
-      //     expect(token, isA<String?>());
-      //   },
-      //   // TODO why is this Android/Web only?
-      //   skip: defaultTargetPlatform == TargetPlatform.iOS ||
-      //       defaultTargetPlatform == TargetPlatform.macOS,
-      // );
+      test(
+        'getToken',
+        () async {
+          final token = await FirebaseAppCheck.instance.getToken(true);
+          expect(token, isA<String?>());
+        },
+        // TODO why is this Android/Web only?
+        skip: defaultTargetPlatform == TargetPlatform.iOS ||
+            defaultTargetPlatform == TargetPlatform.macOS,
+      );
 
-      // test(
-      //   'setTokenAutoRefreshEnabled',
-      //   () async {
-      //     await expectLater(
-      //       FirebaseAppCheck.instance.setTokenAutoRefreshEnabled(true),
-      //       completes,
-      //     );
-      //   },
-      // );
+      test(
+        'setTokenAutoRefreshEnabled',
+        () async {
+          await expectLater(
+            FirebaseAppCheck.instance.setTokenAutoRefreshEnabled(true),
+            completes,
+          );
+        },
+      );
 
-      // test('tokenChanges', () async {
-      //   final stream = FirebaseAppCheck.instance.onTokenChange;
-      //   expect(stream, isA<Stream>());
+      test('tokenChanges', () async {
+        final stream = FirebaseAppCheck.instance.onTokenChange;
+        expect(stream, isA<Stream>());
 
-      //   // TODO how to trigger event listener in e2e tests?
-      //   // await FirebaseAppCheck.instance.getToken(true);
-      //   //
-      //   // final result = await stream.first;
-      //   //
-      //   // expect(result, isA<AppCheckTokenResult>());
-      //   // expect(result.token, isA<String>());
-      // });
+        // TODO how to trigger event listener in e2e tests?
+        // await FirebaseAppCheck.instance.getToken(true);
+        //
+        // final result = await stream.first;
+        //
+        // expect(result, isA<AppCheckTokenResult>());
+        // expect(result.token, isA<String>());
+      });
     },
   );
 }
