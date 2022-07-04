f989a500a5b9efc29403c0d50276242d76d8b09b
Nils Reichardt <nils@reichardt.io>
2022-07-04 13:37:20 +0200

    test(firebase_crashlytics): migrate tests to `integration_test` package (#8973)
    Co-authored-by: Russell Wheatley <russellwheatley85@gmail.com>

--- tests/integration_test/e2e_test.dart
+++ tests/integration_test/e2e_test.dart
@@ -11,6 +11,8 @@ import 'firebase_app_installations/firebase_app_installations_e2e_test.dart'
 import 'firebase_analytics/firebase_analytics_e2e_test.dart'
     as firebase_analytics;
 import 'firebase_core/firebase_core_e2e_test.dart' as firebase_core;
+import 'firebase_crashlytics/firebase_crashlytics_e2e_test.dart'
+    as firebase_crashlytics;
 import 'firebase_auth/firebase_auth_e2e_test.dart' as firebase_auth;
 
 void main() {
@@ -21,6 +23,7 @@ void main() {
     firebase_app_installations.main();
     firebase_analytics.main();
     firebase_core.main();
+    firebase_crashlytics.main();
     firebase_auth.main();
   });
 }
