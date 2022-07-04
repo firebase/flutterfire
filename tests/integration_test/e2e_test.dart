573c298abb3ede510f6e51ced421e65e046e10e0
Nils Reichardt <nils@reichardt.io>
2022-07-04 08:59:42 +0200

    test(cloud_functions): migrate to `integration_test` package (#8938)
    * Add `sample_data.dart`
    
    * Add `cloud_functions_e2e_test.dart`
    
    * Use cloud functions tests
    
    * Remove ignore lint comment
    
    * Re-trigger CI
    
    Co-authored-by: Russell Wheatley <russellwheatley85@gmail.com>

--- tests/integration_test/e2e_test.dart
+++ tests/integration_test/e2e_test.dart
@@ -5,6 +5,7 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
+import 'cloud_functions/cloud_functions_e2e_test.dart' as cloud_functions;
 import 'firebase_app_installations/firebase_app_installations_e2e_test.dart'
     as firebase_app_installations;
 import 'firebase_analytics/firebase_analytics_e2e_test.dart'
@@ -15,6 +16,7 @@ void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   group('FlutterFire', () {
+    cloud_functions.main();
     firebase_app_installations.main();
     firebase_analytics.main();
     firebase_core.main();
