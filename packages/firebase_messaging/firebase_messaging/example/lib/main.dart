719ac78f85b6c1f7dda54b96a64fee00de691329
russellwheatley <russellwheatley85@gmail.com>
2022-07-14 12:17:17 +0100

    chore(messaging): format

--- packages/firebase_messaging/firebase_messaging/example/lib/main.dart
+++ packages/firebase_messaging/firebase_messaging/example/lib/main.dart
@@ -45,7 +45,8 @@ Future<void> main() async {
     channel = const AndroidNotificationChannel(
       'high_importance_channel', // id
       'High Importance Notifications', // title
-      description: 'This channel is used for important notifications.', // description
+      description:
+          'This channel is used for important notifications.', // description
       importance: Importance.high,
     );
 
