667d684c4433624603ae0127c4b987505b7c5005
russellwheatley <russellwheatley85@gmail.com>
2022-08-17 10:09:36 +0100

    test(firestore): make note on skipped test that is timing out

--- packages/cloud_firestore/cloud_firestore/example/test_driver/second_app_e2e.dart
+++ packages/cloud_firestore/cloud_firestore/example/test_driver/second_app_e2e.dart
@@ -25,26 +25,31 @@ void runSecondAppTests() {
       );
     });
 
-    group('Secondary app Firestore instance', () {
-      test(
-          'Second Firestore instance should fail due to firestore.rules forbidding data writes',
-          () async {
-        // successful write on default app instance
-        await firestore
-            .collection('flutter-tests/banned/doc')
-            .add({'foo': 'bar'});
-
-        // permission denied on second app with Firebase that denies database writes
-        await expectLater(
-          secondFirestoreProject
+    group(
+      'Secondary app Firestore instance',
+      () {
+        test(
+            'Second Firestore instance should fail due to firestore.rules forbidding data writes',
+            () async {
+          // successful write on default app instance
+          await firestore
               .collection('flutter-tests/banned/doc')
-              .add({'foo': 'bar'}),
-          throwsA(
-            isA<FirebaseException>()
-                .having((e) => e.code, 'code', 'permission-denied'),
-          ),
-        );
-      });
-    },skip: defaultTargetPlatform == TargetPlatform.android);
+              .add({'foo': 'bar'});
+
+          // permission denied on second app with Firebase that denies database writes
+          await expectLater(
+            secondFirestoreProject
+                .collection('flutter-tests/banned/doc')
+                .add({'foo': 'bar'}),
+            throwsA(
+              isA<FirebaseException>()
+                  .having((e) => e.code, 'code', 'permission-denied'),
+            ),
+          );
+        });
+      },
+      // Skip on android because the test continually times out on the CI. The test passes when running locally.
+      skip: defaultTargetPlatform == TargetPlatform.android,
+    );
   });
 }
