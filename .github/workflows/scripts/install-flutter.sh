8f09c234a365d38e1f1776d5a99fb3c9e7ac1828
nilsreichardt <hi@nils.re>
2022-09-06 11:15:14 +0200

    Add `source $GITHUB_PATH`

--- .github/workflows/scripts/install-flutter.sh
+++ .github/workflows/scripts/install-flutter.sh
@@ -3,4 +3,6 @@
 BRANCH=$1
 git clone https://github.com/flutter/flutter.git --depth 1 -b $BRANCH "$GITHUB_WORKSPACE/_flutter"
 echo "$GITHUB_WORKSPACE/_flutter/bin" >> $GITHUB_PATH
+
+source $GITHUB_PATH
 flutter --version
