b513fb74355e4dd2734e60229b4ba1a97b97237c
Guillaume Bernos <guillaume@bernos.dev>
2022-09-06 14:59:14 +0200

    ci: fix CI issues related to Flutter 3.3 release

--- packages/cloud_functions/cloud_functions/lib/cloud_functions.dart
+++ packages/cloud_functions/cloud_functions/lib/cloud_functions.dart
@@ -6,6 +6,9 @@
 library cloud_functions;
 
 import 'dart:async';
+// TODO(Lyokone): remove once we bump Flutter SDK min version to 3.3
+// ignore: unnecessary_import
+import 'dart:typed_data';
 
 import 'package:cloud_functions_platform_interface/cloud_functions_platform_interface.dart';
 import 'package:firebase_core/firebase_core.dart';
