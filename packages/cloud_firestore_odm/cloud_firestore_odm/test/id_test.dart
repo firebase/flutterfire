f715c73bb948f79298fd35a75d74a009b92b0e37
Remi Rousselet <darky12s@gmail.com>
2022-09-22 17:07:29 +0200

    Remove dead code

--- packages/cloud_firestore_odm/cloud_firestore_odm/test/id_test.dart
+++ packages/cloud_firestore_odm/cloud_firestore_odm/test/id_test.dart
@@ -6,41 +6,4 @@ void main() {
     // ignore: prefer_const_constructors, coverage for the const constructor
     Id();
   });
-
-  test('JsonMapWithId', () {
-    expect(
-      $JsonMapWithId({'a': 42}, '123', 'id'),
-      {'a': 42, 'id': '123'},
-    );
-
-    expect(
-      $JsonMapWithId({'b': 21}, '456', 'foo'),
-      {'b': 21, 'foo': '456'},
-    );
-
-    final map = {'a': 21};
-    final mapWithId = $JsonMapWithId(map, '123', 'id');
-
-    mapWithId['a'] = 42;
-
-    expect(map, {'a': 42});
-
-    expect(
-      () => mapWithId['id'] = '456',
-      throwsUnsupportedError,
-    );
-
-    expect(
-      mapWithId.containsValue('123'),
-      true,
-    );
-    expect(
-      mapWithId.containsValue(42),
-      true,
-    );
-    expect(
-      mapWithId.containsValue(21),
-      false,
-    );
-  });
 }
