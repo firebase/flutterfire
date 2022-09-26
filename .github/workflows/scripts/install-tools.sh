e5b8d235a12f5888c22f10b93c58cf91c4ea47de
russellwheatley <russellwheatley85@gmail.com>
2022-09-26 16:33:56 +0100

    ci(macos): fix

--- .github/workflows/scripts/install-tools.sh
+++ .github/workflows/scripts/install-tools.sh
@@ -3,7 +3,6 @@
 flutter config --no-analytics
 flutter pub global activate melos 2.4.0
 flutter pub global activate flutter_plugin_tools
-echo "/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin" >> $GITHUB_PATH
 echo "$HOME/.pub-cache/bin" >> $GITHUB_PATH
 echo "$GITHUB_WORKSPACE/_flutter/.pub-cache/bin" >> $GITHUB_PATH
 echo "$GITHUB_WORKSPACE/_flutter/bin/cache/dart-sdk/bin" >> $GITHUB_PATH
