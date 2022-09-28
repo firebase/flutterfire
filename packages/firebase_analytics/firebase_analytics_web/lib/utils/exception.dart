efbf358938e30ccd2c5730a67688106e739facec
Remi Rousselet <darky12s@gmail.com>
2022-09-28 15:01:14 +0200

    chore: Add _fluttefire_internals and extract firebase_core/src/internals to the new package.

--- packages/firebase_analytics/firebase_analytics_web/lib/utils/exception.dart
+++ packages/firebase_analytics/firebase_analytics_web/lib/utils/exception.dart
@@ -1,8 +1,6 @@
-export 'package:firebase_core/src/internals.dart' hide guardWebExceptions;
-
 import 'package:firebase_core/firebase_core.dart';
-// ignore: implementation_imports
-import 'package:firebase_core/src/internals.dart' as internals;
+import 'package:_flutterfire_internals/_flutterfire_internals.dart'
+    as internals;
 
 /// Will return a [FirebaseException] from a thrown web error.
 /// Any other errors will be propagated as normal.
