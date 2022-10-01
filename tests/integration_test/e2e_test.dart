8cd393a7163a41ae201097a8af4537add595e194
nilsreichardt <hi@nils.re>
2022-10-01 11:54:24 +0200

    Migrate `firebase_analytics`

--- tests/integration_test/e2e_test.dart
+++ tests/integration_test/e2e_test.dart
@@ -2,11 +2,14 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
 import 'firebase_core/firebase_core_e2e_test.dart' as firebase_core;
+import 'firebase_analytics/firebase_analytics_e2e_test.dart'
+    as firebase_analytics;
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   group('FlutterFire', () {
     firebase_core.main();
+    firebase_analytics.main();
   });
 }
