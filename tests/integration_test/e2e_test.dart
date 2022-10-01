f740e69477d41737e912e0c1752682e20f785ca4
nilsreichardt <hi@nils.re>
2022-10-01 11:56:16 +0200

    Migrate `cloud_functions`

--- tests/integration_test/e2e_test.dart
+++ tests/integration_test/e2e_test.dart
@@ -4,6 +4,7 @@ import 'package:integration_test/integration_test.dart';
 import 'firebase_core/firebase_core_e2e_test.dart' as firebase_core;
 import 'firebase_analytics/firebase_analytics_e2e_test.dart'
     as firebase_analytics;
+import 'cloud_functions/cloud_functions_e2e_test.dart' as cloud_functions;
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
@@ -11,5 +12,6 @@ void main() {
   group('FlutterFire', () {
     firebase_core.main();
     firebase_analytics.main();
+    cloud_functions.main();
   });
 }
