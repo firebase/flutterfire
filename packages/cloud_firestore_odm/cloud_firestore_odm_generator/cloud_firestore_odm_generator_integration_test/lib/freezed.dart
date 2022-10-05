0b6b4970b34011ca0f28d9e96271c6986862f339
Rexios <rexios@rexios.dev>
2022-10-05 18:49:43 -0400

    Ignore prefer_single_quotes too since enforcing that rule for generated files is a whole big mess

--- packages/cloud_firestore_odm/cloud_firestore_odm_generator/cloud_firestore_odm_generator_integration_test/lib/freezed.dart
+++ packages/cloud_firestore_odm/cloud_firestore_odm_generator/cloud_firestore_odm_generator_integration_test/lib/freezed.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: invalid_annotation_target
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cloud_firestore_odm/cloud_firestore_odm.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
