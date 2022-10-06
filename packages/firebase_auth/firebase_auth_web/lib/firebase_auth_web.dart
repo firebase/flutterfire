a4d68050070f814764b0c625811127c008aa1a6f
russellwheatley <russellwheatley85@gmail.com>
2022-10-06 11:41:16 +0100

    chore(auth, web): rm print

--- packages/firebase_auth/firebase_auth_web/lib/firebase_auth_web.dart
+++ packages/firebase_auth/firebase_auth_web/lib/firebase_auth_web.dart
@@ -404,9 +404,6 @@ class FirebaseAuthWeb extends FirebaseAuthPlatform {
     } catch (e) {
       final String code = (e as auth_interop.AuthError).code;
       // this catches Firebase Error from web that occurs after hot reloading & hot restarting
-      // ignore: avoid_print
-      print(
-          "Web auth emulator throws exception on hot restart/reload (Web JS SDK thinks you're calling emulator multiple times). We're hiding the exception for your convenience.");
       if (code != 'auth/emulator-config-failed') {
         throw getFirebaseAuthException(e);
       }
