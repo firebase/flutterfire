1e7531b789b9cbeba1519d4435d62e1ca8007ad6
russellwheatley <russellwheatley85@gmail.com>
2022-10-07 09:28:46 +0100

    chore(app-check): Deprecated label to safety net

--- packages/firebase_app_check/firebase_app_check_platform_interface/lib/src/android_provider.dart
+++ packages/firebase_app_check/firebase_app_check_platform_interface/lib/src/android_provider.dart
@@ -3,6 +3,7 @@ enum AndroidProvider {
   // The debug provider
   debug,
   // The safety net provider (deprecated)
+  @Deprecated('Safety Net provider is deprecated and will be removed in a future release. Play Integrity is the recommended provider.')
   safetyNet,
   // The play integrity provider (Firebase recommended)
   playIntegrity
