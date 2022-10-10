0a4f21835954b57335d1d2fc36e6daf645dd7986
russellwheatley <russellwheatley85@gmail.com>
2022-10-10 16:15:03 +0100

    feat(firestore): count(). universal code unit test

--- packages/cloud_firestore/cloud_firestore/test/aggregate_query_test.dart
+++ packages/cloud_firestore/cloud_firestore/test/aggregate_query_test.dart
@@ -5,12 +5,14 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cloud_firestore_platform_interface/cloud_firestore_platform_interface.dart';
 import 'package:cloud_firestore_platform_interface/src/method_channel/method_channel_firestore.dart';
+import 'package:cloud_firestore_platform_interface/src/method_channel/method_channel_query.dart';
+import 'package:cloud_firestore_platform_interface/src/method_channel/utils/firestore_message_codec.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/foundation.dart';
 
 import './mock.dart';
-import './test_firestore_message_codec.dart';
 
 int kCount = 4;
 
@@ -18,7 +20,7 @@ void main() {
   setupCloudFirestoreMocks();
   MethodChannelFirebaseFirestore.channel = const MethodChannel(
     'plugins.flutter.io/firebase_firestore',
-    StandardMethodCodec(TestFirestoreMessageCodec()),
+    StandardMethodCodec(AggregateQueryMessageCodec()),
   );
 
   MethodChannelFirebaseFirestore.channel.setMockMethodCallHandler((call) async {
@@ -40,7 +42,6 @@ void main() {
     });
 
     test('returns the correct `AggregateQuerySnapshot` with correct `count`', () async {
-      print('WWWWWWW');
       Query query = firestore!.collection('flutter-tests');
       AggregateQuery aggregateQuery = query.count();
 
@@ -50,4 +51,39 @@ void main() {
       expect(snapshot.count, equals(kCount));
     });
   });
+}
+
+class AggregateQueryMessageCodec extends FirestoreMessageCodec {
+  /// Constructor.
+  const AggregateQueryMessageCodec();
+  static const int _kFirestoreInstance = 144;
+  static const int _kFirestoreQuery = 145;
+  static const int _kFirestoreSettings = 146;
+
+
+  @override
+  Object? readValueOfType(int type, ReadBuffer buffer) {
+    switch (type) {
+    // The following cases are only used by unit tests, and not by actual application
+    // code paths.
+      case _kFirestoreInstance:
+        String appName = readValue(buffer)! as String;
+        readValue(buffer);
+        final FirebaseApp app = Firebase.app(appName);
+        return MethodChannelFirebaseFirestore(app: app);
+      case _kFirestoreQuery:
+        Map<dynamic, dynamic> values =
+        readValue(buffer)! as Map<dynamic, dynamic>;
+        final FirebaseApp app = Firebase.app();
+        return MethodChannelQuery(
+          MethodChannelFirebaseFirestore(app: app),
+          values['path'],
+        );
+      case _kFirestoreSettings:
+        readValue(buffer);
+        return const Settings();
+      default:
+        return super.readValueOfType(type, buffer);
+    }
+  }
 }
