49e1beb514aae652c962f6b72a6539b01ca6915f
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2022-10-12 15:20:25 +0200

    feat(ui): add firebase_ui_database (#9341)

---
+++ packages/firebase_ui_database/lib/firebase_ui_database.dart
@@ -0,0 +1,11 @@
+export 'src/query_builder.dart'
+    show
+        FirebaseDatabaseQueryBuilder,
+        FirebaseDatabaseListView,
+        FirebaseQueryBuilderSnapshot,
+        FirebaseErrorBuilder,
+        FirebaseItemBuilder,
+        FirebaseLoadingBuilder,
+        FirebaseQueryBuilderSnapshotBuilder;
+
+export 'src/table_builder.dart' show FirebaseDatabaseDataTable;
