1b61cac9ca7db526f4cd1d09a7698c519eb15fee
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2022-10-12 14:42:00 +0200

    drop localizations

--- packages/firebase_ui_localizations/lib/firebase_ui_localizations.dart
+++
@@ -1,5 +0,0 @@
-export 'src/i10n.dart'
-    show FirebaseUILocalizations, FirebaseUILocalizationDelegate;
-
-export 'src/default_localizations.dart'
-    show DefaultLocalizations, FirebaseUILocalizationLabels;
