d209943fd451caf2f587a44c7fdffe3f14527240
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2022-10-13 13:48:57 +0200

    chore: melos format

--- packages/firebase_ui_localizations/example/windows/runner/main.cpp
+++ packages/firebase_ui_localizations/example/windows/runner/main.cpp
@@ -19,15 +19,15 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
 
   flutter::DartProject project(L"data");
 
-  std::vector<std::string> command_line_arguments =
-      GetCommandLineArguments();
+  std::vector<std::string> command_line_arguments = GetCommandLineArguments();
 
   project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
 
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.CreateAndShow(L"firebase_ui_localizations_example", origin, size)) {
+  if (!window.CreateAndShow(L"firebase_ui_localizations_example", origin,
+                            size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
