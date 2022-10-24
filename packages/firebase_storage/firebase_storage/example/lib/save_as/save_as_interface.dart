dea46c4ca89eda879befb0dbce4190f4090d6d48
Nils Reichardt <hi@nils.re>
2022-10-24 08:41:24 +0200

    chore: add license header for missing files (#9703)
    * Run `addlicense -f header_template.txt .`
    
    * Revert "Run `addlicense -f header_template.txt .`"
    
    This reverts commit d32bcf42c08033ad169c186012787822008006b6.
    
    * Run `addlicense -f header_template.txt --ignore "**/*.yml" --ignore "**/*.yaml" --ignore "**/*.xml" --ignore "**/*.g.dart" --ignore "**/*.sh" --ignore "**/*.html" --ignore "**/*.js" --ignore "**/*.ts" --ignore "**/*.g.h" --ignore "**/*.g.m" --ignore "**/*.rb" .`
    
    * Revert "Run `addlicense -f header_template.txt --ignore "**/*.yml" --ignore "**/*.yaml" --ignore "**/*.xml" --ignore "**/*.g.dart" --ignore "**/*.sh" --ignore "**/*.html" --ignore "**/*.js" --ignore "**/*.ts" --ignore "**/*.g.h" --ignore "**/*.g.m" --ignore "**/*.rb" .`"
    
    This reverts commit 886eec4b6c5a187b58f97c9ce7e32952e549d5a2.
    
    * Run `addlicense -f header_template.txt --ignore "**/*.yml" --ignore "**/*.yaml" --ignore "**/*.xml" --ignore "**/*.g.dart" --ignore "**/*.sh" --ignore "**/*.html" --ignore "**/*.js" --ignore "**/*.ts" --ignore "**/*.g.h" --ignore "**/*.g.m" --ignore "**/*.rb" --ignore "**/Runner/AppDelegate.swift" --ignore "**/Runner/MainFlutterWindow.swift" --ignore "**/Runner/Runner-Bridging-Header.h" --ignore "**/Runner/AppDelegate.h" --ignore "**/Runner/AppDelegate.m" --ignore "**/Runner/main.m" --ignore "**/MainActivity.kt" --ignore "**/MainActivity.java" --ignore "**/FlutterMultiDexApplication.java" --ignore **/GeneratedPluginRegistrant.swift .`
    
    * Format `hi.dart`

--- packages/firebase_storage/firebase_storage/example/lib/save_as/save_as_interface.dart
+++ packages/firebase_storage/firebase_storage/example/lib/save_as/save_as_interface.dart
@@ -1,3 +1,7 @@
+// Copyright 2022, the Chromium project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 import 'dart:typed_data';
 
 /// Present a dialog so the user can save as... a bunch of bytes.
