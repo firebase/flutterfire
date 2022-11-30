0136a2af3693a6e6ea8b3b7a46dd75adbda97acd
russellwheatley <russellwheatley85@gmail.com>
2022-11-30 17:07:46 +0000

    fix analyse issue

--- packages/cloud_firestore/cloud_firestore_platform_interface/lib/src/persistence_settings.dart
+++ packages/cloud_firestore/cloud_firestore_platform_interface/lib/src/persistence_settings.dart
@@ -11,6 +11,8 @@ class PersistenceSettings {
   /// shared execution of queries and latency-compensated local document updates across all connected instances.
   /// To enable this mode, synchronizeTabs:true needs to be set globally in all active tabs.
   /// If omitted or set to 'false', enablePersistence() will fail in all but the first tab.
+  @Deprecated(
+      'This property will be replaced by "forceOwnership" in a future release.')
   final bool synchronizeTabs;
 
   /// Creates a [PersistenceSettings] instance.
