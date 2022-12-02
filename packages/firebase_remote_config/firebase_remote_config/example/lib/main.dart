21e64f7b8bc63e278fe491b132de0f9ab97eacd6
russellwheatley <russellwheatley85@gmail.com>
2022-12-02 13:54:37 +0000

    chore(remote-config): move example app to "flutterfire-e2e-tests" Firebase project

--- packages/firebase_remote_config/firebase_remote_config/example/lib/main.dart
+++ packages/firebase_remote_config/firebase_remote_config/example/lib/main.dart
@@ -10,6 +10,8 @@ import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+import 'firebase_options.dart';
+
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(MaterialApp(
@@ -79,15 +81,7 @@ class WelcomeWidget extends AnimatedWidget {
 
 Future<FirebaseRemoteConfig> setupRemoteConfig() async {
   await Firebase.initializeApp(
-    options: const FirebaseOptions(
-        apiKey: 'AIzaSyAgUhHU8wSJgO5MVNy95tMT07NEjzMOfz0',
-        authDomain: 'react-native-firebase-testing.firebaseapp.com',
-        databaseURL: 'https://react-native-firebase-testing.firebaseio.com',
-        projectId: 'react-native-firebase-testing',
-        storageBucket: 'react-native-firebase-testing.appspot.com',
-        messagingSenderId: '448618578101',
-        appId: '1:448618578101:web:772d484dc9eb15e9ac3efc',
-        measurementId: 'G-0N1G9FLDZE'),
+    options: DefaultFirebaseOptions.currentPlatform,
   );
   final FirebaseRemoteConfig remoteConfig = FirebaseRemoteConfig.instance;
   await remoteConfig.setConfigSettings(RemoteConfigSettings(
