cac7c9fa6fe65b3643631270274649acfae6dafd
Russell Wheatley <russellwheatley85@gmail.com>
2022-12-08 12:13:46 +0000

    chore(in-app-messaging): move example app to "flutterfire-e2e-tests" Firebase project (#10052)

--- packages/firebase_in_app_messaging/firebase_in_app_messaging/example/lib/main.dart
+++ packages/firebase_in_app_messaging/firebase_in_app_messaging/example/lib/main.dart
@@ -10,9 +10,13 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_in_app_messaging/firebase_in_app_messaging.dart';
 import 'package:flutter/material.dart';
 
+import 'firebase_options.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
