6db84fc8f6638d03e9b1442f14060b3f25aabc35
Guillaume Bernos <guillaume@bernos.dev>
2022-12-19 14:42:45 +0100

    feat: add licence header

--- packages/cloud_firestore/cloud_firestore_platform_interface/lib/src/method_channel/utils/server_timestamp_behavior.dart
+++ packages/cloud_firestore/cloud_firestore_platform_interface/lib/src/method_channel/utils/server_timestamp_behavior.dart
@@ -1,7 +1,12 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:cloud_firestore_platform_interface/cloud_firestore_platform_interface.dart';
 
 String getServerTimestampBehaviorString(
-    ServerTimestampBehavior serverTimestampBehavior) {
+  ServerTimestampBehavior serverTimestampBehavior,
+) {
   switch (serverTimestampBehavior) {
     case ServerTimestampBehavior.none:
       return 'none';
