240bd123860f86a74c51349d61a8d9164f9e1fd1
Guillaume Bernos <guillaume@bernos.dev>
2023-02-14 09:27:28 +0100

    feat(core): add trusted types support

--- packages/firebase_core/firebase_core_web/test/firebase_core_tt_test.dart
+++ packages/firebase_core/firebase_core_web/test/firebase_core_tt_test.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+@TestOn('browser')
 import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:flutter_test/flutter_test.dart';
 
