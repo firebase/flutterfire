4d18bd4cb7c975ad27e12e01adce581f5c45f0e8
russellwheatley <russellwheatley85@gmail.com>
2023-02-20 15:31:01 +0000

    format

--- packages/firebase_app_check/firebase_app_check_platform_interface/lib/src/apple_provider.dart
+++ packages/firebase_app_check/firebase_app_check_platform_interface/lib/src/apple_provider.dart
@@ -8,10 +8,12 @@ enum AppleProvider {
   /// from the console when you run the app and add the token to your Firebase console. See documentation:
   /// https://firebase.google.com/docs/app-check/ios/debug-provider
   debug,
+
   /// the default deviceCheck provider. See documentation: https://firebase.google.com/docs/app-check/ios/devicecheck-provider
   deviceCheck,
   // The app attest provider (Firebase recommended). See documentation: https://firebase.google.com/docs/app-check/ios/app-attest-provider
   appAttest,
+
   /// appAttest provider is only available on iOS 14.0+, macOS 14.0+ so this will fall back to deviceCheck provider if appAtest provider
   /// is not available
   appAttestWithDeviceCheckFallback
