d4158b47c2999f37bedfebae0f40204be9d6231b
russellwheatley <russellwheatley85@gmail.com>
2023-02-28 17:52:23 +0000

    ci: flutter

--- .github/workflows/scripts/install-custom-flutter.sh
+++ .github/workflows/scripts/install-custom-flutter.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-git clone https://github.com/invertase/flutter --depth 1 -b "macos-provisioning-profile" "$GITHUB_WORKSPACE/_flutter"
-echo "$GITHUB_WORKSPACE/_flutter/bin" >> $GITHUB_PATH
+git clone https://github.com/invertase/flutter --depth 1 -b "macos-provisioning-profile" "$GITHUB_WORKSPACE/flutter"
+echo "$GITHUB_WORKSPACE/flutter/bin" >> $GITHUB_PATH
 
 flutter config --no-analytics
 flutter pub global activate melos 2.9.0
 flutter pub global activate flutter_plugin_tools
 echo "$HOME/.pub-cache/bin" >> $GITHUB_PATH
-echo "$GITHUB_WORKSPACE/_flutter/.pub-cache/bin" >> $GITHUB_PATH
-echo "$GITHUB_WORKSPACE/_flutter/bin/cache/dart-sdk/bin" >> $GITHUB_PATH
+echo "$GITHUB_WORKSPACE/flutter/.pub-cache/bin" >> $GITHUB_PATH
+echo "$GITHUB_WORKSPACE/flutter/bin/cache/dart-sdk/bin" >> $GITHUB_PATH
