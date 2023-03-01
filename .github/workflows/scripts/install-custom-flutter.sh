b66768f37ed1b2f4fe33e200032387b3a1077483
russellwheatley <russellwheatley85@gmail.com>
2023-03-01 09:12:29 +0000

    ci: update to flutter repo

--- .github/workflows/scripts/install-custom-flutter.sh
+++ .github/workflows/scripts/install-custom-flutter.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 BRANCH=$1
-git clone https://github.com/invertase/flutter --depth 1 -b "stable" "$GITHUB_WORKSPACE/_flutter"
+git clone https://github.com/flutter/flutter --depth 1 -b "stable" "$GITHUB_WORKSPACE/_flutter"
 echo "$GITHUB_WORKSPACE/_flutter/bin" >> $GITHUB_PATH
 
