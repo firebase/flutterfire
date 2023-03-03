3cdbaca75202aaa4247d70cac311d5fd68b51a1b
Guillaume Bernos <guillaume@bernos.dev>
2023-03-03 10:18:58 +0100

    fix(auth, messaging): fix crash on iOS when using Notifications Images

--- packages/firebase_auth/firebase_auth/example/ios/ImageNotification/NotificationService.m
+++ packages/firebase_auth/firebase_auth/example/ios/ImageNotification/NotificationService.m
@@ -7,7 +7,9 @@
 //
 
 #import "NotificationService.h"
+#import "FirebaseAuth.h"
 #import "FirebaseMessaging.h"
+#import <UIKit/UIKit.h>
 
 @interface NotificationService ()
 
@@ -17,6 +19,23 @@
 @end
 
 @implementation NotificationService
+
+- (BOOL)application:(UIApplication *)app
+            openURL:(NSURL *)url
+            options:(NSDictionary<UIApplicationOpenURLOptionsKey, id> *)options {
+  if ([[FIRAuth auth] canHandleURL:url]) {
+    return YES;
+  }
+  // URL not auth related; it should be handled separately.
+    return NO;
+}
+
+- (void)scene:(UIScene *)scene openURLContexts:(NSSet<UIOpenURLContext *> *)URLContexts {
+  for (UIOpenURLContext *urlContext in URLContexts) {
+    [FIRAuth.auth canHandleURL:urlContext.URL];
+    // URL not auth related; it should be handled separately.
+  }
+}
 
 - (void)didReceiveNotificationRequest:(UNNotificationRequest *)request withContentHandler:(void (^)(UNNotificationContent * _Nonnull))contentHandler {
     self.contentHandler = contentHandler;
