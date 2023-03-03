b8f72821bef10d307cd51c4646c4b98b5a8d2094
Guillaume Bernos <guillaume@bernos.dev>
2023-03-03 08:35:09 +0100

    fix(auth, messaging): fix crash on iOS when using Notifications Images

---
+++ packages/firebase_auth/firebase_auth/example/ios/ImageNotification/NotificationService.h
@@ -0,0 +1,13 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by Guillaume Bernos on 03/03/2023.
+//  Copyright © 2023 The Chromium Authors. All rights reserved.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
---
+++ packages/firebase_auth/firebase_auth/example/ios/ImageNotification/NotificationService.m
@@ -0,0 +1,35 @@
+//
+//  NotificationService.m
+//  ImageNotification
+//
+//  Created by Guillaume Bernos on 03/03/2023.
+//  Copyright © 2023 The Chromium Authors. All rights reserved.
+//
+
+#import "NotificationService.h"
+#import "FirebaseMessaging.h"
+
+@interface NotificationService ()
+
+@property (nonatomic, strong) void (^contentHandler)(UNNotificationContent *contentToDeliver);
+@property (nonatomic, strong) UNMutableNotificationContent *bestAttemptContent;
+
+@end
+
+@implementation NotificationService
+
+- (void)didReceiveNotificationRequest:(UNNotificationRequest *)request withContentHandler:(void (^)(UNNotificationContent * _Nonnull))contentHandler {
+    self.contentHandler = contentHandler;
+    self.bestAttemptContent = [request.content mutableCopy];
+    
+    // Modify the notification content here...
+    [[FIRMessaging extensionHelper] populateNotificationContent:self.bestAttemptContent withContentHandler:contentHandler];
+}
+
+- (void)serviceExtensionTimeWillExpire {
+    // Called just before the extension will be terminated by the system.
+    // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
+    self.contentHandler(self.bestAttemptContent);
+}
+
+@end
