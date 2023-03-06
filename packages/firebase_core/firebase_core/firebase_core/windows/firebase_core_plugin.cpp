b4d91269af0c9b07162bc6eabfc92ccb8dc62955
Guillaume Bernos <guillaume@bernos.dev>
2023-03-06 14:52:47 +0100

    add windows

---
+++ packages/firebase_core/firebase_core/firebase_core/windows/firebase_core_plugin.cpp
@@ -0,0 +1,59 @@
+#include "firebase_core_plugin.h"
+
+// This must be included before many other Windows headers.
+#include <windows.h>
+
+// For getPlatformVersion; remove unless needed for your plugin implementation.
+#include <VersionHelpers.h>
+
+#include <flutter/method_channel.h>
+#include <flutter/plugin_registrar_windows.h>
+#include <flutter/standard_method_codec.h>
+
+#include <memory>
+#include <sstream>
+
+namespace firebase_core {
+
+// static
+void FirebaseCorePlugin::RegisterWithRegistrar(
+    flutter::PluginRegistrarWindows *registrar) {
+  auto channel =
+      std::make_unique<flutter::MethodChannel<flutter::EncodableValue>>(
+          registrar->messenger(), "firebase_core",
+          &flutter::StandardMethodCodec::GetInstance());
+
+  auto plugin = std::make_unique<FirebaseCorePlugin>();
+
+  channel->SetMethodCallHandler(
+      [plugin_pointer = plugin.get()](const auto &call, auto result) {
+        plugin_pointer->HandleMethodCall(call, std::move(result));
+      });
+
+  registrar->AddPlugin(std::move(plugin));
+}
+
+FirebaseCorePlugin::FirebaseCorePlugin() {}
+
+FirebaseCorePlugin::~FirebaseCorePlugin() {}
+
+void FirebaseCorePlugin::HandleMethodCall(
+    const flutter::MethodCall<flutter::EncodableValue> &method_call,
+    std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result) {
+  if (method_call.method_name().compare("getPlatformVersion") == 0) {
+    std::ostringstream version_stream;
+    version_stream << "Windows ";
+    if (IsWindows10OrGreater()) {
+      version_stream << "10+";
+    } else if (IsWindows8OrGreater()) {
+      version_stream << "8";
+    } else if (IsWindows7OrGreater()) {
+      version_stream << "7";
+    }
+    result->Success(flutter::EncodableValue(version_stream.str()));
+  } else {
+    result->NotImplemented();
+  }
+}
+
+}  // namespace firebase_core
---
+++ packages/firebase_core/firebase_core/firebase_core/windows/firebase_core_plugin_c_api.cpp
@@ -0,0 +1,12 @@
+#include "include/firebase_core/firebase_core_plugin_c_api.h"
+
+#include <flutter/plugin_registrar_windows.h>
+
+#include "firebase_core_plugin.h"
+
+void FirebaseCorePluginCApiRegisterWithRegistrar(
+    FlutterDesktopPluginRegistrarRef registrar) {
+  firebase_core::FirebaseCorePlugin::RegisterWithRegistrar(
+      flutter::PluginRegistrarManager::GetInstance()
+          ->GetRegistrar<flutter::PluginRegistrarWindows>(registrar));
+}
