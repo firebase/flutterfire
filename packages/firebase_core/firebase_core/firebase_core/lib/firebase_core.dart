e1b75caa8541b464e5d2095fcce5744ecc41e0e2
Guillaume Bernos <guillaume@bernos.dev>
2023-03-14 11:04:55 +0100

    feat(core, windows): add windows support to firebase_core

--- packages/firebase_core/firebase_core/firebase_core/lib/firebase_core.dart
+++
@@ -1,8 +0,0 @@
-
-import 'firebase_core_platform_interface.dart';
-
-class FirebaseCore {
-  Future<String?> getPlatformVersion() {
-    return FirebaseCorePlatform.instance.getPlatformVersion();
-  }
-}
