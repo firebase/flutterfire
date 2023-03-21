33f25803cba3304850c055f3c014b972b54bff39
Guillaume Bernos <guillaume@bernos.dev>
2023-03-21 10:29:20 +0100

    add licence headers

--- packages/firebase_core/firebase_core/windows/firebase_core_plugin.cpp
+++ packages/firebase_core/firebase_core/windows/firebase_core_plugin.cpp
@@ -1,3 +1,7 @@
+// Copyright 2023, the Chromium project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 #include "firebase_core_plugin.h"
 
 // This must be included before many other Windows headers.
