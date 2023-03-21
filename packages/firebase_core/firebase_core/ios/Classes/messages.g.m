cac387a2db06f1527be35e3f2e66680da95a3cde
Guillaume Bernos <guillaume@bernos.dev>
2023-03-21 11:53:30 +0100

    format

--- packages/firebase_core/firebase_core/ios/Classes/messages.g.m
+++ packages/firebase_core/firebase_core/ios/Classes/messages.g.m
@@ -5,7 +5,11 @@
 // See also: https://pub.dev/packages/pigeon
 
 #import "messages.g.h"
+#if TARGET_OS_OSX
+#import <FlutterMacOS/FlutterMacOS.h>
+#else
 #import <Flutter/Flutter.h>
+#endif
 
 #if !__has_feature(objc_arc)
 #error File requires ARC to be enabled.
