234a41dc035f27af0efa9c4b22a19218101c7d72
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2023-03-23 10:00:34 +0100

    chore: add license header (#10637)

--- packages/firebase_ui_shared/test/universal_icon_test.dart
+++ packages/firebase_ui_shared/test/universal_icon_test.dart
@@ -1,3 +1,7 @@
+// Copyright 2023, the Chromium project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 import 'package:firebase_ui_shared/firebase_ui_shared.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
