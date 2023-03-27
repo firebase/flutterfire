9e4f76ee72f0df1c4499eac7314fe3c14784d4c0
Guillaume Bernos <guillaume@bernos.dev>
2023-03-27 14:00:18 +0200

    feat(remote-config): add support for onConfigUpdated

--- packages/firebase_remote_config/firebase_remote_config_platform_interface/lib/src/remote_config_update.dart
+++ packages/firebase_remote_config/firebase_remote_config_platform_interface/lib/src/remote_config_update.dart
@@ -1,3 +1,7 @@
+// Copyright 2023, the Chromium project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 /// Used by Remote Config real-time config update service, this class represents changes between the newly fetched config and the current one.
 ///
 /// An instance of this class is returned from [FirebaseRemoteConfig.onConfigUpdated].
