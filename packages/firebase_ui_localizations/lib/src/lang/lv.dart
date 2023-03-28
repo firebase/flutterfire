40dce27255dd720e9e6a8c0d3ee02c1d0cce6ab5
Marchel0925 <marcis.andersons0925@gmail.com>
2023-03-28 12:34:19 +0300

    Styling

--- packages/firebase_ui_localizations/lib/src/lang/lv.dart
+++ packages/firebase_ui_localizations/lib/src/lang/lv.dart
@@ -8,252 +8,252 @@ class LvLocalizations extends FirebaseUILocalizationLabels {
   const LvLocalizations();
 
   @override
-  String get emailInputLabel => 'E-pasts';
+  String get emailInputLabel => "E-pasts";
 
   @override
-  String get passwordInputLabel => 'Parole';
+  String get passwordInputLabel => "Parole";
 
   @override
-  String get signInActionText => 'Pierakstīties';
+  String get signInActionText => "Pierakstīties";
 
   @override
-  String get registerActionText => 'Reģistrēties';
+  String get registerActionText => "Reģistrēties";
 
   @override
-  String get linkEmailButtonText => 'Turpināt';
+  String get linkEmailButtonText => "Turpināt";
 
   @override
-  String get signInWithPhoneButtonText => 'Pierakstīties ar telefona numuru';
+  String get signInWithPhoneButtonText => "Pierakstīties ar telefona numuru";
 
   @override
-  String get signInWithGoogleButtonText => 'Pierakstīties ar Google';
+  String get signInWithGoogleButtonText => "Pierakstīties ar Google";
 
   @override
-  String get signInWithAppleButtonText => 'Pierakstīties ar Apple';
+  String get signInWithAppleButtonText => "Pierakstīties ar Apple";
 
   @override
-  String get signInWithFacebookButtonText => 'Pierakstīties ar Facebook';
+  String get signInWithFacebookButtonText => "Pierakstīties ar Facebook";
 
   @override
-  String get signInWithTwitterButtonText => 'Pierakstīties ar Twitter';
+  String get signInWithTwitterButtonText => "Pierakstīties ar Twitter";
 
   @override
-  String get phoneVerificationViewTitleText => 'Ievadiet savu telefona numuru';
+  String get phoneVerificationViewTitleText => "Ievadiet savu telefona numuru";
 
   @override
-  String get verifyPhoneNumberButtonText => 'Turpināt';
+  String get verifyPhoneNumberButtonText => "Turpināt";
 
   @override
-  String get verifyCodeButtonText => 'Pārbaudīt';
+  String get verifyCodeButtonText => "Pārbaudīt";
 
   @override
-  String get unknownError => 'Notiksui kļūda. Mēģiniet vēlreiz vlelāk';
+  String get unknownError => "Notiksui kļūda. Mēģiniet vēlreiz vlelāk";
 
   @override
-  String get smsAutoresolutionFailedError => 'Notikusi kļūda. Lūdzu, ievadiet SMS kodu manuāli';
+  String get smsAutoresolutionFailedError => "Notikusi kļūda. Lūdzu, ievadiet SMS kodu manuāli";
 
   @override
-  String get verifyingSMSCodeText => 'Pārbaudīt SMS kodu';
+  String get verifyingSMSCodeText => "Pārbaudīt SMS kodu";
 
   @override
-  String get enterSMSCodeText => 'Ievadiet SMS kodu';
+  String get enterSMSCodeText => "Ievadiet SMS kodu";
 
   @override
-  String get emailIsRequiredErrorText => 'E-pasta lauks ir obligāts';
+  String get emailIsRequiredErrorText => "E-pasta lauks ir obligāts";
 
   @override
-  String get isNotAValidEmailErrorText => 'E-pasta adrese nav pareiza';
+  String get isNotAValidEmailErrorText => "E-pasta adrese nav pareiza";
 
   @override
-  String get userNotFoundErrorText => 'Lietotāja konts nav izveidots';
+  String get userNotFoundErrorText => "Lietotāja konts nav izveidots";
 
   @override
-  String get emailTakenErrorText => 'Konts ar šādu e-pasta adresi jau eksistē';
+  String get emailTakenErrorText => "Konts ar šādu e-pasta adresi jau eksistē";
 
   @override
-  String get accessDisabledErrorText => 'Piekļuve šim kontam ir atspējota';
+  String get accessDisabledErrorText => "Piekļuve šim kontam ir atspējota";
 
   @override
-  String get wrongOrNoPasswordErrorText => 'Ievadītā parole ir nepareiza vai nepastāv';
+  String get wrongOrNoPasswordErrorText => "Ievadītā parole ir nepareiza vai nepastāv";
 
   @override
-  String get signInText => 'Pierakstīties';
+  String get signInText => "Pierakstīties";
 
   @override
-  String get registerText => 'Reģistrējies';
+  String get registerText => "Reģistrējies";
 
   @override
-  String get registerHintText => 'Esi jauns lietotājs?';
+  String get registerHintText => "Esi jauns lietotājs?";
 
   @override
-  String get signInHintText => 'Tev jau pieder konts?';
+  String get signInHintText => "Tev jau pieder konts?";
 
   @override
-  String get signOutButtonText => 'Izrakstīties';
+  String get signOutButtonText => "Izrakstīties";
 
   @override
-  String get phoneInputLabel => 'Telefona numurs';
+  String get phoneInputLabel => "Telefona numurs";
 
   @override
-  String get phoneNumberIsRequiredErrorText => 'Telefona numurs ir obligāts lauks.';
+  String get phoneNumberIsRequiredErrorText => "Telefona numurs ir obligāts lauks.";
 
   @override
-  String get phoneNumberInvalidErrorText => 'Telefona numurs ievadīts nepareizi.';
+  String get phoneNumberInvalidErrorText => "Telefona numurs ievadīts nepareizi.";
 
   @override
-  String get profile => 'Profils';
+  String get profile => "Profils";
 
   @override
-  String get name => 'Lietotāja vārds';
+  String get name => "Lietotāja vārds";
 
   @override
-  String get deleteAccount => 'Izdzēst kontu';
+  String get deleteAccount => "Izdzēst kontu";
 
   @override
-  String get passwordIsRequiredErrorText => 'Parole ir obligāts lauks';
+  String get passwordIsRequiredErrorText => "Parole ir obligāts lauks";
 
   @override
-  String get confirmPasswordIsRequiredErrorText => 'Obligāts lauks';
+  String get confirmPasswordIsRequiredErrorText => "Obligāts lauks";
 
   @override
-  String get confirmPasswordDoesNotMatchErrorText => 'Paroļu lauki nesakrīt';
+  String get confirmPasswordDoesNotMatchErrorText => "Paroļu lauki nesakrīt";
 
   @override
-  String get confirmPasswordInputLabel => 'Atkārtot paroli';
+  String get confirmPasswordInputLabel => "Atkārtot paroli";
 
   @override
-  String get forgotPasswordButtonLabel => 'Aizmirsi paroli?';
+  String get forgotPasswordButtonLabel => "Aizmirsi paroli?";
 
   @override
-  String get forgotPasswordViewTitle => 'Aizmirsu paroli';
+  String get forgotPasswordViewTitle => "Aizmirsu paroli";
 
   @override
-  String get resetPasswordButtonLabel => 'Mainīt paroli';
+  String get resetPasswordButtonLabel => "Mainīt paroli";
 
   @override
-  String get verifyItsYouText => 'Pārbaudiet, vai esat jūs';
+  String get verifyItsYouText => "Pārbaudiet, vai esat jūs";
 
   @override
-  String get differentMethodsSignInTitleText => 'Pierakstīties ar citu metodi';
+  String get differentMethodsSignInTitleText => "Pierakstīties ar citu metodi";
 
   @override
-  String get findProviderForEmailTitleText => 'Ievadiet e-pasta adresi lai tupinātu';
+  String get findProviderForEmailTitleText => "Ievadiet e-pasta adresi lai tupinātu";
 
   @override
-  String get continueText => 'Turpināt';
+  String get continueText => "Turpināt";
 
   @override
-  String get countryCode => 'Valsts kods';
+  String get countryCode => "Valsts kods";
 
   @override
-  String get invalidCountryCode => 'Nepareizs valsts kods';
+  String get invalidCountryCode => "Nepareizs valsts kods";
 
   @override
-  String get chooseACountry => 'Izvēlies valsti';
+  String get chooseACountry => "Izvēlies valsti";
 
   @override
-  String get enableMoreSignInMethods => 'Iespējot citas pierakstīšanās metodes';
+  String get enableMoreSignInMethods => "Iespējot citas pierakstīšanās metodes";
 
   @override
-  String get signInMethods => 'Pierakstīšanās metodes';
+  String get signInMethods => "Pierakstīšanās metodes";
 
   @override
-  String get provideEmail => 'Ievadiet e-pasta adresi un paroli';
+  String get provideEmail => "Ievadiet e-pasta adresi un paroli";
 
   @override
-  String get goBackButtonLabel => 'Doties atpakaļ';
+  String get goBackButtonLabel => "Doties atpakaļ";
 
   @override
-  String get passwordResetEmailSentText => 'Paroles maiņas e-pasts tika nosūtīts.';
+  String get passwordResetEmailSentText => "Paroles maiņas e-pasts tika nosūtīts.";
 
   @override
   String get forgotPasswordHintText =>
-      'Ievadi savu e-pastu un uz to tiks noūtīts links ar kuru varēsi nomainīt savu paroli.';
+      "Ievadi savu e-pastu un uz to tiks noūtīts links ar kuru varēsi nomainīt savu paroli.";
 
   @override
-  String get emailLinkSignInButtonLabel => 'Ielogoties ar e-pasta linku';
+  String get emailLinkSignInButtonLabel => "Ielogoties ar e-pasta linku";
 
   @override
-  String get signInWithEmailLinkViewTitleText => 'Ielogoties ar e-pasta linku';
+  String get signInWithEmailLinkViewTitleText => "Ielogoties ar e-pasta linku";
 
   @override
-  String get signInWithEmailLinkSentText => 'Pārbaudiet savu e-pastu un sekojiet linkam, lai pierakstītos';
+  String get signInWithEmailLinkSentText => "Pārbaudiet savu e-pastu un sekojiet linkam, lai pierakstītos";
 
   @override
-  String get sendLinkButtonLabel => 'Sūtīt linku';
+  String get sendLinkButtonLabel => "Sūtīt linku";
 
   @override
-  String get credentialAlreadyInUseErrorText => 'Konts ar šādu e-pasta adresi jau eksistē';
+  String get credentialAlreadyInUseErrorText => "Konts ar šādu e-pasta adresi jau eksistē";
 
   @override
-  String get disable => 'Atspējot';
+  String get disable => "Atspējot";
 
   @override
-  String get enable => 'Iespējot';
+  String get enable => "Iespējot";
 
   @override
-  String get mfaTitle => '2 soļu verifikācija';
+  String get mfaTitle => "2 soļu verifikācija";
 
   @override
-  String get off => 'Izslēgts';
+  String get off => "Izslēgts";
 
   @override
-  String get on => 'Ieslēgts';
+  String get on => "Ieslēgts";
 
   @override
-  String get valueLabel => 'Vērtība';
+  String get valueLabel => "Vērtība";
 
   @override
-  String get typeLabel => 'Tips';
+  String get typeLabel => "Tips";
 
   @override
-  String get stringLabel => 'Simbolu virkne';
+  String get stringLabel => "Simbolu virkne";
 
   @override
-  String get numberLabel => 'Skaitlis';
+  String get numberLabel => "Skaitlis";
 
   @override
-  String get booleanLabel => 'Būla';
+  String get booleanLabel => "Būla";
 
   @override
-  String get mapLabel => 'Vārdnīca';
+  String get mapLabel => "Vārdnīca";
 
   @override
-  String get arrayLabel => 'Masīvs';
+  String get arrayLabel => "Masīvs";
 
   @override
-  String get nullLabel => 'Tukša vērtība';
+  String get nullLabel => "Tukša vērtība";
 
   @override
-  String get cancelLabel => 'Atcelt';
+  String get cancelLabel => "Atcelt";
 
   @override
-  String get updateLabel => 'Atjaunināt';
+  String get updateLabel => "Atjaunināt";
 
   @override
-  String get northInitialLabel => 'Z';
+  String get northInitialLabel => "Z";
 
   @override
-  String get southInitialLabel => 'D';
+  String get southInitialLabel => "D";
 
   @override
-  String get westInitialLabel => 'R';
+  String get westInitialLabel => "R";
 
   @override
-  String get eastInitialLabel => 'A';
+  String get eastInitialLabel => "A";
 
   @override
-  String get timestampLabel => 'Laika zīmogs';
+  String get timestampLabel => "Laika zīmogs";
 
   @override
-  String get longitudeLabel => 'Ģeogrāfiskais garums';
+  String get longitudeLabel => "Ģeogrāfiskais garums";
 
   @override
-  String get latitudeLabel => 'Ģeogrāfiskais platums';
+  String get latitudeLabel => "Ģeogrāfiskais platums";
 
   @override
-  String get geopointLabel => 'Ģeogrāfiskais punkts';
+  String get geopointLabel => "Ģeogrāfiskais punkts";
 
   @override
-  String get referenceLabel => 'Atsauce';
+  String get referenceLabel => "Atsauce";
 }
