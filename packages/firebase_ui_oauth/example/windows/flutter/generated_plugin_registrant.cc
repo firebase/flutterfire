9fdc4e9d2d9f2e1d020225befefbbf1eb46660ad
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2023-04-27 13:42:23 +0200

    chore(ui): bump dependencies

--- packages/firebase_ui_oauth/example/windows/flutter/generated_plugin_registrant.cc
+++ packages/firebase_ui_oauth/example/windows/flutter/generated_plugin_registrant.cc
@@ -7,8 +7,11 @@
 #include "generated_plugin_registrant.h"
 
 #include <desktop_webview_auth/desktop_webview_auth_plugin.h>
+#include <flutter_secure_storage_windows/flutter_secure_storage_windows_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   DesktopWebviewAuthPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("DesktopWebviewAuthPlugin"));
+  FlutterSecureStorageWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterSecureStorageWindowsPlugin"));
 }
