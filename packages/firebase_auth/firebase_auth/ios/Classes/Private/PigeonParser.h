993073df8c30c75a6f2bcebb7c05abd0eb2015e2
Guillaume Bernos <guillaume@bernos.dev>
2023-05-17 11:12:11 +0200

    add licence

--- packages/firebase_auth/firebase_auth/ios/Classes/Private/PigeonParser.h
+++ packages/firebase_auth/firebase_auth/ios/Classes/Private/PigeonParser.h
@@ -1,3 +1,9 @@
+/*
+ * Copyright 2023, the Chromium project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+
 #import <Firebase/Firebase.h>
 #import <Foundation/Foundation.h>
 #import "messages.g.h"
