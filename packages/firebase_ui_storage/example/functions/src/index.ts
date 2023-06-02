d4c8b1031b0eb3e6e98e7f6c03d1111ec1e01bde
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2023-06-02 13:29:50 +0200

    fix genBlurHash function

--- packages/firebase_ui_storage/example/functions/src/index.ts
+++ packages/firebase_ui_storage/example/functions/src/index.ts
@@ -2,6 +2,15 @@ import * as functions from "firebase-functions/v2";
 import { getStorage } from "firebase-admin/storage";
 import { encode } from "blurhash";
 import * as sharp from "sharp";
+import { initializeApp } from "firebase-admin/app";
+
+initializeApp();
+
+type ResizeResult = {
+  buffer: Buffer;
+  width: number;
+  height: number;
+};
 
 exports.genBlurHash = functions.storage.onObjectFinalized(async (event) => {
   const { bucket: fileBucket, name, contentType } = event.data;
@@ -15,21 +24,32 @@ exports.genBlurHash = functions.storage.onObjectFinalized(async (event) => {
   const res = await file.download();
   const [buffer] = res;
 
-  const sharpImage = sharp(buffer);
-  const metadata = await sharpImage.metadata();
-
-  if (metadata.width === undefined || metadata.height === undefined) {
-    return;
-  }
-
-  const { width, height } = metadata;
-
-  const blurHashString = encode(
-    new Uint8ClampedArray(buffer),
+  const {
+    buffer: sharpBuffer,
     width,
     height,
-    4,
-    4
+  } = await new Promise<ResizeResult>((resolve, reject) => {
+    sharp(buffer)
+      .raw()
+      .ensureAlpha()
+      .resize(64, 64, {
+        fit: "inside",
+      })
+      .toBuffer((err, buffer, { width, height }) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve({ buffer, width, height });
+      });
+  });
+
+  const blurHashString = encode(
+    new Uint8ClampedArray(sharpBuffer),
+    width,
+    height,
+    8,
+    8
   );
 
   await file.setMetadata({
