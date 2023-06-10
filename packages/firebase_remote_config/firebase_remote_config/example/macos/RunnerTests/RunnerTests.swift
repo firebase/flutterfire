3041fa9c3619f99c71712c6176f4a3ee69586f49
nilsreichardt <me@nils.re>
2023-06-10 15:34:16 +0200

    Run `melos run format`

--- packages/firebase_remote_config/firebase_remote_config/example/macos/RunnerTests/RunnerTests.swift
+++ packages/firebase_remote_config/firebase_remote_config/example/macos/RunnerTests/RunnerTests.swift
@@ -7,10 +7,8 @@ import Cocoa
 import XCTest
 
 class RunnerTests: XCTestCase {
-
   func testExample() {
     // If you add code to the Runner application, consider adding tests here.
     // See https://developer.apple.com/documentation/xctest for more information about using XCTest.
   }
-
 }
