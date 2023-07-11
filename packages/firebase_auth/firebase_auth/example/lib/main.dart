df225ec807d3903d07d3300911ebd3a8121023b3
russellwheatley <russellwheatley85@gmail.com>
2023-07-11 12:24:11 +0100

    update example for testing

--- packages/firebase_auth/firebase_auth/example/lib/main.dart
+++ packages/firebase_auth/firebase_auth/example/lib/main.dart
@@ -53,41 +53,14 @@ class AuthExampleApp extends StatelessWidget {
           builder: (context, constraints) {
             return Row(
               children: [
-                Visibility(
-                  visible: constraints.maxWidth >= 1200,
-                  child: Expanded(
-                    child: Container(
-                      height: double.infinity,
-                      color: Theme.of(context).colorScheme.primary,
-                      child: Center(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              'Firebase Auth Desktop',
-                              style: Theme.of(context).textTheme.headlineMedium,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  width: constraints.maxWidth >= 1200
-                      ? constraints.maxWidth / 2
-                      : constraints.maxWidth,
-                  child: StreamBuilder<User?>(
-                    stream: auth.authStateChanges(),
-                    builder: (context, snapshot) {
-                      if (snapshot.hasData) {
-                        return const ProfilePage();
-                      }
-                      return const AuthGate();
-                    },
-                  ),
-                ),
-              ],
+                ElevatedButton(onPressed: ()async {
+                  try {
+                    await FirebaseAuth.instance.signInWithEmailAndPassword(email: 'non@existent.com', password: 'fake-password');
+                  } catch(e) {
+                    print(e);
+                  }
+            }, child: const Text('press me'),),
+            ]
             );
           },
         ),
