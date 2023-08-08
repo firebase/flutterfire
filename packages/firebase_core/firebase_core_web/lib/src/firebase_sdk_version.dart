d9b33880feb8f347529e69f38f0a12192215e90c
Russell Wheatley <russellwheatley85@gmail.com>
2023-08-08 15:02:07 +0100

    feat: bump Firebase web JS SDK `10.1.0` (#11431)

--- packages/firebase_core/firebase_core_web/lib/src/firebase_sdk_version.dart
+++ packages/firebase_core/firebase_core_web/lib/src/firebase_sdk_version.dart
@@ -6,4 +6,4 @@
 part of firebase_core_web;
 
 /// The currently supported Firebase JS SDK version.
-const String supportedFirebaseJsSdkVersion = '9.22.1';
+const String supportedFirebaseJsSdkVersion = '10.1.0';
