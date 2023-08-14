a7f88534c59f1b9de35fb67396e985f11a1d6d68
russellwheatley <russellwheatley85@gmail.com>
2023-08-14 09:51:30 +0100

    chore: restore file

--- packages/firebase_core/firebase_core/ios/Classes/messages.g.m
+++ packages/firebase_core/firebase_core/ios/Classes/messages.g.m
@@ -8,19 +8,7 @@
 #if TARGET_OS_OSX
 #import <FlutterMacOS/FlutterMacOS.h>
 #else
-#if TARGET_OS_OSX
-#import <FlutterMacOS/FlutterMacOS.h>
-#else
-#if TARGET_OS_OSX
-#import <FlutterMacOS/FlutterMacOS.h>
-#else
-#if TARGET_OS_OSX
-#import <FlutterMacOS/FlutterMacOS.h>
-#else
 #import <Flutter/Flutter.h>
-#endif
-#endif
-#endif
 #endif
 
 #if !__has_feature(objc_arc)
