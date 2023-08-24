68d394e65b41469d841ce2156b6ee7bf174d5f21
Guillaume Bernos <guillaume@bernos.dev>
2023-08-24 15:06:25 +0200

    create empty methods

--- packages/cloud_firestore/cloud_firestore/windows/cloud_firestore_plugin.cpp
+++ packages/cloud_firestore/cloud_firestore/windows/cloud_firestore_plugin.cpp
@@ -13,7 +13,7 @@
 #include <memory>
 #include <sstream>
 
-namespace cloud_firestore {
+namespace cloud_firestore_windows {
 
 // static
 void CloudFirestorePlugin::RegisterWithRegistrar(
@@ -25,10 +25,6 @@ void CloudFirestorePlugin::RegisterWithRegistrar(
 
   auto plugin = std::make_unique<CloudFirestorePlugin>();
 
-  channel->SetMethodCallHandler(
-      [plugin_pointer = plugin.get()](const auto &call, auto result) {
-        plugin_pointer->HandleMethodCall(call, std::move(result));
-      });
 
   registrar->AddPlugin(std::move(plugin));
 }
@@ -37,23 +33,100 @@ CloudFirestorePlugin::CloudFirestorePlugin() {}
 
 CloudFirestorePlugin::~CloudFirestorePlugin() {}
 
-void CloudFirestorePlugin::HandleMethodCall(
-    const flutter::MethodCall<flutter::EncodableValue> &method_call,
-    std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result) {
-  if (method_call.method_name().compare("getPlatformVersion") == 0) {
-    std::ostringstream version_stream;
-    version_stream << "Windows ";
-    if (IsWindows10OrGreater()) {
-      version_stream << "10+";
-    } else if (IsWindows8OrGreater()) {
-      version_stream << "8";
-    } else if (IsWindows7OrGreater()) {
-      version_stream << "7";
-    }
-    result->Success(flutter::EncodableValue(version_stream.str()));
-  } else {
-    result->NotImplemented();
-  }
-}
+void CloudFirestorePlugin::LoadBundle(
+    const PigeonFirebaseApp& app, const std::vector<uint8_t>& bundle,
+    std::function<void(ErrorOr<std::string> reply)> result) {}
+
+void CloudFirestorePlugin::NamedQueryGet(
+    const PigeonFirebaseApp& app, const std::string& name,
+    const PigeonGetOptions& options,
+    std::function<void(ErrorOr<PigeonQuerySnapshot> reply)> result) {}
+
+void CloudFirestorePlugin::ClearPersistence(
+    const PigeonFirebaseApp& app,
+    std::function<void(std::optional<FlutterError> reply)> result) {}
+
+void CloudFirestorePlugin::DisableNetwork(
+    const PigeonFirebaseApp& app,
+    std::function<void(std::optional<FlutterError> reply)> result) {}
+
+void CloudFirestorePlugin::EnableNetwork(
+    const PigeonFirebaseApp& app,
+    std::function<void(std::optional<FlutterError> reply)> result) {}
+
+void CloudFirestorePlugin::Terminate(
+    const PigeonFirebaseApp& app,
+    std::function<void(std::optional<FlutterError> reply)> result) {}
+
+void CloudFirestorePlugin::WaitForPendingWrites(
+    const PigeonFirebaseApp& app,
+    std::function<void(std::optional<FlutterError> reply)> result) {}
+
+void CloudFirestorePlugin::SetIndexConfiguration(
+    const PigeonFirebaseApp& app, const std::string& index_configuration,
+    std::function<void(std::optional<FlutterError> reply)> result) {}
+
+void CloudFirestorePlugin::SetLoggingEnabled(
+    bool logging_enabled,
+    std::function<void(std::optional<FlutterError> reply)> result) {}
+
+void CloudFirestorePlugin::SnapshotsInSyncSetup(
+    std::function<void(ErrorOr<std::string> reply)> result) {}
+
+void CloudFirestorePlugin::TransactionCreate(
+    std::function<void(ErrorOr<std::string> reply)> result) {}
+
+void CloudFirestorePlugin::TransactionStoreResult(
+    const std::string& transaction_id,
+    const PigeonTransactionResult& result_type,
+    const flutter::EncodableList* commands,
+    std::function<void(std::optional<FlutterError> reply)> result) {}
+
+void CloudFirestorePlugin::TransactionGet(
+    const PigeonFirebaseApp& app, const std::string& transaction_id,
+    const std::string& path,
+    std::function<void(ErrorOr<PigeonDocumentSnapshot> reply)> result) {}
+
+void CloudFirestorePlugin::DocumentReferenceSet(
+    const PigeonFirebaseApp& app, const DocumentReferenceRequest& request,
+    std::function<void(std::optional<FlutterError> reply)> result) {}
+
+void CloudFirestorePlugin::DocumentReferenceUpdate(
+    const PigeonFirebaseApp& app, const DocumentReferenceRequest& request,
+    std::function<void(std::optional<FlutterError> reply)> result) {}
+
+void CloudFirestorePlugin::DocumentReferenceGet(
+    const PigeonFirebaseApp& app, const DocumentReferenceRequest& request,
+    std::function<void(ErrorOr<PigeonDocumentSnapshot> reply)> result) {}
+
+void CloudFirestorePlugin::DocumentReferenceDelete(
+    const PigeonFirebaseApp& app, const DocumentReferenceRequest& request,
+    std::function<void(std::optional<FlutterError> reply)> result) {}
+
+void CloudFirestorePlugin::QueryGet(
+    const PigeonFirebaseApp& app, const std::string& path,
+    bool is_collection_group, const PigeonQueryParameters& parameters,
+    const PigeonGetOptions& options,
+    std::function<void(ErrorOr<PigeonQuerySnapshot> reply)> result) {}
+
+void CloudFirestorePlugin::AggregateQueryCount(
+    const PigeonFirebaseApp& app, const std::string& path,
+    const PigeonQueryParameters& parameters, const AggregateSource& source,
+    std::function<void(ErrorOr<double> reply)> result) {}
+
+void CloudFirestorePlugin::WriteBatchCommit(
+    const PigeonFirebaseApp& app, const flutter::EncodableList& writes,
+    std::function<void(std::optional<FlutterError> reply)> result) {}
+
+void CloudFirestorePlugin::QuerySnapshot(
+    const PigeonFirebaseApp& app, const std::string& path,
+    bool is_collection_group, const PigeonQueryParameters& parameters,
+    const PigeonGetOptions& options, bool include_metadata_changes,
+    std::function<void(ErrorOr<std::string> reply)> result) {}
+
+void CloudFirestorePlugin::DocumentReferenceSnapshot(
+    const PigeonFirebaseApp& app, const DocumentReferenceRequest& parameters,
+    bool include_metadata_changes,
+    std::function<void(ErrorOr<std::string> reply)> result) {}
 
 }  // namespace cloud_firestore
--- packages/cloud_firestore/cloud_firestore/windows/cloud_firestore_plugin.h
+++ packages/cloud_firestore/cloud_firestore/windows/cloud_firestore_plugin.h
@@ -3,12 +3,15 @@
 
 #include <flutter/method_channel.h>
 #include <flutter/plugin_registrar_windows.h>
+#include <flutter/plugin_registrar_windows.h>
+#include "messages.g.h"
 
 #include <memory>
 
-namespace cloud_firestore {
+namespace cloud_firestore_windows {
 
-class CloudFirestorePlugin : public flutter::Plugin {
+class CloudFirestorePlugin : public flutter::Plugin,
+                             public FirebaseFirestoreHostApi {
  public:
   static void RegisterWithRegistrar(flutter::PluginRegistrarWindows *registrar);
 
@@ -20,12 +23,88 @@ class CloudFirestorePlugin : public flutter::Plugin {
   CloudFirestorePlugin(const CloudFirestorePlugin&) = delete;
   CloudFirestorePlugin& operator=(const CloudFirestorePlugin&) = delete;
 
-  // Called when a method is called on this plugin's channel from Dart.
-  void HandleMethodCall(
-      const flutter::MethodCall<flutter::EncodableValue> &method_call,
-      std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
+  // FirebaseFirestoreHostApi methods.
+
+
+// Inherited via FirebaseFirestoreHostApi
+  virtual void LoadBundle(
+      const PigeonFirebaseApp& app, const std::vector<uint8_t>& bundle,
+      std::function<void(ErrorOr<std::string> reply)> result) override;
+  virtual void NamedQueryGet(
+      const PigeonFirebaseApp& app, const std::string& name,
+      const PigeonGetOptions& options,
+      std::function<void(ErrorOr<PigeonQuerySnapshot> reply)> result) override;
+  virtual void ClearPersistence(
+      const PigeonFirebaseApp& app,
+      std::function<void(std::optional<FlutterError> reply)> result) override;
+  virtual void DisableNetwork(
+      const PigeonFirebaseApp& app,
+      std::function<void(std::optional<FlutterError> reply)> result) override;
+  virtual void EnableNetwork(
+      const PigeonFirebaseApp& app,
+      std::function<void(std::optional<FlutterError> reply)> result) override;
+  virtual void Terminate(
+      const PigeonFirebaseApp& app,
+      std::function<void(std::optional<FlutterError> reply)> result) override;
+  virtual void WaitForPendingWrites(
+      const PigeonFirebaseApp& app,
+      std::function<void(std::optional<FlutterError> reply)> result) override;
+  virtual void SetIndexConfiguration(
+      const PigeonFirebaseApp& app, const std::string& index_configuration,
+      std::function<void(std::optional<FlutterError> reply)> result) override;
+  virtual void SetLoggingEnabled(
+      bool logging_enabled,
+      std::function<void(std::optional<FlutterError> reply)> result) override;
+  virtual void SnapshotsInSyncSetup(
+      std::function<void(ErrorOr<std::string> reply)> result) override;
+  virtual void TransactionCreate(
+      std::function<void(ErrorOr<std::string> reply)> result) override;
+  virtual void TransactionStoreResult(
+      const std::string& transaction_id,
+      const PigeonTransactionResult& result_type,
+      const flutter::EncodableList* commands,
+      std::function<void(std::optional<FlutterError> reply)> result) override;
+  virtual void TransactionGet(
+      const PigeonFirebaseApp& app, const std::string& transaction_id,
+      const std::string& path,
+      std::function<void(ErrorOr<PigeonDocumentSnapshot> reply)> result)
+      override;
+  virtual void DocumentReferenceSet(
+      const PigeonFirebaseApp& app, const DocumentReferenceRequest& request,
+      std::function<void(std::optional<FlutterError> reply)> result) override;
+  virtual void DocumentReferenceUpdate(
+      const PigeonFirebaseApp& app, const DocumentReferenceRequest& request,
+      std::function<void(std::optional<FlutterError> reply)> result) override;
+  virtual void DocumentReferenceGet(
+      const PigeonFirebaseApp& app, const DocumentReferenceRequest& request,
+      std::function<void(ErrorOr<PigeonDocumentSnapshot> reply)> result)
+      override;
+  virtual void DocumentReferenceDelete(
+      const PigeonFirebaseApp& app, const DocumentReferenceRequest& request,
+      std::function<void(std::optional<FlutterError> reply)> result) override;
+  virtual void QueryGet(
+      const PigeonFirebaseApp& app, const std::string& path,
+      bool is_collection_group, const PigeonQueryParameters& parameters,
+      const PigeonGetOptions& options,
+      std::function<void(ErrorOr<PigeonQuerySnapshot> reply)> result) override;
+  virtual void AggregateQueryCount(
+      const PigeonFirebaseApp& app, const std::string& path,
+      const PigeonQueryParameters& parameters, const AggregateSource& source,
+      std::function<void(ErrorOr<double> reply)> result) override;
+  virtual void WriteBatchCommit(
+      const PigeonFirebaseApp& app, const flutter::EncodableList& writes,
+      std::function<void(std::optional<FlutterError> reply)> result) override;
+  virtual void QuerySnapshot(
+      const PigeonFirebaseApp& app, const std::string& path,
+      bool is_collection_group, const PigeonQueryParameters& parameters,
+      const PigeonGetOptions& options, bool include_metadata_changes,
+      std::function<void(ErrorOr<std::string> reply)> result) override;
+  virtual void DocumentReferenceSnapshot(
+      const PigeonFirebaseApp& app, const DocumentReferenceRequest& parameters,
+      bool include_metadata_changes,
+      std::function<void(ErrorOr<std::string> reply)> result) override;
 };
 
-}  // namespace cloud_firestore
+}  // namespace cloud_firestore_windows
 
 #endif  // FLUTTER_PLUGIN_CLOUD_FIRESTORE_PLUGIN_H_
