2ec5e0eba995d844bc581b1527cead84b765938b
smith897 <skyler.smith897@gmail.com>
2023-08-26 14:24:30 -0600

    fix(database): Fixed convertFirebaseDatabaseException method throwing null exception
    convertFirebaseDatabaseException would throw an exception if the original exception did not contain a message.

--- packages/firebase_database/firebase_database_web/lib/src/utils/exception.dart
+++ packages/firebase_database/firebase_database_web/lib/src/utils/exception.dart
@@ -7,28 +7,30 @@ part of firebase_database_web;
 FirebaseException convertFirebaseDatabaseException(Object exception,
     [StackTrace? stackTrace]) {
   String code = 'unknown';
-  String message = util.getProperty(exception, 'message');
+  String? message = util.getProperty(exception, 'message');
 
-  // FirebaseWeb SDK for Database has no error codes, so we manually map known
-  // messages to known error codes for cross platform consistency.
-  if (message.toLowerCase().contains('index not defined')) {
-    code = 'index-not-defined';
-  } else if (message.toLowerCase().contains('permission denied')) {
-    code = 'permission-denied';
-  } else if (message
-      .toLowerCase()
-      .contains('transaction needs to be run again with current data')) {
-    code = 'data-stale';
-  } else if (message
-      .toLowerCase()
-      .contains('transaction had too many retries')) {
-    code = 'max-retries';
-  } else if (message.toLowerCase().contains('service is unavailable')) {
-    code = 'unavailable';
-  } else if (message.toLowerCase().contains('network error')) {
-    code = 'network-error';
-  } else if (message.toLowerCase().contains('write was canceled')) {
-    code = 'write-cancelled';
+  if (message != null) {
+    // FirebaseWeb SDK for Database has no error codes, so we manually map known
+    // messages to known error codes for cross platform consistency.
+    if (message.toLowerCase().contains('index not defined')) {
+      code = 'index-not-defined';
+    } else if (message.toLowerCase().contains('permission denied')) {
+      code = 'permission-denied';
+    } else if (message
+        .toLowerCase()
+        .contains('transaction needs to be run again with current data')) {
+      code = 'data-stale';
+    } else if (message
+        .toLowerCase()
+        .contains('transaction had too many retries')) {
+      code = 'max-retries';
+    } else if (message.toLowerCase().contains('service is unavailable')) {
+      code = 'unavailable';
+    } else if (message.toLowerCase().contains('network error')) {
+      code = 'network-error';
+    } else if (message.toLowerCase().contains('write was canceled')) {
+      code = 'write-cancelled';
+    }
   }
 
   return FirebaseException(
