2f1c8d9b5528a911737a0ae6131fa6339a0c3cbe
Salakar <mike@invertase.io>
2023-09-03 10:55:04 +0530

    remove unused import

--- tests/integration_test/firebase_analytics/firebase_analytics_e2e_test.dart
+++ tests/integration_test/firebase_analytics/firebase_analytics_e2e_test.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'dart:io';
-
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
