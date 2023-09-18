803a3731f2e00bf353cb8eb1b4d6703c326116ca
Rexios <rexios@rexios.dev>
2023-09-18 15:09:39 -0400

    Remove non-existent import

--- packages/cloud_firestore_odm/cloud_firestore_odm_generator/cloud_firestore_odm_generator_integration_test/lib/simple.dart
+++ packages/cloud_firestore_odm/cloud_firestore_odm_generator/cloud_firestore_odm_generator_integration_test/lib/simple.dart
@@ -6,8 +6,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cloud_firestore_odm/cloud_firestore_odm.dart';
 import 'package:json_annotation/json_annotation.dart';
 
-import 'model.dart';
-
 part 'simple.g.dart';
 
 final ignoredGetterRef = IgnoredGetterCollectionReference();
