ecd5c05571d37ab8d2ed2bf76022dba829321707
Cynthia J <cynthiajoan@users.noreply.github.com>
2023-10-22 14:26:52 -0700

    feat(storage): move Storage to use Pigeon for platform channels (#11521)
    * pigeon template setup
    
    * make storage macOS running
    
    * method channel implementation update
    
    * android compilable
    
    * android emulator upload succeed
    
    * add more task listener
    
    * Get event channel working
    
    * pause iOS for now
    
    * update message.dart async
    
    * Implement more Storage for iOS functionality
    
    * make ios e2e test build
    
    * more e2e test fix
    
    * make e2e test pass (android)
    
    * android e2e test
    
    * make storage iOS pass e2e
    
    * Add license header
    
    * code format
    
    * Update unit test, remove ones that's not work with pigeon
    
    * add symbolic link for macos files
    
    * make pigeon generated compatible with both iOS and macOS
    
    * fix for analyze check
    
    * add missing analyze fix
    
    * more touch
    
    * address part of review comments, majorly android ones
    
    * format changes
    
    * addressing the iOS feedback
    
    * update with cleanup
    
    * address the analyze check
    
    * add retry to flaky pause/resume test
    
    * clean up the android task destroy logic
    
    * need to address the flakiness of this specific task
    
    * format
    
    ---------
    
    Co-authored-by: Cynthia Jiang <cynthiajiang@google.com>
    Co-authored-by: a-maurice <amaurice@google.com>

---
+++ packages/firebase_storage/firebase_storage/ios/Classes/FLTTaskStateChannelStreamHandler.m
@@ -0,0 +1,97 @@
+// Copyright 2023 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import <FirebaseStorage/FIRStorageTypedefs.h>
+
+#import "FLTFirebaseStoragePlugin.h"
+#import "FLTTaskStateChannelStreamHandler.h"
+
+@implementation FLTTaskStateChannelStreamHandler {
+  FIRStorageObservableTask *_task;
+
+  FIRStorageHandle successHandle;
+  FIRStorageHandle failureHandle;
+  FIRStorageHandle pausedHandle;
+  FIRStorageHandle progressHandle;
+}
+
+- (instancetype)initWithTask:(FIRStorageObservableTask *)task {
+  self = [super init];
+  if (self) {
+    _task = task;
+  }
+  return self;
+}
+
+- (FlutterError *)onListenWithArguments:(id)arguments eventSink:(FlutterEventSink)events {
+  // Set up the various status listeners
+  successHandle =
+      [_task observeStatus:FIRStorageTaskStatusSuccess
+                   handler:^(FIRStorageTaskSnapshot *snapshot) {
+                     events(@{
+                       @"taskState" : @(PigeonStorageTaskStateSuccess),
+                       @"appName" : snapshot.reference.storage.app.name,
+                       @"snapshot" : [FLTFirebaseStoragePlugin parseTaskSnapshot:snapshot],
+                     });
+                   }];
+  failureHandle =
+      [_task observeStatus:FIRStorageTaskStatusFailure
+                   handler:^(FIRStorageTaskSnapshot *snapshot) {
+                     events(@{
+                       @"taskState" : @(PigeonStorageTaskStateError),
+                       @"appName" : snapshot.reference.storage.app.name,
+                       @"snapshot" : [FLTFirebaseStoragePlugin parseTaskSnapshot:snapshot],
+                     });
+                   }];
+  pausedHandle =
+      [_task observeStatus:FIRStorageTaskStatusPause
+                   handler:^(FIRStorageTaskSnapshot *snapshot) {
+                     events(@{
+                       @"taskState" : @(PigeonStorageTaskStatePaused),
+                       @"appName" : snapshot.reference.storage.app.name,
+                       @"snapshot" : [FLTFirebaseStoragePlugin parseTaskSnapshot:snapshot],
+                     });
+                   }];
+  progressHandle =
+      [_task observeStatus:FIRStorageTaskStatusProgress
+                   handler:^(FIRStorageTaskSnapshot *snapshot) {
+                     events(@{
+                       @"taskState" : @(PigeonStorageTaskStateRunning),
+                       @"appName" : snapshot.reference.storage.app.name,
+                       @"snapshot" : [FLTFirebaseStoragePlugin parseTaskSnapshot:snapshot],
+                     });
+                   }];
+
+  return nil;
+}
+
+- (FlutterError *)onCancelWithArguments:(id)arguments {
+  if (!_task) {
+    return nil;
+  }
+
+  if (successHandle) {
+    [_task removeObserverWithHandle:successHandle];
+  }
+  successHandle = nil;
+
+  if (failureHandle) {
+    [_task removeObserverWithHandle:failureHandle];
+  }
+  failureHandle = nil;
+
+  if (pausedHandle) {
+    [_task removeObserverWithHandle:pausedHandle];
+  }
+  pausedHandle = nil;
+
+  if (progressHandle) {
+    [_task removeObserverWithHandle:progressHandle];
+  }
+  progressHandle = nil;
+
+  return nil;
+}
+
+@end
