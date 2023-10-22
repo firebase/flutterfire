ae179a5acfa694187846a42df4e50917ea45d6a5
Remi Rousselet <darky12s@gmail.com>
2023-10-22 21:03:03 +0200

    Fix lints

--- packages/cloud_firestore_odm/cloud_firestore_odm_generator/lib/src/collection_generator.dart
+++ packages/cloud_firestore_odm/cloud_firestore_odm_generator/lib/src/collection_generator.dart
@@ -118,6 +118,7 @@ class CollectionGenerator
   Iterable<Object> generateForAll(GlobalData globalData) sync* {
     yield '''
 // GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element
 // ignore_for_file: type=lint
     ''';
 
