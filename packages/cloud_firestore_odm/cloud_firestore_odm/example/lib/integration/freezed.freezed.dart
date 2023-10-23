0e4fc52bca9fc97e6b6fa8809905bb5ab481aa7b
Remi Rousselet <darky12s@gmail.com>
2023-10-23 13:14:29 +0200

    Regenerate code

--- packages/cloud_firestore_odm/cloud_firestore_odm/example/lib/integration/freezed.freezed.dart
+++ packages/cloud_firestore_odm/cloud_firestore_odm/example/lib/integration/freezed.freezed.dart
@@ -1,7 +1,3 @@
-// Copyright 2022, the Chromium project authors.  Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
-
 // coverage:ignore-file
 // GENERATED CODE - DO NOT MODIFY BY HAND
 // ignore_for_file: type=lint
