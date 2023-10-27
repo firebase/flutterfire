cf6d7b9af81072c6f0901bdcd6d20560bf118f7a
Cynthia J <cynthiajoan@gmail.com>
2023-10-27 11:06:53 -0700

    fix the format

--- packages/cloud_firestore/cloud_firestore/windows/cloud_firestore_plugin.cpp
+++ packages/cloud_firestore/cloud_firestore/windows/cloud_firestore_plugin.cpp
@@ -2,8 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-#pragma comment( \
-        lib, "rpcrt4.lib")  // UuidCreate - Minimum supported OS Win 2000
+#pragma comment(lib, \
+                "rpcrt4.lib")  // UuidCreate - Minimum supported OS Win 2000
 
 #include "cloud_firestore_plugin.h"
 
