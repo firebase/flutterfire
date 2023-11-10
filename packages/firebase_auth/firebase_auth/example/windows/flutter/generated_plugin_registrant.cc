cde57d059e099913efc994db27141540a2a981d1
Guillaume Bernos <guillaume@bernos.dev>
2023-11-10 10:42:32 +0100

    feat(auth, windows): add Windows support for Google Sign In (#11861)
    * feat(auth, windows): add Windows support for Google Sign In
    
    * format

--- packages/firebase_auth/firebase_auth/example/windows/flutter/generated_plugin_registrant.cc
+++ packages/firebase_auth/firebase_auth/example/windows/flutter/generated_plugin_registrant.cc
@@ -8,10 +8,13 @@
 
 #include <firebase_auth/firebase_auth_plugin_c_api.h>
 #include <firebase_core/firebase_core_plugin_c_api.h>
+#include <url_launcher_windows/url_launcher_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   FirebaseAuthPluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FirebaseAuthPluginCApi"));
   FirebaseCorePluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FirebaseCorePluginCApi"));
+  UrlLauncherWindowsRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("UrlLauncherWindows"));
 }
