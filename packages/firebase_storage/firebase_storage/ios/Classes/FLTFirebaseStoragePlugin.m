7f07d7aaf3e4c978b7404660f736032b90bffd61
Russell Wheatley <russellwheatley85@gmail.com>
2023-11-14 09:57:53 +0000

    fix(storage, apple): set the storage emulator only once to stop it from crashing on hot restart (#11862)

--- packages/firebase_storage/firebase_storage/ios/Classes/FLTFirebaseStoragePlugin.m
+++ packages/firebase_storage/firebase_storage/ios/Classes/FLTFirebaseStoragePlugin.m
@@ -68,7 +68,7 @@ typedef NS_ENUM(NSUInteger, FLTFirebaseStorageStringType) {
 @implementation FLTFirebaseStoragePlugin {
   NSMutableDictionary<NSNumber *, FIRStorageObservableTask<FIRStorageTaskManagement> *> *_tasks;
   dispatch_queue_t _callbackQueue;
-  bool hasEmulatorBooted;
+  NSMutableDictionary<NSString *, NSNumber *> *_emulatorBooted;
   NSObject<FlutterBinaryMessenger> *_binaryMessenger;
   NSMutableDictionary<NSString *, FlutterEventChannel *> *_eventChannels;
   NSMutableDictionary<NSString *, NSObject<FlutterStreamHandler> *> *_streamHandlers;
@@ -97,7 +97,7 @@ typedef NS_ENUM(NSUInteger, FLTFirebaseStorageStringType) {
         dictionary];
     _callbackQueue =
         dispatch_queue_create("io.flutter.plugins.firebase.storage", DISPATCH_QUEUE_SERIAL);
-    hasEmulatorBooted = false;
+    _emulatorBooted = [[NSMutableDictionary alloc] init];
     _binaryMessenger = messenger;
     _eventChannels = [NSMutableDictionary dictionary];
     _streamHandlers = [NSMutableDictionary dictionary];
@@ -242,7 +242,12 @@ typedef NS_ENUM(NSUInteger, FLTFirebaseStorageStringType) {
                          port:(NSNumber *)port
                    completion:(void (^)(FlutterError *_Nullable))completion {
   FIRStorage *storage = [self getFIRStorageFromAppNameFromPigeon:app];
-  [storage useEmulatorWithHost:host port:[port integerValue]];
+  NSNumber *emulatorKey = _emulatorBooted[app.bucket];
+
+  if (emulatorKey == nil) {
+    [storage useEmulatorWithHost:host port:[port integerValue]];
+    [_emulatorBooted setObject:@(YES) forKey:app.bucket];
+  }
   completion(nil);
 }
 
