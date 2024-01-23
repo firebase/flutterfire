8e04628f93fb74ac39cf2721774bfcde1474a1bd
fachrifaul <fachripaul@gmail.com>
2024-01-23 20:53:15 +0700

    Update method_channel_messaging.dart

--- packages/firebase_messaging/firebase_messaging_platform_interface/lib/src/method_channel/method_channel_messaging.dart
+++ packages/firebase_messaging/firebase_messaging_platform_interface/lib/src/method_channel/method_channel_messaging.dart
@@ -5,7 +5,6 @@
 
 import 'dart:async';
 import 'dart:io' show Platform;
-import 'dart:ui';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging_platform_interface/firebase_messaging_platform_interface.dart';
