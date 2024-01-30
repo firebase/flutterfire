c08af5412efc260a5ed3749e426c06c707933632
Guillaume Bernos <guillaume@bernos.dev>
2024-01-30 12:03:16 +0100

    fix typing

--- packages/firebase_app_installations/firebase_app_installations_web/lib/src/interop/installations.dart
+++ packages/firebase_app_installations/firebase_app_installations_web/lib/src/interop/installations.dart
@@ -34,12 +34,14 @@ class Installations
   Future<void> delete() =>
       (installations_interop.deleteInstallations(jsObject)).toDart;
 
-  Future<String> getId() =>
-      (installations_interop.getId(jsObject)).toDart as Future<String>;
+  Future<String> getId() => (installations_interop.getId(jsObject))
+      .toDart
+      .then((value) => value! as String);
 
   Future<String> getToken([bool forceRefresh = false]) =>
-      (installations_interop.getToken(jsObject, forceRefresh.toJS)).toDart
-          as Future<String>;
+      (installations_interop.getToken(jsObject, forceRefresh.toJS))
+          .toDart
+          .then((value) => value! as String);
 
   JSFunction? _onIdChangedUnsubscribe;
 
