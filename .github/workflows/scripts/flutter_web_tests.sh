9c4985b0c9c4c5ac3b82daad5859b2ba83650a93
russellwheatley <russellwheatley85@gmail.com>
2024-02-09 14:36:18 +0000

    ci: fix bash script

--- .github/workflows/scripts/flutter_web_tests.sh
+++ .github/workflows/scripts/flutter_web_tests.sh
@@ -5,8 +5,15 @@
 
 # Run the flutter drive command
 echo "HHHHHHHHHHH"
-echo pwd
-if flutter drive --target=./integration_test/e2e_test.dart --driver=./test_driver/integration_test.dart -d chrome --dart-define=CI=true | grep -q '\[E\]'; then
+echo $(pwd)
+
+TARGET_PATH="$1/integration_test/e2e_test.dart"
+DRIVER_PATH="$1/test_driver/integration_test.dart"
+
+echo "Running flutter drive with target: $TARGET_PATH and driver: $DRIVER_PATH"
+
+
+if flutter drive --target="$TARGET_PATH" --driver="$DRIVER_PATH" -d chrome --dart-define=CI=true | grep -q '\[E\]'; then
     echo "Tests failed"
     exit 1
 else
