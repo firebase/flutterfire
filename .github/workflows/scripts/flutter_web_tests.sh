9ddb68c922809ec84f0efa85ca0dc14eb66f99df
russellwheatley <russellwheatley85@gmail.com>
2024-02-09 13:41:37 +0000

    ci: move to bash script

---
+++ .github/workflows/scripts/flutter_web_tests.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Navigate to the correct directory if necessary
+# cd path/to/your/flutter/project
+
+# Run the flutter drive command
+if flutter drive --target=./integration_test/e2e_test.dart --driver=./test_driver/integration_test.dart -d chrome --dart-define=CI=true | grep -q '\[E\]'; then
+    echo "Tests failed"
+    exit 1
+else
+    echo "All tests have passed"
+fi
