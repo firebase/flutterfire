944c567f27f96afa0ec0e67bae2427aac65dfd66
russellwheatley <russellwheatley85@gmail.com>
2024-02-09 14:24:42 +0000

    ci: echo path from script

--- .github/workflows/scripts/flutter_web_tests.sh
+++ .github/workflows/scripts/flutter_web_tests.sh
@@ -4,6 +4,8 @@
 # cd path/to/your/flutter/project
 
 # Run the flutter drive command
+echo "HHHHHHHHHHH"
+echo pwd
 if flutter drive --target=./integration_test/e2e_test.dart --driver=./test_driver/integration_test.dart -d chrome --dart-define=CI=true | grep -q '\[E\]'; then
     echo "Tests failed"
     exit 1
