36b3a1cd1b27b3021aef7aa5a76bf574a1c2374d
russellwheatley <russellwheatley85@gmail.com>
2024-02-15 13:30:59 +0000

    chore: analyse

--- packages/cloud_firestore/cloud_firestore/example/integration_test/query_e2e.dart
+++ packages/cloud_firestore/cloud_firestore/example/integration_test/query_e2e.dart
@@ -7,7 +7,6 @@ import 'dart:math';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void runQueryTests() {
--- packages/cloud_firestore/cloud_firestore/example/integration_test/write_batch_e2e.dart
+++ packages/cloud_firestore/cloud_firestore/example/integration_test/write_batch_e2e.dart
@@ -3,7 +3,6 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void runWriteBatchTests() {
