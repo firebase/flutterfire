276aff056cc5907d6da965f23832225c068042c6
russellwheatley <russellwheatley85@gmail.com>
2024-03-26 09:46:30 +0000

    chore: move private header back for generated C++ code

--- packages/firebase_auth/firebase_auth/windows/messages.g.h
+++ packages/firebase_auth/firebase_auth/windows/messages.g.h
@@ -542,10 +542,10 @@ class PigeonUserDetails {
   const flutter::EncodableList& provider_data() const;
   void set_provider_data(const flutter::EncodableList& value_arg);
 
- private:
   static PigeonUserDetails FromEncodableList(
       const flutter::EncodableList& list);
   flutter::EncodableList ToEncodableList() const;
+ private:
   friend class PigeonUserCredential;
   friend class FirebaseAuthHostApi;
   friend class FirebaseAuthHostApiCodecSerializer;
