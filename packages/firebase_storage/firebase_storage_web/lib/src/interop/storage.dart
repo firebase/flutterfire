d83f632753707c974fef2ac8a7f9bf6cb8ba8758
Guillaume Bernos <guillaume@bernos.dev>
2024-04-02 09:51:25 +0200

    feat(web): remove the dependency on `package:js` in favor of `dart:js_interop` (#12534)
    * feat(web): remove the dependency on package:js in favor of dart:js_interop
    
    * fix typing
    
    * fixing more stuffs
    
    * tests?
    
    * tests
    
    * more
    
    * fix typing
    
    * fix
    
    * fix typing
    
    * fixing interop typing
    
    * fixing null safety
    
    * more fix
    
    * database
    
    * tests
    
    * tests?
    
    * fix typing
    
    * test order?
    
    * storage
    
    * tests messaging
    
    * fix firestore boxing
    
    * test
    
    * improve
    
    * test
    
    * final??
    
    * clean example app

--- packages/firebase_storage/firebase_storage_web/lib/src/interop/storage.dart
+++ packages/firebase_storage/firebase_storage_web/lib/src/interop/storage.dart
@@ -266,7 +266,7 @@ class UploadMetadata
           contentLanguage: contentLanguage?.toJS,
           contentType: contentType?.toJS,
           customMetadata:
-              (customMetadata != null) ? jsify(customMetadata) : null));
+              (customMetadata != null) ? customMetadata.toJSBox : null));
 
   /// Creates a new UploadMetadata from a [jsObject].
   UploadMetadata.fromJsObject(storage_interop.UploadMetadataJsImpl jsObject)
@@ -447,7 +447,7 @@ class SettableMetadata
           contentLanguage: contentLanguage?.toJS,
           contentType: contentType?.toJS,
           customMetadata:
-              (customMetadata != null) ? jsify(customMetadata) : null));
+              (customMetadata != null) ? customMetadata.jsify() : null));
 
   /// Creates a new SettableMetadata from a [jsObject].
   SettableMetadata.fromJsObject(storage_interop.SettableMetadataJsImpl jsObject)
@@ -496,16 +496,15 @@ abstract class _SettableMetadataBase<
 
   /// Additional user-defined custom metadata.
   Map<String, String> get customMetadata {
-    Map<String, dynamic>? metadata = dartify(jsObject.customMetadata);
-    if (metadata != null) {
-      return Map<String, String>.from(metadata);
-    } else {
-      return {};
+    final customMetadata = jsObject.customMetadata.dartify();
+    if (customMetadata == null) {
+      return <String, String>{};
     }
+    return (customMetadata as Map).cast<String, String>();
   }
 
   set customMetadata(Map<String, String> m) {
-    jsObject.customMetadata = jsify(m);
+    jsObject.customMetadata = m.toJSBox;
   }
 }
 
