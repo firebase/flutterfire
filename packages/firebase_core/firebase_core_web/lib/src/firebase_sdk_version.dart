e6d33f6cf04753c1ea5bf53de0933b97b2103794
Guillaume Bernos <guillaume@bernos.dev>
2024-04-05 10:20:40 +0200

    feat: bump JS SDK to version 10.10.0 (#12592)

--- packages/firebase_core/firebase_core_web/lib/src/firebase_sdk_version.dart
+++ packages/firebase_core/firebase_core_web/lib/src/firebase_sdk_version.dart
@@ -6,4 +6,4 @@
 part of firebase_core_web;
 
 /// The currently supported Firebase JS SDK version.
-const String supportedFirebaseJsSdkVersion = '10.8.1';
+const String supportedFirebaseJsSdkVersion = '10.10.0';
