26cb1865d6d1626227a00d52ca259a1700a1f647
Guillaume Bernos <guillaume@bernos.dev>
2024-04-08 10:09:41 +0200

    fix(web): remove remaining references to dart:html

--- packages/firebase_core/firebase_core_web/test/tools.dart
+++ packages/firebase_core/firebase_core_web/test/tools.dart
@@ -2,13 +2,13 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:html';
+import 'package:web/web.dart' as web;
 
 /// Injects a `<meta>` tag with the provided [attributes] into the [dom.document].
 void injectMetaTag(Map<String, String> attributes) {
-  final Element meta = document.createElement('meta');
+  final web.Element meta = web.document.createElement('meta');
   for (final MapEntry<String, String> attribute in attributes.entries) {
     meta.setAttribute(attribute.key, attribute.value);
   }
-  document.head?.append(meta);
+  web.document.head?.append(meta);
 }
