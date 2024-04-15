0a0f252e927fa022d5c3471087bc02d0f22e7ddc
russellwheatley <russellwheatley85@gmail.com>
2024-04-15 11:34:19 +0100

    chore: reinsert stream removing

--- packages/firebase_database/firebase_database_web/lib/src/interop/database.dart
+++ packages/firebase_database/firebase_database_web/lib/src/interop/database.dart
@@ -432,7 +432,7 @@ class Query<T extends database_interop.QueryJsImpl> extends JsObjectWrapper<T> {
     }
 
     void stopListen() {
-      // database_interop.off(jsObject, eventType.toJS, callbackWrap.toJS);
+      database_interop.off(jsObject, eventType.toJS, callbackWrap.toJS);
     }
 
     streamController = StreamController<QueryEvent>.broadcast(
