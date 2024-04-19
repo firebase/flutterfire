628ca940f2bf20552ea4401db823ce0d45389be4
Kamil Kras <38427679+xVemu@users.noreply.github.com>
2024-04-19 12:18:15 +0200

    feat(app_check, android): add option to use FIREBASE_APP_CHECK_DEBUG_TOKEN env variable as debug token on Android

--- packages/firebase_app_check/firebase_app_check/android/src/main/java/io/flutter/plugins/firebase/appcheck/FlutterFirebaseAppRegistrar.java
+++ packages/firebase_app_check/firebase_app_check/android/src/main/java/io/flutter/plugins/firebase/appcheck/FlutterFirebaseAppRegistrar.java
@@ -5,17 +5,32 @@
 package io.flutter.plugins.firebase.appcheck;
 
 import androidx.annotation.Keep;
+
+import com.google.firebase.appcheck.debug.InternalDebugSecretProvider;
 import com.google.firebase.components.Component;
 import com.google.firebase.components.ComponentRegistrar;
 import com.google.firebase.platforminfo.LibraryVersionComponent;
+
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 @Keep
 public class FlutterFirebaseAppRegistrar implements ComponentRegistrar {
+  private static final String DEBUG_SECRET_NAME = "fire-app-check-debug-secret";
+
   @Override
   public List<Component<?>> getComponents() {
-    return Collections.<Component<?>>singletonList(
-        LibraryVersionComponent.create(BuildConfig.LIBRARY_NAME, BuildConfig.LIBRARY_VERSION));
+    Component<?> library = LibraryVersionComponent.create(BuildConfig.LIBRARY_NAME,
+            BuildConfig.LIBRARY_VERSION);
+
+    if (BuildConfig.FIREBASE_APP_CHECK_DEBUG_TOKEN == null)
+      return Collections.<Component<?>>singletonList(library);
+
+    Component<InternalDebugSecretProvider> debugSecretProvider = Component.builder(InternalDebugSecretProvider.class)
+            .name(DEBUG_SECRET_NAME)
+            .factory(container -> () -> BuildConfig.FIREBASE_APP_CHECK_DEBUG_TOKEN).build();
+
+    return Arrays.<Component<?>>asList(library, debugSecretProvider);
   }
 }
