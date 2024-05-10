346be59777234083956f40c08aad583926da9dec
russellwheatley <russellwheatley85@gmail.com>
2024-05-10 11:38:56 +0100

    rm unused import

--- tests/integration_test/firebase_storage/instance_e2e.dart
+++ tests/integration_test/firebase_storage/instance_e2e.dart
@@ -4,7 +4,6 @@
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_storage/firebase_storage.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:tests/firebase_options.dart';
 
