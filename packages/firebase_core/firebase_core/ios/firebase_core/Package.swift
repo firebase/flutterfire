5250627649b0918d73e878ce624baa3641535b80
russellwheatley <russellwheatley85@gmail.com>
2024-05-17 10:37:57 +0100

    license for Package.swift

--- packages/firebase_core/firebase_core/ios/firebase_core/Package.swift
+++ packages/firebase_core/firebase_core/ios/firebase_core/Package.swift
@@ -1,3 +1,7 @@
+// Copyright 2022, the Chromium project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 // swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
