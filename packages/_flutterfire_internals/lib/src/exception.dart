6ca8228df566590f5a9db7381b3594d2b443a022
Ben White <bwhite@swinetechnologies.com>
2024-05-21 15:34:58 -0500

    updating not found exception to give more information

--- packages/_flutterfire_internals/lib/src/exception.dart
+++ packages/_flutterfire_internals/lib/src/exception.dart
@@ -47,7 +47,14 @@ FirebaseException platformExceptionToFirebaseException(
 
   if (details != null) {
     code = (details['code'] as String?) ?? code;
-    message = (details['message'] as String?) ?? message;
+
+    if ((code?.compareTo('not-found') == 0) && message.contains('NOT_FOUND:')) {
+      // For not-found exceptions, return the Firestore provided reason and
+      // document path rather than generic error message.
+      message = message.split('NOT_FOUND:').last.trim();
+    } else {
+      message = (details['message'] as String?) ?? message;
+    }
   }
 
   return FirebaseException(
