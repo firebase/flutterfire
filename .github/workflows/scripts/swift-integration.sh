5d95766dd4e63f34954bb650ac3faadf4e34895c
russellwheatley <russellwheatley85@gmail.com>
2024-05-28 14:54:23 +0100

    ci: swift integration into own shell script + macos build

---
+++ .github/workflows/scripts/swift-integration.sh
@@ -0,0 +1,55 @@
+# Just Core example has pure Swift capability at the moment
+
+
+# firebase_core iOS example
+
+# Setup for SPM integration
+cd packages/firebase_core/firebase_core/example/ios
+rm Podfile
+pod deintegrate
+
+# Run the flutter build command and capture the output and exit status
+flutter_output=$(flutter build ios --no-codesign)
+flutter_exit_code=$?
+
+# Check if the flutter build command was successful
+if [[ $flutter_exit_code -ne 0 ]]; then
+  echo "Flutter build failed with exit code $flutter_exit_code."
+  exit 1
+fi
+
+# Check the output for the specific string
+if [[ "$flutter_output" =~ "Running pod install" ]]; then
+  exit 1
+else
+  echo "Successfully built iOS project using Swift Package Manager."
+  exit 0
+fi
+
+# Come back to root
+cd ~
+
+# firebase_core macOS example
+
+# Setup for SPM integration
+cd packages/firebase_core/firebase_core/example/macos
+rm Podfile
+pod deintegrate
+
+# Run the flutter build command and capture the output and exit status
+flutter_output=$(flutter build macos)
+flutter_exit_code=$?
+
+# Check if the flutter build command was successful
+if [[ $flutter_exit_code -ne 0 ]]; then
+  echo "Flutter build failed with exit code $flutter_exit_code."
+  exit 1
+fi
+
+# Check the output for the specific string
+if [[ "$flutter_output" =~ "Running pod install" ]]; then
+  exit 1
+else
+  echo "Successfully built macOS project using Swift Package Manager."
+  exit 0
+fi
