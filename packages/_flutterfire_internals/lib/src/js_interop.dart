b63b420096a80a926eba3d420510a198ed42f20f
Russell Wheatley <russellwheatley85@gmail.com>
2024-06-25 16:26:30 +0100

    Update js_interop.dart

--- packages/_flutterfire_internals/lib/src/js_interop.dart
+++ packages/_flutterfire_internals/lib/src/js_interop.dart
@@ -1,14 +1,6 @@
 // Copyright 2024, the Chromium project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
-// ignore_for_file: require_trailing_commas
-// DO NOT MOVE THIS FILE
-//
-// Other firebase packages may import `package:firebase_core/src/internals.dart`.
-// Moving it would break the imports
-//
-// This file exports utilities shared between firebase packages, without making
-// them public.
 
 /// This is a stub file to work around odd dart web compiler builder issues.
 export 'dart:js_interop';
