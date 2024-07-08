3b7a2c52ba79945f3729c5b1d177313856b8c9ce
russellwheatley <russellwheatley85@gmail.com>
2024-07-08 09:43:36 +0100

    chore: fix analyze issues

--- packages/firebase_database/firebase_database/lib/ui/firebase_list.dart
+++ packages/firebase_database/firebase_database/lib/ui/firebase_list.dart
@@ -21,7 +21,7 @@ typedef ErrorCallback = void Function(FirebaseException error);
 /// Sorts the results of `query` on the client side using `DataSnapshot.key`.
 class FirebaseList extends ListBase<DataSnapshot>
     with
-        // ignore: prefer_mixin
+        // ignore: prefer_mixin, class_used_as_mixin
         StreamSubscriberMixin<DatabaseEvent> {
   FirebaseList({
     required this.query,
