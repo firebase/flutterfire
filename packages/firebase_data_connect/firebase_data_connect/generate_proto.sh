8983df106e2ecae552353c0e7e5cde1b01de5757
Maneesh Tewani <mtewani@google.com>
2024-07-23 10:31:45 -0700

    Removed unnecessary protos

--- packages/firebase_data_connect/firebase_data_connect/generate_proto.sh
+++ packages/firebase_data_connect/firebase_data_connect/generate_proto.sh
@@ -1,1 +1,4 @@
-protoc --dart_out=grpc:lib/src/generated  -I./protos -I./protos/google protos/connector_service.proto google/protobuf/struct.proto protos/graphql_error.proto
+#!/bin/bash
+rm -rf lib/src/generated
+mkdir lib/src/generated
+protoc --dart_out=grpc:lib/src/generated -I./protos/firebase -I./protos/google  connector_service.proto google/protobuf/struct.proto graphql_error.proto --proto_path=./protos
