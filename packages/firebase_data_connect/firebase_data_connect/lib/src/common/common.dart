7d0826a0ca28b109269a2193813a5323cc10c97f
Maneesh Tewani <mtewani@google.com>
2024-08-13 17:01:50 -0700

    Added missing files

---
+++ packages/firebase_data_connect/firebase_data_connect/lib/src/common/common.dart
@@ -0,0 +1,44 @@
+part of firebase_data_connect;
+
+/// Transport Options for connecting to a specific host.
+class TransportOptions {
+  /// Constructor
+  TransportOptions(this.host, this.port, this.isSecure);
+
+  /// Host to connect to
+  String host;
+
+  /// Port to connect to
+  int? port;
+
+  /// isSecure - use secure protocol
+  bool? isSecure;
+}
+
+/// Interface for transports connecting to the DataConnect backend.
+abstract class DataConnectTransport {
+  /// Constructor.
+  DataConnectTransport(this.transportOptions, this.options);
+
+  /// Transport options.
+  TransportOptions transportOptions;
+
+  /// DataConnect backend configuration.
+  DataConnectOptions options;
+
+  /// Invokes corresponding query endpoint.
+  Future<Data> invokeQuery<Data, Variables>(
+      String queryName,
+      Deserializer<Data> deserializer,
+      Serializer<Variables>? serializer,
+      Variables? vars,
+      String? token);
+
+  /// Invokes corresponding mutation endpoint.
+  Future<Data> invokeMutation<Data, Variables>(
+      String queryName,
+      Deserializer<Data> deserializer,
+      Serializer<Variables>? serializer,
+      Variables? vars,
+      String? token);
+}
