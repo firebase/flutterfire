abb4354a2be5b32cfe0b39566c2c871a95e902c6
Maneesh Tewani <mtewani@google.com>
2024-09-06 11:52:11 -0700

    Removed requirement on firebase_options

--- packages/firebase_data_connect/firebase_data_connect/example/lib/main.dart
+++ packages/firebase_data_connect/firebase_data_connect/example/lib/main.dart
@@ -5,8 +5,9 @@
 import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_data_connect_example/login.dart';
 
-import 'firebase_options.dart';
 import 'package:flutter/material.dart';
+// Uncomment this line after running flutterfire configure
+// import 'firebase_options.dart';
 import 'package:firebase_data_connect/firebase_data_connect.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
@@ -16,9 +17,12 @@ import 'generated/movies.dart';
 const appCheckEnabled = false;
 const configureEmulator = false;
 
+// Required for web. Set equal to `DefaultFirebaseOptions.currentPlatform`
+FirebaseOptions? options;
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Firebase.initializeApp(options: options);
   if (appCheckEnabled) {
     await FirebaseAppCheck.instance.activate(
       // You can also use a `ReCaptchaEnterpriseProvider` provider instance as an
