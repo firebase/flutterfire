c8538815adf2950377433a864f1bb2996c539d00
Guillaume Bernos <guillaume@bernos.dev>
2024-09-09 16:29:47 +0200

    licence

--- packages/firebase_data_connect/firebase_data_connect/test/src/optional_test.dart
+++ packages/firebase_data_connect/firebase_data_connect/test/src/optional_test.dart
@@ -1,3 +1,7 @@
+// Copyright 2024, the Chromium project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 // ignore_for_file: unused_local_variable
 
 import 'package:firebase_data_connect/firebase_data_connect.dart';
