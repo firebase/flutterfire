2e02f0e94e1547c33b265f89c39ee3399477d77c
Guillaume Bernos <guillaume@bernos.dev>
2024-09-11 11:56:40 +0200

    test

--- packages/firebase_data_connect/firebase_data_connect/example/integration_test/e2e_test.dart
+++ packages/firebase_data_connect/firebase_data_connect/example/integration_test/e2e_test.dart
@@ -28,7 +28,7 @@ void main() {
 
       if (kUseFDCEmulator) {
         FirebaseDataConnect.instanceFor(connectorConfig: connector)
-            .useDataConnectEmulator('localhost', 1242);
+            .useDataConnectEmulator('localhost', 9399);
       }
     });
 
