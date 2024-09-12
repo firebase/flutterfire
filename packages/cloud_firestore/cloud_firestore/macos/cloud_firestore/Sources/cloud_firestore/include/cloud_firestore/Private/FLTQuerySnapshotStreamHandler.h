047bf049b48488ae521c0b648dad0c13fbcbd9cc
russellwheatley <russellwheatley85@gmail.com>
2024-09-12 15:09:12 +0100

    chore: initial macOS SPM

---
+++ packages/cloud_firestore/cloud_firestore/macos/cloud_firestore/Sources/cloud_firestore/include/cloud_firestore/Private/FLTQuerySnapshotStreamHandler.h
@@ -0,0 +1,1 @@
+/Users/russellwheatley/projects/flutterfire/packages/cloud_firestore/cloud_firestore/ios/cloud_firestore/Sources/cloud_firestore/include/cloud_firestore/Private/FLTQuerySnapshotStreamHandler.h
