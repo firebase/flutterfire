6f6b33a4ca9e7e8b9f0abc77b63600d52cb1d67d
Guillaume Bernos <guillaume@bernos.dev>
2024-10-16 15:09:35 +0200

    use universal_io

--- packages/firebase_storage/firebase_storage/lib/firebase_storage.dart
+++ packages/firebase_storage/firebase_storage/lib/firebase_storage.dart
@@ -14,6 +14,7 @@ import 'package:firebase_core_platform_interface/firebase_core_platform_interfac
     show FirebasePluginPlatform;
 import 'package:firebase_storage_platform_interface/firebase_storage_platform_interface.dart';
 import 'package:flutter/foundation.dart';
+import 'package:universal_io/io.dart' show File;
 
 import 'src/utils.dart';
 
