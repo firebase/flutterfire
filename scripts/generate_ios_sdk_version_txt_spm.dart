8845393f07f055951f90d522e0a691a5d3af5b85
russellwheatley <russellwheatley85@gmail.com>
2024-11-08 13:20:01 +0000

    license header

--- scripts/generate_ios_sdk_version_txt_spm.dart
+++ scripts/generate_ios_sdk_version_txt_spm.dart
@@ -1,3 +1,7 @@
+// Copyright 2024 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:melos/melos.dart' as melos;
 import 'package:glob/glob.dart';
 import 'dart:io';
