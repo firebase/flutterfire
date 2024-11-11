469a3c847c62ef5f42434c43a2c074a6dfa75194
russellwheatley <russellwheatley85@gmail.com>
2024-11-11 16:23:48 +0000

    test spam words

--- .github/workflows/spam-removal/index.js
+++ .github/workflows/spam-removal/index.js
@@ -2,7 +2,7 @@ import { Octokit } from "@octokit/rest";
 import { context } from "@actions/github";
 import {franc} from "franc-min";
 
-const spamUsers = ['Krakensu', 'Botakjelek', 'pemainlama', 'imyourmanposs', 'clav-code', 'sarahagus', 'teisanilan', 'jagat3131', 'sorenmoren6', 'SDGTAL', 'jedaymc', 'eztillieiium', 'danasatria89', 'puput212306'];
+const spamWords = ['pemain'];
 
 async function closeSpamIssues() {
   const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
@@ -18,20 +18,20 @@ async function closeSpamIssues() {
     const issueContent = `${issue.title} ${issue.body || ''}`;
     const detectedLanguage = franc(issueContent);
 
-    if (spamUsers.includes(issueCreator) || detectedLanguage === 'ind') {
-      await octokit.rest.issues.update({
-        owner: context.repo.owner,
-        repo: context.repo.repo,
-        issue_number: issue.number,
-        state: 'closed'
-      });
+    if (spamWords.includes(issueContent) || detectedLanguage === 'ind') {
+      // await octokit.rest.issues.update({
+      //   owner: context.repo.owner,
+      //   repo: context.repo.repo,
+      //   issue_number: issue.number,
+      //   state: 'closed'
+      // });
 
-      await octokit.rest.issues.addLabels({
-        owner: context.repo.owner,
-        repo: context.repo.repo,
-        issue_number: issue.number,
-        labels: ['resolution: invalid', 'platform: all']
-      });
+      // await octokit.rest.issues.addLabels({
+      //   owner: context.repo.owner,
+      //   repo: context.repo.repo,
+      //   issue_number: issue.number,
+      //   labels: ['resolution: invalid', 'platform: all']
+      // });
 
       console.log(`Closed issue #${issue.number} created by spam user: ${issueCreator} or detected as Indonesian language and added labels.`);
     }
