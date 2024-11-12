2b0ba1c0022044b460775cac928acf9e89b5efd6
Russell Wheatley <russellwheatley85@gmail.com>
2024-11-12 10:36:31 +0000

    chore: common spam (#16225)

--- .github/workflows/spam-removal/index.js
+++ .github/workflows/spam-removal/index.js
@@ -4,6 +4,9 @@ import { franc } from 'franc-min';
 
 const spamWords = [
   'pemain',
+  'dan', // "and" in indonesian, very common
+  'wallet wallet', // seems to be in most crypto issues
+  'minecraft',
   'paybis',
   'blockchain',
   'your feature request title here',
