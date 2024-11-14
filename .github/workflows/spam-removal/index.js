c934ad9e604ad9ad258e2be6bc50c6d719400126
russellwheatley <russellwheatley85@gmail.com>
2024-11-14 16:03:50 +0000

    chore: update title and body

--- .github/workflows/spam-removal/index.js
+++ .github/workflows/spam-removal/index.js
@@ -19,6 +19,7 @@ const spamWords = [
   'moonpay',
   'coinmama',
   ['wallet', 'support'],
+  ['contact', 'support', 'number'],
 ];
 
 async function closeSpamIssues() {
@@ -46,19 +47,21 @@ async function closeSpamIssues() {
     });
 
     if (spam || detectedLanguage === 'ind') {
-      await octokit.rest.issues.update({
-        owner: context.repo.owner,
-        repo: context.repo.repo,
-        issue_number: issue.number,
-        state: 'closed',
-      });
+      // await octokit.rest.issues.update({
+      //   owner: context.repo.owner,
+      //   repo: context.repo.repo,
+      //   issue_number: issue.number,
+      //   state: 'closed',
+      //   title: 'Spam',
+      //   body: 'This issue was filtered as spam.',
+      // });
 
-      await octokit.rest.issues.addLabels({
-        owner: context.repo.owner,
-        repo: context.repo.repo,
-        issue_number: issue.number,
-        labels: ['resolution: invalid', 'platform: all'],
-      });
+      // await octokit.rest.issues.addLabels({
+      //   owner: context.repo.owner,
+      //   repo: context.repo.repo,
+      //   issue_number: issue.number,
+      //   labels: ['resolution: invalid', 'platform: all'],
+      // });
 
       console.log(
         `Closed issue #${issue.number} created by spam user: ${issueCreator} or detected as Indonesian language and added labels.`
