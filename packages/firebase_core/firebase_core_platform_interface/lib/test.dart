a80a2ef9d5da94a9a1f9e3d311a1a651b9cafab4
Kevin Moore <kevmoo@users.noreply.github.com>
2024-11-15 02:45:26 -0800

    fix(firebase_core_platform_interface): move test APIs to test.dart (#16672)
    * fix(firebase_core_platform_interface)!: move test APIs to test.dart
    
    BREAKING CHANGE: move testing apis to a new library: test.dart
    
    This fixes analysis of this package and dependents
    
    * fix lib header

---
+++ packages/firebase_core/firebase_core_platform_interface/lib/test.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: require_trailing_commas
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/// Test helpers for this package.
+library;
+
+export 'package:firebase_core_platform_interface/src/pigeon/messages.pigeon.dart';
+export 'package:firebase_core_platform_interface/src/pigeon/mocks.dart';
+export 'package:firebase_core_platform_interface/src/pigeon/test_api.dart';
