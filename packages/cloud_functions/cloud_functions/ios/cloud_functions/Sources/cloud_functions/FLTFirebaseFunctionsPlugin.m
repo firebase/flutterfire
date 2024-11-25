6fe57d99cd15b20b6c2526ff1fdc3b941cbc7e06
russellwheatley <russellwheatley85@gmail.com>
2024-11-25 17:16:27 +0000

    more fixes

--- packages/cloud_functions/cloud_functions/ios/cloud_functions/Sources/cloud_functions/FLTFirebaseFunctionsPlugin.m
+++ packages/cloud_functions/cloud_functions/ios/cloud_functions/Sources/cloud_functions/FLTFirebaseFunctionsPlugin.m
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "FLTFirebaseFunctionsPlugin.h"
+#import "include/FLTFirebaseFunctionsPlugin.h"
 
 @import FirebaseFunctions;
 #if __has_include(<firebase_core/FLTFirebasePlugin.h>)
