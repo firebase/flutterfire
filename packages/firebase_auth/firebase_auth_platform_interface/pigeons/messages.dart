de208654ade4d8b9e7ab89b5d8c9c443e298f330
russellwheatley <russellwheatley85@gmail.com>
2024-11-26 12:47:54 +0000

    update pigeon output

--- packages/firebase_auth/firebase_auth_platform_interface/pigeons/messages.dart
+++ packages/firebase_auth/firebase_auth_platform_interface/pigeons/messages.dart
@@ -18,8 +18,8 @@ import 'package:pigeon/pigeon.dart';
       className: 'GeneratedAndroidFirebaseAuth',
     ),
     objcHeaderOut:
-        '../firebase_auth/ios/Classes/Public/firebase_auth_messages.g.h',
-    objcSourceOut: '../firebase_auth/ios/Classes/firebase_auth_messages.g.m',
+        '../firebase_auth/ios/firebase_auth/Sources/firebase_auth/include/Public/firebase_auth_messages.g.h',
+    objcSourceOut: '../firebase_auth/ios/firebase_auth/Sources/firebase_auth/firebase_auth_messages.g.m',
     cppHeaderOut: '../firebase_auth/windows/messages.g.h',
     cppSourceOut: '../firebase_auth/windows/messages.g.cpp',
     cppOptions: CppOptions(namespace: 'firebase_auth_windows'),
