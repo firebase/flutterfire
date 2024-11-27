c007cddf49e8f8c1578f84bd4035a59b96170aa3
russellwheatley <russellwheatley85@gmail.com>
2024-11-27 16:22:07 +0000

    fix(database, macos): conditional imports for headers

--- packages/firebase_database/firebase_database/ios/firebase_database/Sources/firebase_database/include/FLTFirebaseDatabasePlugin.h
+++ packages/firebase_database/firebase_database/ios/firebase_database/Sources/firebase_database/include/FLTFirebaseDatabasePlugin.h
@@ -1,11 +1,15 @@
 // Copyright 2021 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-@import FirebaseDatabase;
+
+#import <TargetConditionals.h>
+
 #if TARGET_OS_OSX
 #import <FlutterMacOS/FlutterMacOS.h>
+#import <FirebaseDatabase/FirebaseDatabase.h>
 #else
 #import <Flutter/Flutter.h>
+@import FirebaseDatabase;
 #endif
 
 #import <Foundation/Foundation.h>
