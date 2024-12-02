c85925e161913545c617c712247d877a5790529c
russellwheatley <russellwheatley85@gmail.com>
2024-12-02 13:03:45 +0000

    test: swift integration for app check

--- .github/workflows/scripts/swift-integration.dart
+++ .github/workflows/scripts/swift-integration.dart
@@ -16,6 +16,8 @@ void main() async {
   await buildSwiftExampleApp('macos', 'cloud_functions');
   await buildSwiftExampleApp('ios', 'firebase_database');
   await buildSwiftExampleApp('macos', 'firebase_database');
+  await buildSwiftExampleApp('ios', 'firebase_app_check');
+  await buildSwiftExampleApp('macos', 'firebase_app_check');
 }
 
 Future<void> deleteFirstLine(String filePath) async {
