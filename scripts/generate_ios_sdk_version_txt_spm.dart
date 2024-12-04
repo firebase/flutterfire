6ad514234e919481a6f496927e19f7977d9ad7d3
russellwheatley <russellwheatley85@gmail.com>
2024-12-04 13:44:56 +0000

    latest way of getting shared core requires getting everything

--- scripts/generate_ios_sdk_version_txt_spm.dart
+++ scripts/generate_ios_sdk_version_txt_spm.dart
@@ -6,6 +6,7 @@ import 'package:melos/melos.dart' as melos;
 import 'package:glob/glob.dart';
 import 'dart:io';
 import 'package:cli_util/cli_logging.dart' as logging;
+import 'package:yaml/yaml.dart';
 
 // Used to generate a simple txt file for Package.swift file to parse in order to use correct firebase-ios-sdk version
 
@@ -33,6 +34,8 @@ void main(List<String> args) async {
       versionFile.writeAsStringSync(firebaseiOSVersion);
     }
   }
+  // Update the versions in root Package.swift
+  updateSharedCoreVersion(firebaseiOSVersion);
 }
 
 Future<melos.MelosWorkspace> getMelosWorkspace() async {
@@ -68,4 +71,68 @@ String getFirebaseiOSVersion(File firebaseCoreIosSdkVersion) {
   } else {
     throw Exception('firebase_sdk_version.rb file does not exist.');
   }
+}
+
+void updateSharedCoreVersion(String firebaseiOSVersion) {
+  // Define the path to the pubspec.yaml file
+  const pubspecPath = 'packages/firebase_core/firebase_core/pubspec.yaml';
+
+
+  // Read the pubspec.yaml file
+  final pubspecFile = File(pubspecPath);
+  if (!pubspecFile.existsSync()) {
+    print('Error: pubspec.yaml file not found at $pubspecPath');
+    return;
+  }
+
+
+  // Parse the YAML content
+  final pubspecContent = pubspecFile.readAsStringSync();
+  final pubspecYaml = loadYaml(pubspecContent);
+
+
+  // Extract the version
+  final version = pubspecYaml['version'];
+  if (version == null) {
+    print('Error: Version not found in pubspec.yaml');
+    return;
+  }
+
+
+  // Define the path to the Package.swift file
+  const packageSwiftPath = 'Package.swift';
+
+
+  // Read the Package.swift file
+  final packageSwiftFile = File(packageSwiftPath);
+  if (!packageSwiftFile.existsSync()) {
+    print('Error: Package.swift file not found at $packageSwiftPath');
+    return;
+  }
+
+
+  // Read the content of Package.swift
+  final packageSwiftContent = packageSwiftFile.readAsStringSync();
+
+
+  // Update the library_version_string with the new version
+  final updatedFirebaseCoreVersion = packageSwiftContent.replaceAll(
+    RegExp('let firebase_core_version: String = "[^"]+"'),
+    'let firebase_core_version: String = "$version"',
+  );
+
+
+  final updatedFirebaseIosVersion = updatedFirebaseCoreVersion.replaceAll(
+    RegExp('let firebase_ios_sdk_version: String = "[^"]+"'),
+    'let firebase_ios_sdk_version: String = "$firebaseiOSVersion"',
+  );
+
+
+  // Write the updated content back to Package.swift
+  packageSwiftFile.writeAsStringSync(updatedFirebaseIosVersion);
+
+
+  print(
+    'Updated Package.swift with firebase_core version: $version & firebase-ios-sdk version: $firebaseiOSVersion',
+  );
 }
