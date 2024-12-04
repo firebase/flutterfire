03f2e66f730a84a68d9d7f4c24f0734e389d2e5d
russellwheatley <russellwheatley85@gmail.com>
2024-12-04 15:28:24 +0000

    format

--- packages/firebase_crashlytics/firebase_crashlytics/ios/firebase_crashlytics/Package.swift
+++ packages/firebase_crashlytics/firebase_crashlytics/ios/firebase_crashlytics/Package.swift
@@ -34,7 +34,12 @@ func loadFirebaseSDKVersion() throws -> String {
 }
 
 func loadPubspecVersions() throws -> (packageVersion: String, firebaseCoreVersion: String) {
-  let pubspecPath = NSString.path(withComponents: [crashlyticsDirectory, "..", "..", "pubspec.yaml"])
+  let pubspecPath = NSString.path(withComponents: [
+    crashlyticsDirectory,
+    "..",
+    "..",
+    "pubspec.yaml",
+  ])
   do {
     let yamlString = try String(contentsOfFile: pubspecPath, encoding: .utf8)
     let lines = yamlString.split(separator: "\n")
