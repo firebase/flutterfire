bf27785bf045bfc396cacc5d0b8cddeb761e4991
russellwheatley <russellwheatley85@gmail.com>
2024-12-06 10:51:06 +0000

    fix macOS build

--- packages/firebase_auth/firebase_auth/ios/firebase_auth/Sources/firebase_auth/PigeonParser.m
+++ packages/firebase_auth/firebase_auth/ios/firebase_auth/Sources/firebase_auth/PigeonParser.m
@@ -2,6 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+@import FirebaseAuth;
+
 #import "include/Private/PigeonParser.h"
 #import <Foundation/Foundation.h>
 #import "include/Public/CustomPigeonHeader.h"
