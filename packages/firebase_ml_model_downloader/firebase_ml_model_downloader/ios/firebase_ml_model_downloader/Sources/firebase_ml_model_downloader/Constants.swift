40a3b80d50df3456db630e2d9c1451d782235585
russellwheatley <russellwheatley85@gmail.com>
2024-12-10 16:56:01 +0000

    add license header

--- packages/firebase_ml_model_downloader/firebase_ml_model_downloader/ios/firebase_ml_model_downloader/Sources/firebase_ml_model_downloader/Constants.swift
+++ packages/firebase_ml_model_downloader/firebase_ml_model_downloader/ios/firebase_ml_model_downloader/Sources/firebase_ml_model_downloader/Constants.swift
@@ -1,1 +1,5 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 public let versionNumber = "0.3.1+6"
