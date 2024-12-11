fdf4423d98dc19f3dbd5892495ec599c2a1eb7bf
russellwheatley <russellwheatley85@gmail.com>
2024-12-11 10:11:29 +0000

    add note

--- packages/firebase_ml_model_downloader/firebase_ml_model_downloader/ios/firebase_ml_model_downloader/Sources/firebase_ml_model_downloader/Constants.swift
+++ packages/firebase_ml_model_downloader/firebase_ml_model_downloader/ios/firebase_ml_model_downloader/Sources/firebase_ml_model_downloader/Constants.swift
@@ -2,4 +2,5 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// Auto-generated file. Do not edit.
 public let versionNumber = "0.3.1+6"
