814ab189e4c36d0efbcd089069772b5217631e76
Jude Kwashie <judekwashie70@gmail.com>
2025-03-10 13:33:49 +0000

    chore: use `toDart` instead

--- packages/firebase_app_installations/firebase_app_installations_web/lib/src/interop/installations.dart
+++ packages/firebase_app_installations/firebase_app_installations_web/lib/src/interop/installations.dart
@@ -34,12 +34,12 @@ class Installations
 
   Future<String> getId() => (installations_interop.getId(jsObject))
       .toDart
-      .then((value) => value.toString());
+      .then((value) => value.toDart);
 
   Future<String> getToken([bool forceRefresh = false]) =>
       (installations_interop.getToken(jsObject, forceRefresh.toJS))
           .toDart
-          .then((value) => value.toString());
+          .then((value) => value.toDart);
 
   JSFunction? _onIdChangedUnsubscribe;
 
