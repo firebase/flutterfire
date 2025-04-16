44b182e5663db41f71a56544f288027b807af2bc
MichaelVerdon <mwverdon2000@gmail.com>
2025-04-16 17:31:44 +0100

    chore(deps): automated script working

--- scripts/generate_versions_gradle.dart
+++ scripts/generate_versions_gradle.dart
@@ -16,25 +16,41 @@ import 'package:melos/melos.dart' as melos;
 import 'package:glob/glob.dart';
 import 'dart:io';
 import 'package:cli_util/cli_logging.dart' as logging;
-import 'package:yaml/yaml.dart';
+import 'package:path/path.dart' as path;
 
 // Used to generate a simple txt file for local-config.gradle files to parse in order to use correct java and compilation versions.
 
-void main() async{
-
+void main() async {
   final workspace = await getMelosWorkspace();
-  final globalConfig = File('../gradle/global_config.gradle');
+  // get version from core
+  // To edit versions for all packages, edit the global-config.txt file in firebase_core package
+  // located in the android folder for global-config.txt
+  final firebaseCorePackage = workspace.filteredPackages.values
+      .firstWhere((package) => package.name == 'firebase_core');
 
-  final javaVersion = getVersions(globalConfig);
+  final globalConfig = File(
+    '${firebaseCorePackage.path}/android/global-config.txt',
+  );
+  
+  if (!globalConfig.existsSync()) {
+    throw Exception(
+    'global_config.txt file not found in the expected location.',
+    );
+  }
+
+  final versions = getVersions(globalConfig);
 
   for (final package in workspace.filteredPackages.values) {
     final localConfigGradleFile =
         File('${package.path}/android/local-config.gradle');
 
     if (localConfigGradleFile.existsSync()) {
-      final versionFile =
+      final packageVersionFile =
           File('${package.path}/android/generated_android_versions.txt');
-      versionFile.writeAsStringSync(await javaVersion);
+      packageVersionFile.writeAsStringSync(await versions);
+      final exampleAppVersionFile = File(
+          '${package.path}/example/android/app/generated_android_versions.txt');
+      exampleAppVersionFile.writeAsStringSync(await versions);
     }
   }
 }
@@ -59,19 +75,18 @@ Future<melos.MelosWorkspace> getMelosWorkspace() async {
 
 Future<String> getVersions(File globalConfig) async {
   if (globalConfig.existsSync()) {
-    final contents = await globalConfig.readAsString();
+    final contents = globalConfig.readAsStringSync();
     final lines = contents.split('\n');
-    final javaVersion = lines.firstWhere((line) => line.contains('javaVersion'));
+    final javaVersion =
+        lines.firstWhere((line) => line.contains('javaVersion'));
     final compileSdkVersion =
-        lines.firstWhere((line) => line.contains('compileSdkVersion'));
+        lines.firstWhere((line) => line.contains('compileSdk'));
     final minSdkVersion =
-        lines.firstWhere((line) => line.contains('minSdkVersion'));
+        lines.firstWhere((line) => line.contains('minSdk'));
     final targetSdkVersion =
-        lines.firstWhere((line) => line.contains('targetSdkVersion'));
+        lines.firstWhere((line) => line.contains('targetSdk'));
 
     return '$javaVersion\n$compileSdkVersion\n$minSdkVersion\n$targetSdkVersion';
   }
-  throw Exception(
-      'global_config.txt file not found in the expected location.',
-    );
+  throw Exception('global-config.txt file has wrong format or something missing.');
 }
