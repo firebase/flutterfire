c38263e48a9dd4df754cac1dce0c1533692dad62
Cynthia J <cynthiajoan@users.noreply.github.com>
2025-04-18 10:55:36 -0700

    Add appId for in request header (#17278)

--- packages/firebase_vertexai/firebase_vertexai/lib/src/generative_model.dart
+++ packages/firebase_vertexai/firebase_vertexai/lib/src/generative_model.dart
@@ -56,7 +56,7 @@ final class GenerativeModel extends BaseApiClientModel {
             client: HttpApiClient(
                 apiKey: app.options.apiKey,
                 httpClient: httpClient,
-                requestHeaders: BaseModel.firebaseTokens(appCheck, auth)));
+                requestHeaders: BaseModel.firebaseTokens(appCheck, auth, app)));
 
   GenerativeModel._constructTestModel({
     required String model,
@@ -82,7 +82,8 @@ final class GenerativeModel extends BaseApiClientModel {
             client: apiClient ??
                 HttpApiClient(
                     apiKey: app.options.apiKey,
-                    requestHeaders: BaseModel.firebaseTokens(appCheck, auth)));
+                    requestHeaders:
+                        BaseModel.firebaseTokens(appCheck, auth, app)));
 
   final List<SafetySetting> _safetySettings;
   final GenerationConfig? _generationConfig;
