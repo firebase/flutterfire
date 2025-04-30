a6087fb8e08ebaa9db31633b44f99cce2ee472b4
Cynthia J <cynthiajoan@users.noreply.github.com>
2025-04-30 09:34:45 -0700

    Revert to latest version of example app
    Minor code changes for the interface change.

--- packages/firebase_vertexai/firebase_vertexai/example/lib/pages/imagen_page.dart
+++ packages/firebase_vertexai/firebase_vertexai/example/lib/pages/imagen_page.dart
@@ -25,7 +25,7 @@ class ImagenPage extends StatefulWidget {
   });
 
   final String title;
-  final GenerativeModel model;
+  final ImagenModel model;
 
   @override
   State<ImagenPage> createState() => _ImagenPageState();
@@ -37,26 +37,6 @@ class _ImagenPageState extends State<ImagenPage> {
   final FocusNode _textFieldFocus = FocusNode();
   final List<MessageData> _generatedContent = <MessageData>[];
   bool _loading = false;
-  late final ImagenModel _imagenModel;
-
-  @override
-  void initState() {
-    super.initState();
-    var generationConfig = ImagenGenerationConfig(
-      negativePrompt: 'frog',
-      numberOfImages: 1,
-      aspectRatio: ImagenAspectRatio.square1x1,
-      imageFormat: ImagenFormat.jpeg(compressionQuality: 75),
-    );
-    _imagenModel = FirebaseVertexAI.instance.imagenModel(
-      model: 'imagen-3.0-generate-001',
-      generationConfig: generationConfig,
-      safetySettings: ImagenSafetySettings(
-        ImagenSafetyFilterLevel.blockLowAndAbove,
-        ImagenPersonFilterLevel.allowAdult,
-      ),
-    );
-  }
 
   void _scrollDown() {
     WidgetsBinding.instance.addPostFrameCallback(
@@ -154,7 +134,7 @@ class _ImagenPageState extends State<ImagenPage> {
     });
 
     try {
-      var response = await _imagenModel.generateImages(prompt);
+      var response = await widget.model.generateImages(prompt);
 
       if (response.images.isNotEmpty) {
         var imagenImage = response.images[0];
@@ -186,7 +166,7 @@ class _ImagenPageState extends State<ImagenPage> {
   //   });
   //   var gcsUrl = 'gs://vertex-ai-example-ef5a2.appspot.com/imagen';
 
-  //   var response = await _imagenModel.generateImagesGCS(prompt, gcsUrl);
+  //   var response = await widget.model.generateImagesGCS(prompt, gcsUrl);
 
   //   if (response.images.isNotEmpty) {
   //     var imagenImage = response.images[0];
