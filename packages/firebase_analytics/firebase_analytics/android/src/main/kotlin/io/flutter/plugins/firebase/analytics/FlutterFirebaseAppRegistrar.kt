57c091395d86a3a40c6520f4b5cffcd8165de4f1
Jude Selase Kwashie <64037520+SelaseKay@users.noreply.github.com>
2025-06-03 14:51:32 +0000

    feat(analytics): add Pigeon support for firebase_analytics (#17403)
    * feat(analytics): add Pigeon support for firebase_analytics
    
    * chore: fix ci
    
    * chore: fix ci
    
    * chore: remove reference to symlink
    
    * chore: update kotlin version to 2.1.0 for test app
    
    * chore: remove failing tests

---
+++ packages/firebase_analytics/firebase_analytics/android/src/main/kotlin/io/flutter/plugins/firebase/analytics/FlutterFirebaseAppRegistrar.kt
@@ -0,0 +1,20 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package io.flutter.plugins.firebase.analytics
+
+import androidx.annotation.Keep
+import com.google.firebase.components.Component
+import com.google.firebase.components.ComponentRegistrar
+import com.google.firebase.platforminfo.LibraryVersionComponent
+
+
+@Keep
+class FlutterFirebaseAppRegistrar : ComponentRegistrar {
+  override fun getComponents(): List<Component<*>> {
+    return listOf(
+      LibraryVersionComponent.create(BuildConfig.LIBRARY_NAME, BuildConfig.LIBRARY_VERSION)
+    )
+  }
+}
