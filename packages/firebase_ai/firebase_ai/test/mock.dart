d01c27752a7a46a291db20002f989f9421a5b896
Cynthia J <cynthiajoan@gmail.com>
2025-07-02 13:30:25 -0700

    Add missing api_test to firebase_ai

--- packages/firebase_ai/firebase_ai/test/mock.dart
+++ packages/firebase_ai/firebase_ai/test/mock.dart
@@ -18,7 +18,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
-class MockFirebaseAppVertexAI implements TestFirebaseCoreHostApi {
+class MockFirebaseAppAI implements TestFirebaseCoreHostApi {
   @override
   Future<CoreInitializeResponse> initializeApp(
     String appName,
@@ -61,13 +61,13 @@ class MockFirebaseAppVertexAI implements TestFirebaseCoreHostApi {
 void setupFirebaseVertexAIMocks() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
-  TestFirebaseCoreHostApi.setUp(MockFirebaseAppVertexAI());
+  TestFirebaseCoreHostApi.setUp(MockFirebaseAppAI());
 }
 
 // FirebaseVertexAIPlatform Mock
-class MockFirebaseVertexAI extends Mock
+class MockFirebaseAI extends Mock
     with
         // ignore: prefer_mixin, plugin_platform_interface needs to migrate to use `mixin`
         MockPlatformInterfaceMixin {
-  MockFirebaseVertexAI();
+  MockFirebaseAI();
 }
