5c589bad06fab316d58804a42c85a7e50de50452
Cynthia J <cynthiajoan@gmail.com>
2025-07-07 11:45:58 -0700

    remove unnecessary import

--- packages/firebase_ai/firebase_ai/test/api_test.dart
+++ packages/firebase_ai/firebase_ai/test/api_test.dart
@@ -11,8 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-import 'dart:convert';
-
 import 'package:firebase_ai/firebase_ai.dart';
 import 'package:firebase_ai/src/api.dart';
 
