de4eabe4317a1c7e2c3d0b7f602da59627e69154
Jude Kwashie <judekwashie70@gmail.com>
2025-07-29 13:04:03 +0000

    clean up code

--- .github/workflows/scripts/swift-integration.dart
+++ .github/workflows/scripts/swift-integration.dart
@@ -87,10 +87,6 @@ Future<void> updatePackageSwiftForPackage(String packageName, String branch) asy
     // handles forked repositories
     final repoSlug = headRepo != baseRepo ? headRepo : baseRepo;
 
-    // handles forked repositories
-    // final repoSlug = Platform.environment['GITHUB_REPOSITORY'] ?? 'firebase/flutterfire';
-    print('Using repository: $repoSlug');
-
     // Replace with branch dependency
     final branchDependency = '.package(url: "https://github.com/$repoSlug", branch: "$branch")';
 
