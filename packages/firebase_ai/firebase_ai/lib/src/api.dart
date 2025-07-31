3ccc4515fe82060921b41c92660828fd12da56b7
Greg Spencer <gspencer@google.com>
2025-07-31 09:11:09 -0700

    Fix analyzer warning

--- packages/firebase_ai/firebase_ai/lib/src/api.dart
+++ packages/firebase_ai/firebase_ai/lib/src/api.dart
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import 'dart:convert';
-
 import 'content.dart';
 import 'error.dart';
 import 'schema.dart';
