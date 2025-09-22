33489239703476ae36d5b0e7580d9444f494b4f3
Jude Kwashie <judekwashie70@gmail.com>
2025-09-22 12:52:50 +0000

    fix(tests): update imports and correct test formatting for consistency

--- tests/integration_test/firebase_crashlytics/firebase_crashlytics_e2e_test.dart
+++ tests/integration_test/firebase_crashlytics/firebase_crashlytics_e2e_test.dart
@@ -5,9 +5,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:tests/firebase_options.dart';
+import 'e2e_test.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
@@ -98,9 +100,8 @@ void main() {
             );
           },
         );
-      });
 
-       test(
+        test(
           'should have consistent error reason format',
           () async {
               final eventChannel = EventChannel('plugins.flutter.io/firebase_crashlytics_test_stream');
