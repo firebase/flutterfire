007b2b366f49263660e946a5a631e6919fc48eac
Jude Selase Kwashie <64037520+SelaseKay@users.noreply.github.com>
2025-09-25 10:12:40 +0000

    feat: bump Firebase JS SDK to 12.3.0 (#17743)

--- packages/firebase_core/firebase_core_web/lib/src/firebase_sdk_version.dart
+++ packages/firebase_core/firebase_core_web/lib/src/firebase_sdk_version.dart
@@ -6,4 +6,4 @@
 part of '../firebase_core_web.dart';
 
 /// The currently supported Firebase JS SDK version.
-const String supportedFirebaseJsSdkVersion = '12.2.1';
+const String supportedFirebaseJsSdkVersion = '12.3.0';
