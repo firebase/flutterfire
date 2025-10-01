0cdc0e3944284537e2a10976bd2afca409452378
MichaelVerdon <mwverdon2000@gmail.com>
2025-10-01 13:49:01 +0100

    chore: more test skips

--- tests/integration_test/firebase_auth/firebase_auth_instance_e2e_test.dart
+++ tests/integration_test/firebase_auth/firebase_auth_instance_e2e_test.dart
@@ -193,7 +193,7 @@ void main() {
             );
           });
         },
-        skip: !kIsWeb && (Platform.isWindows || Platform.isMacOS),
+        skip: !kIsWeb && (Platform.isWindows || Platform.isMacOS || Platform.isIOS),
       );
 
       group('test all stream listeners', () {
@@ -255,7 +255,7 @@ void main() {
             }
           });
         },
-        skip: !kIsWeb && Platform.isWindows && Platform.isMacOS,
+        skip: !kIsWeb && (Platform.isWindows || Platform.isMacOS || Platform.isIOS),
       );
 
       group(
@@ -272,7 +272,7 @@ void main() {
             }
           });
         },
-        skip: !kIsWeb && Platform.isWindows && Platform.isMacOS,
+        skip: !kIsWeb && (Platform.isWindows || Platform.isMacOS || Platform.isIOS),
       );
 
       group(
@@ -292,7 +292,7 @@ void main() {
             }
           });
         },
-        skip: !kIsWeb && Platform.isWindows && Platform.isMacOS,
+        skip: !kIsWeb && (Platform.isWindows || Platform.isMacOS || Platform.isIOS),
       );
 
       group('createUserWithEmailAndPassword', () {
@@ -443,7 +443,7 @@ void main() {
             }
           });
         },
-        skip: !kIsWeb && Platform.isWindows && Platform.isMacOS,
+        skip: !kIsWeb && (Platform.isWindows || Platform.isMacOS || Platform.isIOS),
       );
 
       group(
@@ -868,7 +868,7 @@ void main() {
             }
           });
         },
-        skip: !kIsWeb && Platform.isWindows && Platform.isMacOS,
+        skip: !kIsWeb && (Platform.isWindows || Platform.isMacOS),
       );
 
       group(
