0ea1377c11f8299d2ae09263ea8a7ef04651b2bf
Jaime Blasco <jaime@sparkli.ai>
2025-10-08 17:25:01 +0200

    chore: export setup complete

--- packages/firebase_ai/firebase_ai/lib/firebase_ai.dart
+++ packages/firebase_ai/firebase_ai/lib/firebase_ai.dart
@@ -108,6 +108,7 @@ export 'src/live_api.dart'
         LiveServerMessage,
         LiveServerContent,
         LiveServerToolCall,
+        LiveServerSetupComplete,
         Transcription,
         LiveServerToolCallCancellation,
         LiveServerResponse,
