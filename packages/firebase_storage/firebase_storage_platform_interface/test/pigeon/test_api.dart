290511fd90bcf8a851a5a1ef9687eb020f5932f6
MichaelVerdon <mwverdon2000@gmail.com>
2025-10-24 12:15:12 +0100

    chore: undo redundant change

--- packages/firebase_storage/firebase_storage_platform_interface/test/pigeon/test_api.dart
+++ packages/firebase_storage/firebase_storage_platform_interface/test/pigeon/test_api.dart
@@ -7,7 +7,6 @@
 // ignore_for_file: avoid_relative_lib_imports
 import 'dart:async';
 import 'dart:typed_data' show Uint8List;
-
 import 'package:firebase_storage_platform_interface/src/pigeon/messages.pigeon.dart';
 import 'package:flutter/foundation.dart' show ReadBuffer, WriteBuffer;
 import 'package:flutter/services.dart';
