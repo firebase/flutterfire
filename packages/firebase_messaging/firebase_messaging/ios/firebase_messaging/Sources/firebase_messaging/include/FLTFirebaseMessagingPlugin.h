1a638a72a3838caea360bdc28e4720f128c7794c
MichaelVerdon <mwverdon2000@gmail.com>
2025-11-18 13:18:39 +0000

    chore: imports

--- packages/firebase_messaging/firebase_messaging/ios/firebase_messaging/Sources/firebase_messaging/include/FLTFirebaseMessagingPlugin.h
+++ packages/firebase_messaging/firebase_messaging/ios/firebase_messaging/Sources/firebase_messaging/include/FLTFirebaseMessagingPlugin.h
@@ -14,7 +14,7 @@
 #import <Foundation/Foundation.h>
 #import <UserNotifications/UserNotifications.h>
 
-#if __has_include(<firebase_core_shared/firebase_core_shared-Swift.h>)
+#ifdef SWIFT_PACKAGE
 @import firebase_core_shared;
 #else
 @import firebase_core;
