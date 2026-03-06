8c1705960ef143afdfd4613c0f40e1b9586a55ab
Guillaume Bernos <guillaume@bernos.dev>
2026-03-06 08:35:13 +0100

    clean

--- tests/integration_test/firebase_database/firebase_database_e2e_test.dart
+++ tests/integration_test/firebase_database/firebase_database_e2e_test.dart
@@ -4,7 +4,6 @@
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:tests/firebase_options.dart';
