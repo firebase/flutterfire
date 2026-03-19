48e7fd2846a7c2517df13aa1b5752028ae66047e
Guillaume Bernos <guillaume@bernos.dev>
2026-03-19 13:53:25 +0100

    restore pod files

--- packages/cloud_firestore/cloud_firestore/example/macos/Runner/AppDelegate.swift
+++ packages/cloud_firestore/cloud_firestore/example/macos/Runner/AppDelegate.swift
@@ -6,8 +6,4 @@ class AppDelegate: FlutterAppDelegate {
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
     return true
   }
-
-  override func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
-    return true
-  }
 }
