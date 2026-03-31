5adbce42c4691afe73b9286cba3a2066ce22e94d
Cynthia J <cynthiajoan@gmail.com>
2026-03-30 22:03:40 -0700

    this package doesn't need cSettings

--- packages/firebase_ai/firebase_ai/macos/firebase_ai/Package.swift
+++ packages/firebase_ai/firebase_ai/macos/firebase_ai/Package.swift
@@ -1,13 +1,11 @@
 // swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
-// Copyright 2024, the Chromium project authors.  Please see the AUTHORS file
+// Copyright 2026, the Chromium project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
 import PackageDescription
-
-let library_version = "3.10.0"
 
 let package = Package(
   name: "firebase_ai",
@@ -25,10 +23,6 @@ let package = Package(
       resources: [
         .process("Resources"),
       ],
-      cSettings: [
-        .define("LIBRARY_VERSION", to: "\"\(library_version)\""),
-        .define("LIBRARY_NAME", to: "\"flutter-fire-ai\""),
-      ]
     ),
   ]
 )
