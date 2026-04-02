e62eaaa4d2c51d955c9bfeb10d0aa6720b0580cb
Cynthia J <cynthiajoan@gmail.com>
2026-04-02 11:50:10 -0700

    remove unrelated files from  PR

--- packages/firebase_ai/firebase_ai/example/android/app/build.gradle.kts
+++ packages/firebase_ai/firebase_ai/example/android/app/build.gradle.kts
@@ -1,8 +1,5 @@
 plugins {
     id("com.android.application")
-    // START: FlutterFire Configuration
-    id("com.google.gms.google-services")
-    // END: FlutterFire Configuration
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
