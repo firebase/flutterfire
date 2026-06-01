0853e6898f745c63135e255689789fadeaeff77d
Guillaume Bernos <guillaume@bernos.dev>
2026-06-01 15:50:36 +0200

    updating Packages.swift

--- packages/firebase_performance/firebase_performance/ios/firebase_performance/Package.swift
+++ packages/firebase_performance/firebase_performance/ios/firebase_performance/Package.swift
@@ -8,7 +8,7 @@
 import PackageDescription
 
 let libraryVersion = "0.11.4-1"
-let firebaseSdkVersion: Version = "12.13.0"
+let firebaseSdkVersion: Version = "12.14.0"
 
 let package = Package(
   name: "firebase_performance",
