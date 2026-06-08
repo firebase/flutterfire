a2bb57cdd711065ba3aa25b68d5042ae700a27c9
Jude Kwashie <judekwashie70@gmail.com>
2026-06-08 14:22:22 +0000

    feat: Built-in Kotlin migration

--- packages/cloud_firestore/cloud_firestore/pipeline_example/android/app/build.gradle.kts
+++ packages/cloud_firestore/cloud_firestore/pipeline_example/android/app/build.gradle.kts
@@ -3,8 +3,7 @@ plugins {
     // START: FlutterFire Configuration
     id("com.google.gms.google-services")
     // END: FlutterFire Configuration
-    id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    // The Flutter Gradle Plugin must be applied after the Android Gradle plugin.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
@@ -16,10 +15,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
@@ -44,4 +39,10 @@ android {
 
 flutter {
     source = "../.."
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+    }
 }
