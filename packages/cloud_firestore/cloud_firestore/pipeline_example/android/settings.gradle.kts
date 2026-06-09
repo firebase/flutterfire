c25c31dc86bd2e4152298eaa2fc722ade2c52ab3
Jude Kwashie <judekwashie70@gmail.com>
2026-06-09 08:14:58 +0000

    chore: update Google Services plugin version to 4.4.4 in settings.gradle.kts

--- packages/cloud_firestore/cloud_firestore/pipeline_example/android/settings.gradle.kts
+++ packages/cloud_firestore/cloud_firestore/pipeline_example/android/settings.gradle.kts
@@ -21,7 +21,7 @@ plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "9.0.0" apply false
     // START: FlutterFire Configuration
-    id("com.google.gms.google-services") version("4.3.15") apply false
+    id("com.google.gms.google-services") version("4.4.4") apply false
     // END: FlutterFire Configuration
 }
 
