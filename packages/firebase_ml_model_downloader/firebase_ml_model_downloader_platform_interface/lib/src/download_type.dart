84756885f12d50c75faf9e0bbbb41cf4243a690b
Guillaume Bernos <guillaume@bernos.dev>
2026-07-22 16:09:02 +0200

    feat(ml): adding depreciationg notice to ML plugin

--- packages/firebase_ml_model_downloader/firebase_ml_model_downloader_platform_interface/lib/src/download_type.dart
+++ packages/firebase_ml_model_downloader/firebase_ml_model_downloader_platform_interface/lib/src/download_type.dart
@@ -3,6 +3,11 @@
 // found in the LICENSE file.
 
 /// A download type to download a custom model when calling the [getModel] API.
+@Deprecated(
+  'Firebase ML is deprecated and will shut down on June 15, 2027. '
+  'Migrate hosted custom models to another solution, such as Cloud Storage '
+  'for Firebase. See https://firebase.google.com/docs/ml/migrate-to-cloud-storage.',
+)
 enum FirebaseModelDownloadType {
   /// Returns the current model if present, otherwise triggers new download
   /// (or finds one in progress) and only completes when download is finished.
