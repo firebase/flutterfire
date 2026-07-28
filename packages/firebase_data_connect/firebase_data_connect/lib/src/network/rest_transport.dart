04b94acf8f0a0188d6abbf3770edfe31b4e5ef2e
Denver Coneybeare <dconeybe@google.com>
2026-07-28 20:55:16 +0000

    feat(sql_connect): add "X-Client-Platform" and "X-Client-Version" headers (#18484)

--- packages/firebase_data_connect/firebase_data_connect/lib/src/network/rest_transport.dart
+++ packages/firebase_data_connect/firebase_data_connect/lib/src/network/rest_transport.dart
@@ -85,7 +85,9 @@ class RestTransport implements DataConnectTransport {
       'Content-Type': 'application/json',
       'Accept': 'application/json',
       'x-goog-api-client': getGoogApiVal(sdkType, packageVersion),
-      'x-firebase-client': getFirebaseClientVal(packageVersion)
+      'x-firebase-client': getFirebaseClientVal(packageVersion),
+      'x-client-platform': 'flutter',
+      'x-client-version': packageVersion,
     };
     String? appCheckToken;
     try {
