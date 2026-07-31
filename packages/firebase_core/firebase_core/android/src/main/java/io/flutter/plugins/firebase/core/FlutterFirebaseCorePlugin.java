8edbc78ee7d96d489d19ff4b02d371339db7d977
Cynthia J <cynthiajoan@gmail.com>
2026-07-31 10:03:25 -0700

    fix build error

--- packages/firebase_core/firebase_core/android/src/main/java/io/flutter/plugins/firebase/core/FlutterFirebaseCorePlugin.java
+++ packages/firebase_core/firebase_core/android/src/main/java/io/flutter/plugins/firebase/core/FlutterFirebaseCorePlugin.java
@@ -149,7 +149,6 @@ public class FlutterFirebaseCorePlugin
                     .setProjectId(initializeAppRequest.getProjectId())
                     .setStorageBucket(initializeAppRequest.getStorageBucket())
                     .setGaTrackingId(initializeAppRequest.getTrackingId())
-                    .setRecaptchaSiteKey(initializeAppRequest.getRecaptchaSiteKey())
                     .build();
             // TODO(Salakar) hacky workaround a bug with FirebaseInAppMessaging causing the error:
             //    Can't create handler inside thread Thread[pool-3-thread-1,5,main] that has not
