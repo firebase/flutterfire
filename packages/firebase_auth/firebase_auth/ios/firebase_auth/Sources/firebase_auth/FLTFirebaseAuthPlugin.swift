9ba10ff5f1c1d63dd9de7251f6ce4a33e727ba88
Jude Selase Kwashie <64037520+SelaseKay@users.noreply.github.com>
2026-08-19 16:28:54 +0000

    fix(auth,apple): use lowerCamelCase for openURLContexts internal parameter

--- packages/firebase_auth/firebase_auth/ios/firebase_auth/Sources/firebase_auth/FLTFirebaseAuthPlugin.swift
+++ packages/firebase_auth/firebase_auth/ios/firebase_auth/Sources/firebase_auth/FLTFirebaseAuthPlugin.swift
@@ -189,11 +189,9 @@ public class FLTFirebaseAuthPlugin: NSObject, FlutterPlugin, FLTFirebasePluginPr
       Auth.auth().canHandle(url)
     }
 
-    // UIKit's scene(_:openURLContexts:) uses this parameter name.
-    // swift-format-ignore: AlwaysUseLowerCamelCase
-    public func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) -> Bool
+    public func scene(_ scene: UIScene, openURLContexts urlContexts: Set<UIOpenURLContext>) -> Bool
     {
-      for urlContext in URLContexts where Auth.auth().canHandle(urlContext.url) {
+      for urlContext in urlContexts where Auth.auth().canHandle(urlContext.url) {
         return true
       }
       return false
