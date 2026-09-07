f51f6fa9bcc7031d3788f7bd9f0f0a4803a56942
Jude Kwashie <judekwashie70@gmail.com>
2026-09-07 01:46:56 +0000

    fix(app_installations,android): dispatch ID-change events on the main thread
    FidListener runs on Firebase's blocking executor, and EventSink.success must be called on the main looper.
    
    Fixes https://github.com/firebase/flutterfire/issues/18646

--- packages/firebase_app_installations/firebase_app_installations/android/src/main/kotlin/io/flutter/plugins/firebase/installations/firebase_app_installations/TokenChannelStreamHandler.kt
+++ packages/firebase_app_installations/firebase_app_installations/android/src/main/kotlin/io/flutter/plugins/firebase/installations/firebase_app_installations/TokenChannelStreamHandler.kt
@@ -3,25 +3,36 @@
 // found in the LICENSE file.
 package io.flutter.plugins.firebase.installations.firebase_app_installations
 
+import android.os.Handler
+import android.os.Looper
 import com.google.firebase.installations.FirebaseInstallations
 import com.google.firebase.installations.internal.FidListener
+import com.google.firebase.installations.internal.FidListenerHandle
 import io.flutter.plugin.common.EventChannel
 
 class TokenChannelStreamHandler(private val firebaseInstallations: FirebaseInstallations) :
     EventChannel.StreamHandler {
 
   private var listener: FidListener? = null
+  private var listenerHandle: FidListenerHandle? = null
+  private val mainHandler = Handler(Looper.getMainLooper())
 
   override fun onListen(arguments: Any?, events: EventChannel.EventSink) {
     listener = createTokenEventListener(events)
-    firebaseInstallations.registerFidListener(listener!!)
+    listenerHandle = firebaseInstallations.registerFidListener(listener!!)
   }
 
   override fun onCancel(arguments: Any?) {
+    listenerHandle?.unregister()
+    listenerHandle = null
     listener = null
   }
 
   internal fun createTokenEventListener(events: EventChannel.EventSink): FidListener {
-    return FidListener { token -> events.success(mapOf("token" to token)) }
+    return FidListener { token ->
+      // FidListener is invoked on Firebase's blocking executor. EventSink
+      // must be used on the main thread.
+      mainHandler.post { events.success(mapOf("token" to token)) }
+    }
   }
 }
